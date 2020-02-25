b51e98563fa435066c40183380cf0ad0d5aac646
Daehee Han <bookdori81@gmail.com>
2020-02-25 13:09:01 -0800

    Updated Top K Frequent Elements

--- src/main/java/com/booknara/problem/hash/TopKFrequentElements.java
+++ src/main/java/com/booknara/problem/hash/TopKFrequentElements.java
@@ -7,15 +7,6 @@ import java.util.*;
  * https://leetcode.com/problems/top-k-frequent-elements/
  */
 public class TopKFrequentElements {
-    class Pair {
-        int num;
-        int count;
-        Pair(int num, int count) {
-            this.num = num;
-            this.count = count;
-        }
-    }
-
     public List<Integer> topKFrequent(int[] nums, int k) {
         List<Integer> ans = new ArrayList<>();
         if (nums == null || nums.length == 0) {
@@ -28,22 +19,24 @@ public class TopKFrequentElements {
             map.put(nums[i], count + 1);
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((a, b) -> {
-            if (a.count < b.count) {
+        PriorityQueue<Map.Entry<Integer, Integer>> pq
+                = new PriorityQueue<>((a, b) -> {
+            if (a.getValue() < b.getValue()) {
                 return -1;
             } else {
                 return 1;
             }
         });
+
         for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
+            pq.add(entry);
             if (pq.size() > k) {
                 pq.poll();
             }
         }
 
         while (!pq.isEmpty()) {
-            ans.add(pq.poll().num);
+            ans.add(pq.poll().getKey());
         }
 
         return ans;
