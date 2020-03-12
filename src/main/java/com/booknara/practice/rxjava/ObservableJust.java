965d95038a2094a1b328b13a4ade5c0c1b2b7927
Daehee Han <bookdori81@gmail.com>
2020-03-12 10:16:31 -0700

    Added Simple RxJava example(Observable)

---
+++ src/main/java/com/booknara/practice/rxjava/ObservableJust.java
@@ -0,0 +1,33 @@
+package com.booknara.practice.rxjava;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class ObservableJust {
+    public static void main(String[] args) {
+        Observable.just("x", "y", "z")
+                .subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("onNext() " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
+        System.out.println("last line");
+    }
+}
