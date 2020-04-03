a56573b50343bf1f03713a78a326ff7dce519af3
Daehee Han <bookdori81@gmail.com>
2020-04-03 00:03:54 -0700

    Added Diagonal Traverse problem

---
+++ src/main/java/com/booknara/problem/array/DiagonalTraverse.java
@@ -0,0 +1,35 @@
+package com.booknara.problem.array;
+
+/**
+ * 498. Diagonal Traverse (Medium)
+ * https://leetcode.com/problems/diagonal-traverse/
+ */
+public class DiagonalTraverse {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return new int[0];
+
+        int m = matrix.length, n = matrix[0].length;
+
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+        boolean up = true;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+            if (up) {
+                row--;
+                col++;
+            } else {
+                row++;
+                col--;
+            }
+
+            if (row >= m) { row = m - 1; col += 2; up = !up;}
+            if (col >= n) { col = n - 1; row += 2; up = !up;}
+            if (row < 0)  { row = 0; up = !up;}
+            if (col < 0)  { col = 0; up = !up;}
+        }
+
+        return result;
+    }
+}
