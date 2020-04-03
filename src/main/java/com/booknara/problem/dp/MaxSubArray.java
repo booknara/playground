8581ea0d2c99500fede28abe2b3ffd575e3929a1
Daehee Han <bookdori81@gmail.com>
2020-04-03 00:23:53 -0700

    Coding challenge Day 3

--- src/main/java/com/booknara/problem/dp/MaxSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxSubArray.java
@@ -2,15 +2,25 @@ package com.booknara.problem.dp;
 
 /**
  * Leet code: 53 (Easy)
- * Given an integer array nums,
- * find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
- * Input: [-2,1,-3,4,-1,2,1,-5,4],
- * Output: 6
- * Explanation: [4,-1,2,1] has the largest sum = 6.
- *
- * sum[i] = max(nums[i], sum[i - 1] + nums[i])
+ * https://leetcode.com/problems/maximum-subarray/
  */
 public class MaxSubArray {
+    // Time complexity: O(n), Space complexity: O(1)
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = nums[0];
+        int total = max;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(nums[i] + max, nums[i]);
+            total = Math.max(total, max);
+        }
+
+        return total;
+    }
+
     // Time complexity: O(n), Space complexity: O(n)
     public int maxSubArray1(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -26,27 +36,6 @@ public class MaxSubArray {
         for (int i = 1; i < nums.length; i++) {
             sum[i] = Math.max(nums[i], sum[i - 1] + nums[i]);
             max = Math.max(max, sum[i]);
-        }
-
-        return max;
-    }
-
-    // Time complexity: O(n), Space complexity: O(1)
-    public int maxSubArray2(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
-        int prev = nums[0];
-        int max = prev;
-        for (int i = 1; i < nums.length; i++) {
-            int curr = Math.max(nums[i], prev + nums[i]);
-            max = Math.max(max, curr);
-            prev = curr;
         }
 
         return max;
--- src/test/java/com/booknara/problem/dp/MaxSubArrayTest.java
+++ src/test/java/com/booknara/problem/dp/MaxSubArrayTest.java
@@ -14,11 +14,11 @@ public class MaxSubArrayTest {
         Assert.assertEquals(maxSubArray.maxSubArray1(new int[] {-1,-2}), -1);
         Assert.assertEquals(maxSubArray.maxSubArray1(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}), 6);
 
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {0, 1}), 1);
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {2, 0, 1}), 3);
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {2, -1, 1}), 2);
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {-2,-1}), -1);
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {-1,-2}), -1);
-        Assert.assertEquals(maxSubArray.maxSubArray2(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}), 6);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {0, 1}), 1);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {2, 0, 1}), 3);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {2, -1, 1}), 2);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {-2,-1}), -1);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {-1,-2}), -1);
+        Assert.assertEquals(maxSubArray.maxSubArray(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}), 6);
     }
 }
