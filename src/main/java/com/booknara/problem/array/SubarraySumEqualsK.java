60c83f36a209537c493b05e6d2e4151677d2e51c
Daehee Han <bookdori81@gmail.com>
2020-04-04 22:01:09 -0700

    Added Subarray Sum Equals K problem

--- src/main/java/com/booknara/problem/array/SubarraySumEqualsK.java
+++ src/main/java/com/booknara/problem/array/SubarraySumEqualsK.java
@@ -3,31 +3,35 @@ package com.booknara.problem.array;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 560. Subarray Sum Equals K (Medium)
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ */
 public class SubarraySumEqualsK {
+    // Using Hash(one pass)
     public int subarraySum(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        int[] sum = new int[nums.length + 1];
-        sum[0] = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum[i + 1] = sum[i] + nums[i];
-        }
-
         int count = 0;
-        for (int i = 0; i < sum.length; i++) {
-            for (int j = i + 1; j < sum.length; j++) {
-                if (sum[j] - sum[i] == k) {
-                    count++;
-                }
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
             }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
     }
 
-    public int subarraySumWithoutSpace(int[] nums, int k) {
+    // O(n^2) without space
+    public int subarraySum1(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -46,23 +50,25 @@ public class SubarraySumEqualsK {
         return count;
     }
 
-    public int subarraySumWithHash(int[] nums, int k) {
+    // O(n^2) with space
+    public int subarraySum2(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
 
-        int count = 0;
-        int sum = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            }
+            sum[i + 1] = sum[i] + nums[i];
+        }
 
-            int z = map.getOrDefault(sum, 0) + 1;
-            map.put(sum, z);
+        int count = 0;
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = i + 1; j < sum.length; j++) {
+                if (sum[j] - sum[i] == k) {
+                    count++;
+                }
+            }
         }
 
         return count;
--- src/test/java/com/booknara/problem/array/SubarraySumEqualsKTest.java
+++ src/test/java/com/booknara/problem/array/SubarraySumEqualsKTest.java
@@ -11,12 +11,12 @@ public class SubarraySumEqualsKTest {
         Assert.assertEquals(0, subarraySumEqualsK.subarraySum(new int[] {1}, 0));
         Assert.assertEquals(55, subarraySumEqualsK.subarraySum(new int[] {0,0,0,0,0,0,0,0,0,0}, 0));
 
-        Assert.assertEquals(2, subarraySumEqualsK.subarraySumWithoutSpace(new int[] {1, 1, 1}, 2));
-        Assert.assertEquals(0, subarraySumEqualsK.subarraySumWithoutSpace(new int[] {1}, 0));
-        Assert.assertEquals(55, subarraySumEqualsK.subarraySumWithoutSpace(new int[] {0,0,0,0,0,0,0,0,0,0}, 0));
+        Assert.assertEquals(2, subarraySumEqualsK.subarraySum1(new int[] {1, 1, 1}, 2));
+        Assert.assertEquals(0, subarraySumEqualsK.subarraySum1(new int[] {1}, 0));
+        Assert.assertEquals(55, subarraySumEqualsK.subarraySum1(new int[] {0,0,0,0,0,0,0,0,0,0}, 0));
 
-        Assert.assertEquals(2, subarraySumEqualsK.subarraySumWithHash(new int[] {1, 1, 1}, 2));
-        Assert.assertEquals(0, subarraySumEqualsK.subarraySumWithHash(new int[] {1}, 0));
-        Assert.assertEquals(55, subarraySumEqualsK.subarraySumWithHash(new int[] {0,0,0,0,0,0,0,0,0,0}, 0));
+        Assert.assertEquals(2, subarraySumEqualsK.subarraySum2(new int[] {1, 1, 1}, 2));
+        Assert.assertEquals(0, subarraySumEqualsK.subarraySum2(new int[] {1}, 0));
+        Assert.assertEquals(55, subarraySumEqualsK.subarraySum2(new int[] {0,0,0,0,0,0,0,0,0,0}, 0));
     }
 }
