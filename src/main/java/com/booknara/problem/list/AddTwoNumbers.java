a14da961743f532f87f1368b6c6bd84b27ba3073
Daehee Han <bookdori81@gmail.com>
2020-04-08 03:11:11 -0700

    Updated Add Two Numbers problem

--- src/main/java/com/booknara/problem/list/AddTwoNumbers.java
+++ src/main/java/com/booknara/problem/list/AddTwoNumbers.java
@@ -2,8 +2,16 @@ package com.booknara.problem.list;
 
 import com.booknara.problem.common.ListNode;
 
+/**
+ * 2. Add Two Numbers (Medium)
+ * https://leetcode.com/problems/add-two-numbers/
+ */
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
         // l1, l2 is non-empty linked list
         ListNode current = new ListNode(0);
         ListNode head = current;
@@ -12,11 +20,13 @@ public class AddTwoNumbers {
             int num1 = 0;
             if (l1 != null) {
                 num1 = l1.val;
+                l1 = l1.next;
             }
 
             int num2 = 0;
             if (l2 != null) {
                 num2 = l2.val;
+                l2 = l2.next;
             }
 
             int sum = num1 + num2 + carry;
@@ -24,14 +34,6 @@ public class AddTwoNumbers {
             ListNode node = new ListNode(sum % 10);
             current.next = node;
             current = node;
-
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-
-            if (l2 != null) {
-                l2 = l2.next;
-            }
         }
 
         return head.next;
