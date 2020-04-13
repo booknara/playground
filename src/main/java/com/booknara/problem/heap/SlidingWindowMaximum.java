326ceb7609db3e71a8caf85b447e038d3bf5f0e4
Daehee Han <bookdori81@gmail.com>
2020-04-13 04:38:05 -0700

    Added Sliding Window Maximum problem

---
+++ src/main/java/com/booknara/problem/heap/SlidingWindowMaximum.java
@@ -0,0 +1,57 @@
+package com.booknara.problem.heap;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 239. Sliding Window Maximum (Hard)
+ * https://leetcode.com/problems/sliding-window-maximum/
+ */
+public class SlidingWindowMaximum {
+    // O(n) using Deque
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[] {};
+        }
+
+        int[] res = new int[nums.length - k + 1];
+        // Dequeue: to store index
+        Deque<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            // Remove an element out of range from k
+            while (!queue.isEmpty() && queue.peekFirst() <= i - k) {
+                queue.pollFirst();
+            }
+            // Remove all the smaller elements than k
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+
+            queue.offerLast(i);
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[queue.peekFirst()];
+            }
+        }
+
+        return res;
+    }
+
+    // O(n*k)
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[] {};
+        }
+
+        int[] res = new int[nums.length - k + 1];
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+
+            res[i] = max;
+        }
+
+        return res;
+    }
+}
