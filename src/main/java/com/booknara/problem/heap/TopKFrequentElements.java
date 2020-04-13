becdac44df3f98333faa52470c63fcd23836c03e
Daehee Han <bookdori81@gmail.com>
2020-04-12 23:39:39 -0700

    Added Top K Frequent Elements problem

--- src/main/java/com/booknara/problem/heap/TopKFrequentElements.java
+++ src/main/java/com/booknara/problem/heap/TopKFrequentElements.java
@@ -1,4 +1,4 @@
-package com.booknara.problem.hash;
+package com.booknara.problem.heap;
 
 import java.util.*;
 
@@ -8,25 +8,21 @@ import java.util.*;
  */
 public class TopKFrequentElements {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer> ans = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return ans;
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0 || k == 0) {
+            return res;
         }
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            //System.out.println(nums[i]);
             int count = map.getOrDefault(nums[i], 0);
             map.put(nums[i], count + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> pq
-                = new PriorityQueue<>((a, b) -> {
-            if (a.getValue() < b.getValue()) {
-                return -1;
-            } else {
-                return 1;
-            }
-        });
+        // Min Heap
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((entry1, entry2)
+                -> entry1.getValue() < entry2.getValue() ? -1 : 1);
 
         for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
             pq.add(entry);
@@ -36,9 +32,9 @@ public class TopKFrequentElements {
         }
 
         while (!pq.isEmpty()) {
-            ans.add(pq.poll().getKey());
+            res.add(pq.poll().getKey());
         }
 
-        return ans;
+        return res;
     }
 }
