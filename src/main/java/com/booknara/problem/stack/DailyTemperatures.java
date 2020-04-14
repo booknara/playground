ce77e38b2b26848b1ae2d592be1f052ad14f29b1
Daehee Han <bookdori81@gmail.com>
2020-04-14 04:23:29 -0700

    Added Daily Temperatures problem

--- src/main/java/com/booknara/problem/stack/DailyTemperatures.java
+++ src/main/java/com/booknara/problem/stack/DailyTemperatures.java
@@ -1,7 +1,5 @@
 package com.booknara.problem.stack;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -10,6 +8,59 @@ import java.util.Stack;
  */
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] T) {
+        int[] res = new int[T.length];
+        if (T == null || T.length < 2) {
+            return res;
+        }
+
+        for (int i = T.length - 2; i >= 0; i--) {
+            //System.out.println(i);
+            int j = i + 1;
+            while (j < T.length && T[j] <= T[i]) {
+                // edge case: if T[i] is the highest element currently
+                if (res[j] == 0) break;
+                // Jump to next higher element
+                j += res[j];
+            }
+
+            // edge case: if T[i] is the highest element currently
+            if (T[i] >= T[j]) {
+                continue;
+            }
+            res[i] = j - i;
+        }
+
+        return res;
+    }
+
+    // 4.14.2020 version
+    public int[] dailyTemperatures1(int[] T) {
+        int[] res = new int[T.length];
+        if (T == null || T.length < 2) {
+            return res;
+        }
+
+        // keep all the index numbers that has higher than current element
+        Stack<Integer> stack = new Stack<>();
+        for (int i = T.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && T[stack.peek()] <= T[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                res[i] = 0;
+            } else {
+                res[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    // 3.10.2020 version
+    public int[] dailyTemperatures2(int[] T) {
         if (T == null || T.length == 0) {
             return null;
         }
@@ -29,46 +80,6 @@ public class DailyTemperatures {
             }
 
             stack.push(i);
-        }
-
-        return res;
-    }
-
-    // Time exceed
-    public int[] dailyTemperatures1(int[] T) {
-        if (T == null || T.length == 0) {
-            return null;
-        }
-
-        Map<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
-        int[] res = new int[T.length];
-        res[T.length - 1] = 0;
-        stack.push(T[T.length - 1]);
-        map.put(T[T.length - 1], T.length - 1);
-        for (int i = T.length - 2; i >= 0; i--) {
-            //System.out.println(T[i]);
-            int temperature = T[i];
-            Stack<Integer> inner = new Stack<>();
-            while (!stack.empty()) {
-                int temp = stack.pop();
-                if (temperature < temp) {
-                    res[i] = map.get(temp) - i;
-                    inner.push(temp);
-                    break;
-                }
-
-                inner.push(temp);
-            }
-
-            while (!inner.empty()) {
-                stack.push(inner.pop());
-            }
-
-            if (stack.peek() != temperature) {
-                stack.push(temperature);
-            }
-            map.put(temperature, i);
         }
 
         return res;
