00ec37ec498987ef35560cc166ff8e29a976d946
Daehee Han <bookdori81@gmail.com>
2020-04-14 03:33:45 -0700

    Added Longest Palindromic Substring problem

--- src/main/java/com/booknara/problem/string/LongestPalindromicSubstring.java
+++ src/main/java/com/booknara/problem/string/LongestPalindromicSubstring.java
@@ -1,33 +1,50 @@
 package com.booknara.problem.string;
 
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
 /**
- *
+ * 5. Longest Palindromic Substring (Medium)
+ * https://leetcode.com/problems/longest-palindromic-substring/
  */
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        Set<String> set = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return s1.length() - s2.length();
-            }
-        });
+        if (s == null || s.length() < 1) {
+            return s;
+        }
 
+        int max = 1;
+        int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                String word = s.substring(i, j + 1);
-                if (set.contains(word)) {
-                    continue;
+            // case #1: aba (odd length palindrome)
+            int l = i - 1, r = i + 1;
+            while (l >= 0 && r < s.length()) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                if (max < r - l + 1) {
+                    max = r - l + 1;
+                    start = l;
+                    end = r;
+                }
+                l--;
+                r++;
+            }
+            // case #2: aa (even length palindrome)
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.length()) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                if (max < r - l + 1) {
+                    max = r - l + 1;
+                    start = l;
+                    end = r;
                 }
 
-                set.add(word);
+                l--;
+                r++;
             }
         }
 
-        return "";
-
+        return s.substring(start, end + 1);
     }
 }
