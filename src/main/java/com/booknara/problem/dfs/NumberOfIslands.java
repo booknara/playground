65671b10fdd2f0e4e38772f79e60086907f3bd07
Daehee Han <bookdori81@gmail.com>
2020-04-17 10:47:15 -0700

    Added Number of Islands problem

--- src/main/java/com/booknara/problem/dfs/NumberOfIslands.java
+++ src/main/java/com/booknara/problem/dfs/NumberOfIslands.java
@@ -1,20 +1,8 @@
 package com.booknara.problem.dfs;
 
 /**
- * Leet code: 200 Number of Islands(Medium)
- * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
- * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
- * You may assume all four edges of the grid are all surrounded by water.
- *
- * Example 1:
- *
- * Input:
- * 11110
- * 11010
- * 11000
- * 00000
- *
- * Output: 1
+ * 200. Number of Islands (Medium)
+ * https://leetcode.com/problems/number-of-islands/
  */
 public class NumberOfIslands {
     public int numIslands(char[][] grid) {
@@ -22,34 +10,30 @@ public class NumberOfIslands {
             return 0;
         }
 
-        int total = 0;
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    total++;
-                    visitGrid(grid, i, j);
+                    count++;
+                    dfs(grid, i, j);
                 }
             }
         }
 
-        return total;
+        return count;
     }
 
-    private void visitGrid(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length
-                || j < 0 || j >= grid[i].length
-                || grid[i][j] == '0') {
+    public void dfs(char[][] grid, int i, int j) {
+        // out of scope or invalid case
+        if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[i].length - 1 || grid[i][j] == '0') {
             return;
         }
 
+        // grid[i][j] = '1' case
         grid[i][j] = '0';
-        // left
-        visitGrid(grid, i - 1, j);
-        // top
-        visitGrid(grid, i, j - 1);
-        // right
-        visitGrid(grid, i + 1, j);
-        // bottom
-        visitGrid(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 }
