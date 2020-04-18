96d4fb08648c7d47d6c02a0b3c21a8fe48a177b2
Daehee Han <bookdori81@gmail.com>
2020-04-18 08:40:06 -0700

    Added Minimum Path Sum problem

--- src/main/java/com/booknara/problem/dp/MinimumPathSum.java
+++ src/main/java/com/booknara/problem/dp/MinimumPathSum.java
@@ -1,5 +1,9 @@
 package com.booknara.problem.dp;
 
+/**
+ * 64. Minimum Path Sum (Medium)
+ * https://leetcode.com/problems/minimum-path-sum/
+ */
 public class MinimumPathSum {
     public int minPathSum(int[][] grid) {
         // check input handling
@@ -7,25 +11,20 @@ public class MinimumPathSum {
             return 0;
         }
 
-        if (grid.length == 1 && grid[0].length == 1) {
-            return grid[0][0];
-        }
-
-        int i = 0, j = 0;
-        for (i = 0; i < grid.length; i++) {
-            for (j = 0; j < grid[i].length; j++) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (i == 0 && j == 0) {
                     continue;
                 }
 
-                int up = i - 1 < 0 ? Integer.MAX_VALUE : grid[i - 1][j];
-                int left = j - 1 < 0 ? Integer.MAX_VALUE : grid[i][j - 1];
+                int up = i < 1 ? Integer.MAX_VALUE : grid[i - 1][j];
+                int left = j < 1 ? Integer.MAX_VALUE : grid[i][j - 1];
                 // F[i][j] = min(F[i - 1][j] => up, F[i][j - 1] => left) + F[i][j];
-                grid[i][j] = Math.min(up, left) + grid[i][j];
+                grid[i][j] = grid[i][j] + Math.min(up, left);
             }
         }
 
-        return grid[i - 1][j - 1];
+        return grid[grid.length - 1][grid[0].length - 1];
     }
 
     public int minPathSum1(int[][] grid) {
