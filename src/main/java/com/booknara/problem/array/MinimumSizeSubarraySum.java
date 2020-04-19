c5f5110436b5a261a52e8eca88f769c751d9caa9
Daehee Han <bookdori81@gmail.com>
2020-04-19 06:17:20 -0700

    Updated Minimum Size Subarray Sum problem

--- src/main/java/com/booknara/problem/array/MinimumSizeSubarraySum.java
+++ src/main/java/com/booknara/problem/array/MinimumSizeSubarraySum.java
@@ -1,11 +1,39 @@
 package com.booknara.problem.array;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 209. Minimum Size Subarray Sum (Medium)
  * https://leetcode.com/problems/minimum-size-subarray-sum/
  */
 public class MinimumSizeSubarraySum {
+    // 04/19/2020 version
     public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+        map.put(0, -1);
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum >= s) {
+                Map.Entry<Integer, Integer> ceiling = map.ceilingEntry(sum - s);
+                res = Math.min(i - ceiling.getValue(), res);
+            }
+
+            map.put(sum, i);
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    // 03/17/2020 version
+    public int minSubArrayLen1(int s, int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
