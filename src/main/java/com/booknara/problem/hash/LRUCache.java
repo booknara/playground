f2d4866afe8e73d110d179b1d016be9d9f87234b
Daehee Han <bookdori81@gmail.com>
2020-04-24 22:31:49 -0700

    Updated LRU Cache problem

--- src/main/java/com/booknara/problem/hash/LRUCache.java
+++ src/main/java/com/booknara/problem/hash/LRUCache.java
@@ -8,35 +8,37 @@ import java.util.Map;
  * https://leetcode.com/problems/lru-cache/
  */
 public class LRUCache {
-    private Map<Integer, Integer> map;
-    private int capacity;
-
+    Map<Integer, Integer> cache;
+    int capacity;
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        map = new LinkedHashMap<Integer, Integer>() {
+        cache = new LinkedHashMap<Integer, Integer>() {
             @Override
             protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > capacity;
+                boolean evict = size() > capacity;
+                if (evict) {
+                    //System.out.println("remove " + eldest.getKey());
+                }
+
+                return evict;
             }
         };
     }
 
     public int get(int key) {
-        if (map.containsKey(key)) {
-            int value = map.get(key);
-            map.remove(key);
-            map.put(key, value);
-            return value;
+        int res = cache.getOrDefault(key, -1);
+        if (res != -1) {
+            cache.remove(key);
+            cache.put(key, res);
         }
 
-        return -1;
+        return res;
     }
 
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
-            map.remove(key);
+        if (cache.containsKey(key)) {
+            cache.remove(key);
         }
-
-        map.put(key, value);
+        cache.put(key, value);
     }
 }
