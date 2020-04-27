7f6119a5d96ecde77d0f43d9c1fa22433d32b24a
Daehee Han <bookdori81@gmail.com>
2020-04-27 04:09:48 -0700

    Added Longest Increasing Subsequence problem

---
+++ src/main/java/com/booknara/problem/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+package com.booknara.problem.dp;
+
+/**
+ * 300. Longest Increasing Subsequence (Medium)
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ */
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+
+        int total = 0;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            total = Math.max(total, dp[i]);
+        }
+
+        return total;
+    }
+}
