5e1acb0424203c655a5ea51290c987de86f73941
Daehee Han <bookdori81@gmail.com>
2020-04-27 18:07:06 -0700

    Updated Contains Duplicate II problem

--- src/main/java/com/booknara/problem/hash/ContainsDuplicateII.java
+++ src/main/java/com/booknara/problem/hash/ContainsDuplicateII.java
@@ -1,6 +1,8 @@
 package com.booknara.problem.hash;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -8,7 +10,31 @@ import java.util.Set;
  * https://leetcode.com/problems/contains-duplicate-ii/
  */
 public class ContainsDuplicateII {
+    // 04/27/2020 version
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                int diff = i - map.get(num);
+                // at most k btw previous and current index
+                if (diff <= k) {
+                    return true;
+                }
+            }
+
+            map.put(num, i);
+        }
+
+        return false;
+    }
+
+    // 04/02/2020 version
+    public boolean containsNearbyDuplicate1(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return false;
         }
@@ -24,7 +50,7 @@ public class ContainsDuplicateII {
         return false;
     }
 
-    public boolean containsNearbyDuplicate1(int[] nums, int k) {
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return false;
         }
