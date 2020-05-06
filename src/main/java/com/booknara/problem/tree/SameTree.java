5cc77d4f90d81235a6e614d95dd9cc7d0b78766d
Daehee Han <bookdori81@gmail.com>
2020-05-05 21:32:26 -0700

    Updated Same Tree problem

--- src/main/java/com/booknara/problem/tree/SameTree.java
+++ src/main/java/com/booknara/problem/tree/SameTree.java
@@ -7,20 +7,13 @@ import com.booknara.problem.common.TreeNode;
  * https://leetcode.com/problems/same-tree/
  */
 public class SameTree {
+    // T:O(n, the number of nodes), S:O(n, the number of nodes in case of skewed tree)
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
 
-        return isSameTree(p.left, q.left)
-                && isSameTree(p.right, q.right);
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-
-
 }
