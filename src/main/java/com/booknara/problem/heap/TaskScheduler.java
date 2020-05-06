de3a8b370b3969f1ea66175d8755d9fe67f27227
Daehee Han <bookdori81@gmail.com>
2020-05-06 03:03:09 -0700

    Updated Task Scheduler problem

--- src/main/java/com/booknara/problem/heap/TaskScheduler.java
+++ src/main/java/com/booknara/problem/heap/TaskScheduler.java
@@ -7,7 +7,48 @@ import java.util.*;
  * https://leetcode.com/problems/task-scheduler/
  */
 public class TaskScheduler {
+    // 05/06/2020 version
+    // T: O(n * m), S:O(n), n: the number of tasks, m: interval
     public int leastInterval(char[] tasks, int n) {
+        if (tasks == null || tasks.length == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: tasks) {
+            int count = map.getOrDefault(c, 0);
+            map.put(c, count + 1);
+        }
+
+        // No need Task name.
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(map.values());
+
+        int time = 0;
+        while (!pq.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                if (!pq.isEmpty()) {
+                    int v = pq.poll();
+                    if (v != 1) {
+                        list.add(v - 1);
+                    }
+                } else if (list.isEmpty()) {
+                    // finish because pq is empty and no remaining task
+                    return time;
+                }
+
+                time++;
+            }
+
+            pq.addAll(list);
+        }
+
+        return time;
+    }
+
+    // 03/10/2020 version
+    public int leastInterval1(char[] tasks, int n) {
         if (tasks == null || tasks.length == 0) {
             return 0;
         }
