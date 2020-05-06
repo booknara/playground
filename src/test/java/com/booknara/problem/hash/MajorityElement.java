43c8406bf54eab86ba01a76e32c159ad7583d3d0
Daehee Han <bookdori81@gmail.com>
2020-05-06 00:40:28 -0700

    Added Majority Element problem

---
+++ src/test/java/com/booknara/problem/hash/MajorityElement.java
@@ -0,0 +1,55 @@
+package com.booknara.problem.hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 169. Majority Element (Easy)
+ * https://leetcode.com/problems/majority-element/
+ */
+public class MajorityElement {
+    // Boyer-Moore Voting algorithm
+    // T:O(n), S:O(1)
+    public int majorityElement(int[] nums) {
+        int c = 0, res = 0;
+        for (int n: nums) {
+            if (c == 0) {
+                res = n;
+                c++;
+            } else if (n == res) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+
+        return res;
+    }
+
+    // T:O(nlogn), S:O(n)
+    public int majorityElement1(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    // T:O(n), S:O(n)
+    public int majorityElement2(int[] nums) {
+        // input : the array is not empty
+        int res = 0;    // the majority element always exists
+
+        // Used map because input range is not defined (i.e. Integer.MIN_VALUE ~ Integer.MAX_VALUE)
+        Map<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        for (int n: nums) {
+            int count = map.getOrDefault(n, 0);
+            if (count + 1 > len / 2) {
+                res = n;
+                break;
+            }
+            map.put(n, count + 1);
+        }
+
+        return res;
+    }
+}
