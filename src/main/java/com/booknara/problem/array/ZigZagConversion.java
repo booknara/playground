b972afbc77744af954000aba1dbe9356f903699f
Daehee Han <bookdori81@gmail.com>
2020-05-05 22:40:18 -0700

    Updated ZigZag Conversion problem

--- src/main/java/com/booknara/problem/array/ZigZagConversion.java
+++ src/main/java/com/booknara/problem/array/ZigZagConversion.java
@@ -8,40 +8,41 @@ import java.util.List;
  * https://leetcode.com/problems/zigzag-conversion/
  */
 public class ZigZagConversion {
-    // Better readability
+    // T:O(n, the length of string), S:O(n, the length of string)
     public String convert(String s, int numRows) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-
-        if (numRows == 1) {
+        if (s == null || s.length() == 0 || numRows == 1) {
             return s;
         }
 
-        List<StringBuilder> list = new ArrayList<>();
+        // the number of rows StringBuilder needed
+        List<StringBuilder> builders = new ArrayList<>();
         int next = -1;
+        // cycle variable decides a dedicated StringBuilder
         int cycle = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (list.size() < numRows) {
-                list.add(new StringBuilder());
+            if (builders.size() == i) {
+                builders.add(new StringBuilder());
             }
 
-            list.get(cycle).append(s.charAt(i));
+            char c = s.charAt(i);
+            builders.get(cycle).append(c);
+            // System.out.println(cycle);
             if (cycle == 0 || cycle == numRows - 1) {
-                next = -next;
+                next *= -1;
             }
 
             cycle += next;
         }
 
+        // append builders
         StringBuilder res = new StringBuilder();
-        for (StringBuilder builder: list) {
-            //System.out.println(builder.toString());
-            res.append(builder.toString());
+        for (StringBuilder b: builders) {
+            res.append(b.toString());
         }
 
         return res.toString();
     }
+
     // #1 solution
     public String convert1(String s, int numRows) {
         if (s == null || s.length() == 0) {
