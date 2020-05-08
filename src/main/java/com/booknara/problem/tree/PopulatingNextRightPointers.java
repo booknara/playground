15a2876e9fe308a55e5cfc32c275d0e7c61b450c
Daehee Han <bookdori81@gmail.com>
2020-05-08 14:33:29 -0700

    Added Populating Next Right Pointers in Each Node problem

--- src/main/java/com/booknara/problem/tree/PopulatingNextRightPointers.java
+++ src/main/java/com/booknara/problem/tree/PopulatingNextRightPointers.java
@@ -8,25 +8,27 @@ import java.util.Queue;
  * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
  */
 public class PopulatingNextRightPointers {
-    // Without Space
+    // T:O(n), S:O(1)
     public Node connect(Node root) {
         if (root == null) {
             return null;
         }
 
-        Node leftMost = root;
-        while (leftMost.left != null) {
-            Node head = leftMost;
+        Node leftAtMost = root;
+        while (leftAtMost.left != null) {
+            Node head = leftAtMost;
             while (head != null) {
+                // left (same parent connection)
                 head.left.next = head.right;
                 if (head.next != null) {
+                    // right (different parent connection)
                     head.right.next = head.next.left;
                 }
 
                 head = head.next;
             }
-
-            leftMost = leftMost.left;
+            // move to next level
+            leftAtMost = leftAtMost.left;
         }
 
         return root;
@@ -66,6 +68,44 @@ public class PopulatingNextRightPointers {
 
     private boolean isPower(int num) {
         return num > 0 && (num & num - 1) == 0;
+    }
+
+    // T:O(n), S:O(logn)
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        Node prev = null;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node == null) {
+                if (prev != null) prev.next = null;
+                prev = null;
+                queue.offer(null);
+                if (queue.peek() == null) break;
+                else continue;
+            }
+
+            // System.out.println(node.val);
+            if (prev != null) {
+                prev.next = node;
+            }
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+
+            prev = node;
+        }
+
+        return root;
     }
 
     protected static class Node {
