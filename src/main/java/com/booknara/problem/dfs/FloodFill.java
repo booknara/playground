6bac1ea12882179676f7657098c145b55f642241
Daehee Han <bookdori81@gmail.com>
2020-05-11 01:39:05 -0700

    Updated Flood Fill problem

--- src/main/java/com/booknara/problem/dfs/FloodFill.java
+++ src/main/java/com/booknara/problem/dfs/FloodFill.java
@@ -1,44 +1,35 @@
 package com.booknara.problem.dfs;
 
-import com.booknara.problem.common.Pair;
-
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 733. Flood Fill (Easy)
  * https://leetcode.com/problems/flood-fill/
  */
 public class FloodFill {
-    // T:O(n^2), S:O(n^2, because of memoization)
+    // T:O(n^2), S:O(1)
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if (image == null || image.length == 0) {
+        if (image == null || image.length == 0 || image[sr][sc] == newColor) {
             return image;
         }
 
-        // No need to process if the existing and new color are same
-        if (image[sr][sc] != newColor) {
-            Set<Pair<Integer, Integer>> set = new HashSet<>();
-            dfs(image, sr, sc, newColor, set);
-        }
-
+        dfs(image, sr, sc, image[sr][sc], newColor);
         return image;
     }
 
-    private void dfs(int[][] image, int sr, int sc, int newColor, Set<Pair<Integer, Integer>> set) {
-        if (set.contains(new Pair(sr, sc))) return;
-        int prevColor = image[sr][sc];
+    private void dfs(int[][] image, int sr, int sc, int srcColor, int newColor) {
+        if (sr < 0 || sr == image.length || sc < 0 || sc == image[sr].length || image[sr][sc] != srcColor) {
+            return;
+        }
+
         image[sr][sc] = newColor;
 
-        set.add(new Pair(sr, sc));
         // 4 direction
         // top
-        if (sr - 1 >= 0 && prevColor == image[sr - 1][sc]) dfs(image, sr - 1, sc, newColor, set);
+        dfs(image, sr - 1, sc, srcColor, newColor);
         // bottom
-        if (sr + 1 < image.length && prevColor == image[sr + 1][sc]) dfs(image, sr + 1, sc, newColor, set);
+        dfs(image, sr + 1, sc, srcColor, newColor);
         // left
-        if (sc - 1 >= 0 && prevColor == image[sr][sc - 1]) dfs(image, sr, sc - 1, newColor, set);
+        dfs(image, sr, sc - 1, srcColor, newColor);
         // right
-        if (sc + 1 < image[sr].length && prevColor == image[sr][sc + 1]) dfs(image, sr, sc + 1, newColor, set);
+        dfs(image, sr, sc + 1, srcColor, newColor);
     }
 }
