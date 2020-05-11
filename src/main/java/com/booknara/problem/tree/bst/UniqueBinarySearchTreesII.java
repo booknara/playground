52aef998c6d3e5581acf92fbeae65db766c3285b
Daehee Han <bookdori81@gmail.com>
2020-05-11 01:00:22 -0700

    Updated Unique Binary Search Trees II problem - add memoization

--- src/main/java/com/booknara/problem/tree/bst/UniqueBinarySearchTreesII.java
+++ src/main/java/com/booknara/problem/tree/bst/UniqueBinarySearchTreesII.java
@@ -1,9 +1,12 @@
 package com.booknara.problem.tree.bst;
 
+import com.booknara.problem.common.Pair;
 import com.booknara.problem.common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 95. Unique Binary Search Trees II (Medium)
@@ -16,19 +19,25 @@ public class UniqueBinarySearchTreesII {
             return new ArrayList<>();
         }
 
-        return dfs(1, n);
+        Map<Pair<Integer, Integer>, List<TreeNode>> map = new HashMap<>();
+        return dfs(1, n, map);
     }
 
-    public List<TreeNode> dfs(int lower, int higher) {
+    public List<TreeNode> dfs(int lower, int higher, Map<Pair<Integer, Integer>, List<TreeNode>> map) {
         List<TreeNode> list = new ArrayList<>();
         if (lower > higher) {
             list.add(null);
             return list;
         }
 
+        Pair p = new Pair(lower, higher);
+        if (map.containsKey(p)) {
+            return map.get(p);
+        }
+
         for (int i = lower; i <= higher; i++) {
-            List<TreeNode> left = dfs(lower, i - 1);
-            List<TreeNode> right = dfs(i + 1, higher);
+            List<TreeNode> left = dfs(lower, i - 1, map);
+            List<TreeNode> right = dfs(i + 1, higher, map);
             for (TreeNode l: left) {
                 for (TreeNode r: right) {
                     TreeNode n = new TreeNode(i);
@@ -38,6 +47,8 @@ public class UniqueBinarySearchTreesII {
                 }
             }
         }
+
+        map.put(p, list);
 
         return list;
     }
