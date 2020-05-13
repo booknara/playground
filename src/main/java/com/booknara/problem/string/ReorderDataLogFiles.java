5953f3c02b4b7193110974e01a2e76990e810e61
Daehee Han <bookdori81@gmail.com>
2020-05-12 20:11:51 -0700

    Updated Reorder Data in Log Files problem

--- src/main/java/com/booknara/problem/string/ReorderDataLogFiles.java
+++ src/main/java/com/booknara/problem/string/ReorderDataLogFiles.java
@@ -1,63 +1,53 @@
 package com.booknara.problem.string;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * 937. Reorder Data in Log Files (Easy)
+ * https://leetcode.com/problems/reorder-data-in-log-files/
+ */
 public class ReorderDataLogFiles {
-    class Pair {
-        String id;
-        String log;
-        String origin;
-
-        public Pair(String id, String log, String origin) {
-            this.id = id;
-            this.log = log;
-            this.origin = origin;
-        }
-    }
-
+    // T:O(m*logm), m is the length of letter logs, S:O(n)
     public String[] reorderLogFiles(String[] logs) {
-        if (logs == null || logs.length == 0) {
-            return null;
+        if (logs == null || logs.length <= 1) {
+            return logs;
         }
 
-        PriorityQueue<Pair> p = new PriorityQueue<Pair>(new Comparator<Pair>() {
-            @Override
-            public int compare(Pair o1, Pair o2) {
-                if (Character.isDigit(o1.log.charAt(0))) {
-                    StringBuilder builder = new StringBuilder();
-                    for (char c: o1.log.toCharArray()) {
-                        builder.append(c + 'z');
-                    }
-                    o1.log = builder.toString();
-                }
-
-                if (Character.isDigit(o2.log.charAt(0))) {
-                    StringBuilder builder = new StringBuilder();
-                    for (char c: o2.log.toCharArray()) {
-                        builder.append(c + 'z');
-                    }
-                    o2.log = builder.toString();
-                }
-
-                return o1.log.compareTo(o2.log) == 0 ?
-                        o1.id.compareTo(o2.id) : o1.log.compareTo(o1.log);
+        List<String> letters = new ArrayList<>();
+        List<String> digits = new ArrayList<>();
+        for (String s: logs) {
+            int idx = s.indexOf(" ");
+            if (Character.isDigit(s.charAt(idx + 1))) {
+                digits.add(s);
+            } else {
+                letters.add(s);
             }
+        }
+
+        // Sorting rule
+        // 1. letter-log lexicographically ignoring the identifier, with the identifier in case of ties
+        // 2. digit-log with the existing order
+        Collections.sort(letters, (a, b) -> {
+            int x = a.substring(a.indexOf(" "))
+                    .compareTo(b.substring(b.indexOf(" ")));
+            if (x == 0) {
+                return a.substring(0,a.indexOf(" "))
+                        .compareTo(b.substring(0,b.indexOf(" ")));
+            }
+
+            return x;
         });
 
-        for (String s: logs) {
-            // System.out.println(s);
-            String[] pair = s.split(" ", 2);
-            //System.out.println(pair[0] + "," + pair[1]);
-            p.add(new Pair(pair[0], pair[1].replaceAll("\\s+", ""), s));
-        }
-
-        String[] ans = new String[logs.length];
         int i = 0;
-        while (!p.isEmpty()) {
-            ans[i++] = p.poll().origin;
+        for (String s: letters) {
+            logs[i++] = s;
+        }
+        for (String s: digits) {
+            logs[i++] = s;
         }
 
-        return ans;
+        return logs;
     }
 }
