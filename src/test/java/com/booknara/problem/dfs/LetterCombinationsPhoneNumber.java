543e946085f3697956a3bc69a45c205f123aa1f1
Daehee Han <bookdori81@gmail.com>
2020-05-14 01:55:59 -0700

    Updated Letter Combinations of a Phone Number problem

--- src/test/java/com/booknara/problem/dfs/LetterCombinationsPhoneNumber.java
+++ src/test/java/com/booknara/problem/dfs/LetterCombinationsPhoneNumber.java
@@ -1,53 +1,60 @@
 package com.booknara.problem.dfs;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Leet code : 17. Letter Combinations of a Phone Number (Medium)
- * Given a string containing digits from 2-9 inclusive,
- * return all possible letter combinations that the number could represent.
- *
- * A mapping of digit to letters (just like on the telephone buttons) is given below.
- * Note that 1 does not map to any letters.
- *
- * Example:
- * Input: "23"
- * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 17. Letter Combinations of a Phone Number (Medium)
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  */
 public class LetterCombinationsPhoneNumber {
-    Map<Character, List<Character>> map = new HashMap<>();
-
-    // Time: 3^N * 4^M(N: the length of strings, M: the length of strings, e.g. 7, 9)
+    // T: O(3^N * 4^M(N: the length of strings, M: the length of strings, e.g. 7, 9), S:O(3^N * 4^M)
     public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         if (digits == null || digits.length() == 0) {
-            return ans;
+            return res;
         }
 
-        map.put('2', Arrays.asList('a','b','c'));
-        map.put('3', Arrays.asList('d','e','f'));
-        map.put('4', Arrays.asList('g','h','i'));
-        map.put('5', Arrays.asList('j','k','l'));
-        map.put('6', Arrays.asList('m','n','o'));
-        map.put('7', Arrays.asList('p','q','r','s'));
-        map.put('8', Arrays.asList('t','u','v'));
-        map.put('9', Arrays.asList('w','x','y','z'));
-
-        dfs(digits, new StringBuilder(digits), digits.length(), 0, ans);
-        return ans;
+        StringBuilder builder = new StringBuilder();
+        dfs(digits, 0, builder, res);
+        return res;
     }
 
-    public void dfs(String input, StringBuilder source, int len, int i, List<String> ans) {
-        if (len == i) {
-            ans.add(source.toString());
+    private void dfs(String digits, int index, StringBuilder s, List<String> list) {
+        if (index == digits.length()) {
+            list.add(s.toString());
             return;
         }
 
-        List<Character> symbols = map.get(input.charAt(i));
-        for (char c: symbols) {
-            source.setCharAt(i, c);
-            dfs(input, source, len, i + 1, ans);
+        String letters = getLetter(digits.charAt(index));
+        //System.out.println(letters);
+        for (char c: letters.toCharArray()) {
+            StringBuilder builder = new StringBuilder(s.toString());
+            builder.append(c);
+            dfs(digits, index + 1, builder, list);
         }
     }
 
+    private String getLetter(char c) {
+        switch(c) {
+            case '2':
+                return "abc";
+            case '3':
+                return "def";
+            case '4':
+                return "ghi";
+            case '5':
+                return "jkl";
+            case '6':
+                return "mno";
+            case '7':
+                return "pqrs";
+            case '8':
+                return "tuv";
+            case '9':
+                return "wxyz";
+        }
+
+        return null;
+    }
 }
