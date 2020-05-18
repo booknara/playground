96fc179a117c15b673fc8221372c9591784979c3
Daehee Han <bookdori81@gmail.com>
2020-05-18 02:06:24 -0700

    Updated Permutation in String problem

--- src/main/java/com/booknara/problem/hash/PermutationInString.java
+++ src/main/java/com/booknara/problem/hash/PermutationInString.java
@@ -1,15 +1,16 @@
 package com.booknara.problem.hash;
 
+import java.util.Arrays;
+
 /**
  * 567. Permutation in String (Medium)
  * https://leetcode.com/problems/permutation-in-string/
  */
 public class PermutationInString {
+    // T:O(n), S:O(1)
     public boolean checkInclusion(String s1, String s2) {
         if (s1 == null || s1.length() == 0) return true;
         if (s2 == null || s2.length() == 0) return false;
-
-        if (s1.length() > s2.length()) return false;
 
         int[] s1Count = new int[26];
         int[] s2Count = new int[26];
@@ -18,22 +19,17 @@ public class PermutationInString {
         }
 
         for (int i = 0; i < s2.length(); i++) {
-            s2Count[s2.charAt(i) - 'a']++;
+            char c = s2.charAt(i);
+            s2Count[c - 'a']++;
 
             int r = i - s1.length();
             if (r >= 0) {
                 s2Count[s2.charAt(r) - 'a']--;
             }
 
-            boolean equal = true;
-            for (int j = 0; j < 26; j++) {
-                if (s1Count[j] != s2Count[j]) {
-                    equal = false;
-                    break;
-                }
+            if (Arrays.equals(s1Count, s2Count)) {
+                return true;
             }
-
-            if (equal) return true;
         }
 
         return false;
