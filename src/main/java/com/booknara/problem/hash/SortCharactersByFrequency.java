c84171e986a378f84b0976a60d0bdb88935d32bc
Daehee Han <bookdori81@gmail.com>
2020-05-22 01:07:50 -0700

    Updated Sort Characters By Frequency problem

--- src/main/java/com/booknara/problem/hash/SortCharactersByFrequency.java
+++ src/main/java/com/booknara/problem/hash/SortCharactersByFrequency.java
@@ -9,7 +9,34 @@ import java.util.PriorityQueue;
  * https://leetcode.com/problems/sort-characters-by-frequency/
  */
 public class SortCharactersByFrequency {
+    // Using bucket, T:O(n), S:O(1)
     public String frequencySort(String s) {
+        int[] bucket = new int[128];
+        for (char c: s.toCharArray()) {
+            bucket[c]++;
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((b1, b2) -> Integer.compare(b2[1], b1[1]));
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] != 0) {
+                pq.offer(new int[] {i, bucket[i]});
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (!pq.isEmpty()) {
+            int[] d = pq.poll();
+            for (int i = 0; i < d[1]; i++) {
+                int c = d[0];
+                builder.append((char) c);
+            }
+        }
+
+        return builder.toString();
+    }
+
+    // Using Map, T:O(n), S:O(1)
+    public String frequencySort1(String s) {
         if (s == null || s.length() == 0) {
             return "";
         }
