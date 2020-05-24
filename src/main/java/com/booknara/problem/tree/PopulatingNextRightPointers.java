d058f7569c6e78141e6ad24d14fb652a03faeea4
Daehee Han <bookdori81@gmail.com>
2020-05-24 02:51:03 -0700

    Updated Populating Next Right Pointers in Each Node problem

--- src/main/java/com/booknara/problem/tree/PopulatingNextRightPointers.java
+++ src/main/java/com/booknara/problem/tree/PopulatingNextRightPointers.java
@@ -16,18 +16,23 @@ public class PopulatingNextRightPointers {
 
         Node leftAtMost = root;
         while (leftAtMost.left != null) {
-            Node head = leftAtMost;
-            while (head != null) {
+            Node node = leftAtMost;
+            while (node != null) {
                 // left (same parent connection)
-                head.left.next = head.right;
-                if (head.next != null) {
-                    // right (different parent connection)
-                    head.right.next = head.next.left;
+                if (node.left != null) {
+                    node.left.next = node.right;
                 }
 
-                head = head.next;
+                // right (different parent connection)
+                if (node.right != null) {
+                    if (node.next != null) {
+                        node.right.next = node.next.left;
+                    }
+                }
+
+                node = node.next;
             }
-            // move to next level
+
             leftAtMost = leftAtMost.left;
         }
 
@@ -41,33 +46,22 @@ public class PopulatingNextRightPointers {
         }
 
         Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        Node node = root;
-        int count = 1;
+        queue.offer(root);
         while (!queue.isEmpty()) {
-            Node pre = node;
-            node = queue.poll();
-            if (isPower(count)) {
-                // System.out.println("count : " + count + " " + node.val);
-                pre.next = null;
-            } else {
-                pre.next = node;
-            }
+            Node node = queue.poll();
             if (node.left != null) {
                 queue.offer(node.left);
+                node.left.next = node.right;
             }
-
             if (node.right != null) {
                 queue.offer(node.right);
+                if (node.next != null) {
+                    node.right.next = node.next.left;
+                }
             }
-            count++;
         }
 
         return root;
-    }
-
-    private boolean isPower(int num) {
-        return num > 0 && (num & num - 1) == 0;
     }
 
     // T:O(n), S:O(logn)
