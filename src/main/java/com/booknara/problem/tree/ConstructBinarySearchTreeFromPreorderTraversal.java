68385069240ca99eb0e4de3b017958cf10948cda
Daehee Han <bookdori81@gmail.com>
2020-05-24 01:36:50 -0700

    Updated Construct Binary Search Tree from Preorder Traversal problem

--- src/main/java/com/booknara/problem/tree/ConstructBinarySearchTreeFromPreorderTraversal.java
+++ src/main/java/com/booknara/problem/tree/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -7,30 +7,32 @@ import com.booknara.problem.common.TreeNode;
  * https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
  */
 public class ConstructBinarySearchTreeFromPreorderTraversal {
-    int idx;
+    int index = 0;
+
+    // T:O(n), S:O(n, skewed tree)
     public TreeNode bstFromPreorder(int[] preorder) {
         if (preorder == null || preorder.length == 0) {
             return null;
         }
-        idx = 0;
+
         return dfs(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    private TreeNode dfs(int[] preorder, int start, int end) {
-        if (idx >= preorder.length) {
+    private TreeNode dfs(int[] preorder, int min, int max) {
+        if (index >= preorder.length) {
             return null;
         }
 
-        int val = preorder[idx];
-        if (val < start || val > end) {
+        int val = preorder[index];
+        if (val < min || val > max) {
             return null;
         }
 
-        idx++;
-        TreeNode root = new TreeNode(val);
-        root.left = dfs(preorder, start, root.val);
-        root.right = dfs(preorder, root.val, end);
+        TreeNode node = new TreeNode(val);
+        index++;
+        node.left = dfs(preorder, min, val);
+        node.right = dfs(preorder, val, max);
 
-        return root;
+        return node;
     }
 }
