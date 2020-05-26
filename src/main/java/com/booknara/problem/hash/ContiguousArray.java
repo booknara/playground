68a9fe3844bea2580382267e8439e820d0cd2329
Daehee Han <bookdori81@gmail.com>
2020-05-26 01:20:26 -0700

    Updated Contiguous Array problem

--- src/main/java/com/booknara/problem/hash/ContiguousArray.java
+++ src/main/java/com/booknara/problem/hash/ContiguousArray.java
@@ -1,4 +1,4 @@
-package com.booknara.problem.array;
+package com.booknara.problem.hash;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,21 +8,21 @@ import java.util.Map;
  * https://leetcode.com/problems/contiguous-array/
  */
 public class ContiguousArray {
+    // T:O(n), S:O(n)
     public int findMaxLength(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return 0;
 
         Map<Integer, Integer> map = new HashMap<>();
-        // For edge case: (0, 1), it doesn't get maxLen because '0' or '1' appears once.
-        map.put(0, -1);
-        int maxLen = 0;
         int count = 0;
+        int maxLen = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                count--;
-            } else {
-                count++;
+            count += nums[i] == 0 ? -1 : 1;
+
+            // count is the same of the count when starting
+            if (count == 0) {
+                maxLen = Math.max(maxLen, i + 1);
+                continue;
             }
 
             if (map.containsKey(count)) {
