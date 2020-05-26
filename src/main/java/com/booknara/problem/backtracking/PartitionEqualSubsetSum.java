dac8a741faaf1ff4a74dcb42da9adf4d9fd2ec25
Daehee Han <bookdori81@gmail.com>
2020-05-26 00:34:51 -0700

    Updated Partition Equal Subset Sum problem

--- src/main/java/com/booknara/problem/backtracking/PartitionEqualSubsetSum.java
+++ src/main/java/com/booknara/problem/backtracking/PartitionEqualSubsetSum.java
@@ -5,9 +5,34 @@ package com.booknara.problem.backtracking;
  * https://leetcode.com/problems/partition-equal-subset-sum/
  */
 public class PartitionEqualSubsetSum {
+    // T:O(n*m), S:O(n)
     public boolean canPartition(int[] nums) {
-        // TODO DP solution
-        return false;
+        // input check (non-empty from problem statement)
+        int sum = 0;
+        for (int n: nums) {
+            sum += n;
+        }
+
+        // sum is odd number which can't be divided by 2
+        if (sum % 2 != 0) return false;
+
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum + 1];
+        for (int i = 1; i < sum + 1; i++) {
+            dp[i] = false;
+        }
+        dp[0] = true;
+
+        for (int i = 1; i < nums.length + 1; i++) {
+            for (int j = sum; j >= 0; j--) {
+                if (j >= nums[i - 1]) {         // possible and picked, otherwise use remaining value
+                    dp[j] = dp[j] || dp[j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[sum];
     }
 
     // T:O(2^n), S:O(2^n)
@@ -43,4 +68,40 @@ public class PartitionEqualSubsetSum {
         // recovery
         return backtracking(nums, j, target);
     }
+
+    // T:O(n*m), S:O(n*m)
+    public boolean canPartition2(int[] nums) {
+        // input check (non-empty from problem statement)
+        int sum = 0;
+        for (int n: nums) {
+            sum += n;
+        }
+
+        // sum is odd number which can't be divided by 2
+        if (sum % 2 != 0) return false;
+
+        sum /= 2;
+
+        boolean[][] dp = new boolean[nums.length + 1][sum + 1];
+        for (int i = 1; i < nums.length + 1; i++) {
+            dp[i][0] = true;
+        }
+        // no needed
+        // for (int j = 1; j < sum + 1; j++) {
+        //     dp[0][j] = false;
+        // }
+        dp[0][0] = true;
+
+        for (int i = 1; i < nums.length + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                dp[i][j] = dp[i - 1][j];    // not picked
+                if (j >= nums[i - 1]) {         // possible and picked
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[nums.length][sum];
+    }
+
 }
