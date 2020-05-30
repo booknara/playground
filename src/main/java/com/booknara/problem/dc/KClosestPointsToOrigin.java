3529dac0b0ac636ba141833c6c264c2851d80455
Daehee Han <bookdori81@gmail.com>
2020-05-30 02:13:31 -0700

    Updated K Closest Points to Origin problem

--- src/main/java/com/booknara/problem/dc/KClosestPointsToOrigin.java
+++ src/main/java/com/booknara/problem/dc/KClosestPointsToOrigin.java
@@ -1,65 +1,57 @@
 package com.booknara.problem.dc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * 973. K Closest Points to Origin (Medium)
  * https://leetcode.com/problems/k-closest-points-to-origin/
  */
 public class KClosestPointsToOrigin {
-    // O(n), Quick search
+    // O(n^2), but best/average O(n) Quick select
     public int[][] kClosest(int[][] points, int K) {
-        List<int[]> res = new ArrayList<>();
-        if (points == null || points.length == 0) {
-            return res.toArray(new int[0][]);
-        }
-
-        if (points.length <= K) {
+        if (points == null || points.length <= K) {
             return points;
         }
 
-        int l = 0, h = points.length - 1;
-        while (l <= h) {
-            int partition = getPartition(points, l, h);
+        int left = 0, right = points.length;
+
+        while (left < right) {
+            int partition = getPartition(points, left, right, right - 1);
             if (partition == K) {
-                break;
+                return Arrays.copyOf(points, K);
             }
 
             if (partition < K) {
-                l = partition + 1;
+                left = partition;
             } else {
-                h = partition - 1;
+                right = partition;
             }
         }
 
-        return Arrays.copyOf(points, K);
+        return null;
     }
 
-    public int getPartition(int[][] points, int l, int h) {
-        int pivot = l;
-        int pivotDistance = getDistance(points, pivot);
-        while (l <= h) {
-            while (l <= h && getDistance(points, l) <= pivotDistance) {
-                l++;
+    public int getPartition(int[][] points, int left, int right, int pivot) {
+        int pDistance = getDistance(points, pivot);
+        int border = left;
+        for (int i = left; i < right; i++) {
+            if (getDistance(points, i) < pDistance) {
+                swap(points, i, border);
+                border++;
             }
-
-            while (l <= h && getDistance(points, h) > pivotDistance) {
-                h--;
-            }
-
-            if (l > h) {
-                break;
-            }
-
-            swap(points, l, h);
         }
 
-        swap(points, h, pivot);
+        // Move pivot to its final place
+        swap(points, border, pivot);
 
-        return h;
+        return border;
+    }
+
+    // Get distance
+    public int getDistance(int[][] points, int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
     }
 
     public void swap(int[][] points, int i, int j) {
@@ -68,40 +60,35 @@ public class KClosestPointsToOrigin {
         points[j] = temp;
     }
 
-    public int getDistance(int[][] points, int i) {
-        return (points[i][0] * points[i][0])
-                + (points[i][1] * points[i][1]);
-    }
-
-    // O(nlogn)
+    // T:O(nlogn), S:O(k)
     public int[][] kClosest1(int[][] points, int K) {
-        List<int[]> res = new ArrayList<>();
-        if (points == null || points.length == 0) {
-            return res.toArray(new int[0][]);
-        }
-
-        if (points.length <= K) {
+        if (points == null || points.length <= K) {
             return points;
         }
 
-        // Max heap
-        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> {
-            return (p2[0] * p2[0] + p2[1] * p2[1]) -
-                    (p1[0] * p1[0] + p1[1] * p1[1]);
+        // 1. max heap(descending) by x^2 + y^2
+        Queue<int[]> pq = new PriorityQueue<>((p1, p2) -> {
+            return Integer.compare(p2[0] * p2[0] + p2[1] * p2[1],
+                    p1[0] * p1[0] + p1[1] * p1[1]);
         });
 
+        // 2. iterate points array and input them into max heap
         for (int[] p: points) {
             pq.offer(p);
+            // Key part: if the number of heap size are greater than K, pop();
+            // maintain the number of K points
             if (pq.size() > K) {
-                int[] temp = pq.poll();
-                //System.out.println(temp[0] + "," + temp[1]);
+                pq.poll();
             }
         }
 
+        // 3. return the remaining data from max heap
+        int[][] res = new int[pq.size()][2];
+        int pos = 0;
         while (!pq.isEmpty()) {
-            res.add(pq.poll());
+            res[pos++] = pq.poll();
         }
 
-        return res.toArray(new int[pq.size()][]);
+        return res;
     }
 }
