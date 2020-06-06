d29af8e3d7d2a7766e1bed8f950a1369d4349374
Daehee Han <bookdori81@gmail.com>
2020-06-05 22:46:04 -0700

    Updated Largest Rectangle in Histogram problem

--- src/main/java/com/booknara/problem/stack/LargestRectangleHistogram.java
+++ src/main/java/com/booknara/problem/stack/LargestRectangleHistogram.java
@@ -2,26 +2,33 @@ package com.booknara.problem.stack;
 
 import java.util.Stack;
 
+/**
+ * 84. Largest Rectangle in Histogram (Hard)
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/
+ */
 public class LargestRectangleHistogram {
+    // T:O(n), S:O(n)
     public int largestRectangleArea(int[] heights) {
         if (heights == null || heights.length == 0) {
             return 0;
         }
 
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
         for (int i = 0; i < heights.length; i++) {
+            // checking descending(monotonic stack)
             while (stack.peek() != -1 && heights[i] < heights[stack.peek()]) {
-                int prev = stack.pop();
-                max = Math.max(max, (i - 1 - stack.peek()) * heights[prev]);
+                int h = stack.pop();
+                // formular -> current index - 1(index just before) - previos stack(lower) * height
+                max = Math.max(max, (i - 1 - stack.peek()) * heights[h]);
             }
             stack.push(i);
         }
 
         while (stack.peek() != -1) {
-            int prev = stack.pop();
-            max = Math.max(max, (heights.length - 1 - stack.peek()) * heights[prev]);
+            int h = stack.pop();
+            max = Math.max(max, (heights.length - 1 - stack.peek()) * heights[h]);
         }
 
         return max;
