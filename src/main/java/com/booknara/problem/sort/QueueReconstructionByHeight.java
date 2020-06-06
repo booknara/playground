3796159b4cb917e6f32f275b4a40166cd142c61a
Daehee Han <bookdori81@gmail.com>
2020-06-06 01:01:10 -0700

    Updated Queue Reconstruction by Height problem

--- src/main/java/com/booknara/problem/sort/QueueReconstructionByHeight.java
+++ src/main/java/com/booknara/problem/sort/QueueReconstructionByHeight.java
@@ -11,22 +11,27 @@ import java.util.List;
 public class QueueReconstructionByHeight {
     // T:O(n*logn), S:O(1)
     public int[][] reconstructQueue(int[][] people) {
-        // Rule #1 Order by the height because the tallest person should be front
-        // Rule #2 The height is same, order by k ascending (7, 0) , (7, 1), (7, 2)
-        Arrays.sort(people, (p1, p2) -> {
-            if (Integer.compare(p1[0], p2[0]) != 0) {
-                return Integer.compare(p2[0], p1[0]);
-            }
-
-            return Integer.compare(p1[1], p2[1]);
-        });
-        // [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
-        // And then k means index
-        List<int[]> list = new ArrayList<>();
-        for (int[] p: people) {
-            list.add(p[1], p);
+        if (people == null || people.length == 0) {
+            return people;
         }
 
-        return list.toArray(new int[0][]);
+        Arrays.sort(people, (p1, p2) -> {
+            // 1. height descending, 2. k ascending
+            if (p1[0] == p2[0]) {
+                // k ascending (e.g. [7, 0] , [7, 1], [7, 2])
+                return Integer.compare(p1[1], p2[1]);
+            }
+            // height descending
+            return Integer.compare(p2[0], p1[0]);
+        });
+
+        List<int[]> res = new ArrayList<>();
+        // [[7,0],[7,1],[6,1],[5,0],[5,2],[4,4]]
+        // And then k means index
+        for (int i = 0; i < people.length; i++) {
+            res.add(people[i][1], people[i]);
+        }
+
+        return res.toArray(new int[res.size()][2]);
     }
 }
