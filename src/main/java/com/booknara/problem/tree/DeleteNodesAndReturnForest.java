34b63e9e5bd4ad62e1cbff23c434a98dd568ccb3
Daehee Han <bookdori81@gmail.com>
2020-06-17 00:23:00 -0700

    Added Delete Nodes And Return Forest problem

--- src/main/java/com/booknara/problem/tree/DeleteNodesAndReturnForest.java
+++ src/main/java/com/booknara/problem/tree/DeleteNodesAndReturnForest.java
@@ -9,7 +9,55 @@ import java.util.*;
  * https://leetcode.com/problems/delete-nodes-and-return-forest/
  */
 public class DeleteNodesAndReturnForest {
+    // T:O(n), S:O(h), recursive
     public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<TreeNode> res = new LinkedList<>();
+        if (root == null) return res;
+
+        Set<Integer> set = new HashSet<>();
+        // to_delete to Set
+        for (int i: to_delete) {
+            set.add(i);
+        }
+
+        if (!set.contains(root.val)) {
+            res.add(root);
+        }
+
+        dfs(root, null, res, set);
+
+        return res;
+    }
+
+    public void dfs(TreeNode node, TreeNode parent, List<TreeNode> res, Set<Integer> set) {
+        if (node == null) return;
+
+        if (set.contains(node.val)) {
+            if (parent != null) {
+                // find node btw parent.left or parent.right to disconnect
+                if (parent.left != null && parent.left.val == node.val) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+
+            // add children nodes
+            if (node.left != null && !set.contains(node.left.val)) {
+                res.add(node.left);
+            }
+
+            if (node.right != null && !set.contains(node.right.val)) {
+                res.add(node.right);
+            }
+        }
+
+        dfs(node.left, node, res, set);
+        dfs(node.right, node, res, set);
+    }
+
+    // T:O(n), S:O(h), iterative
+    public List<TreeNode> delNodes1(TreeNode root, int[] to_delete) {
         List<TreeNode> res = new LinkedList<>();
         if (root == null) return res;
 
