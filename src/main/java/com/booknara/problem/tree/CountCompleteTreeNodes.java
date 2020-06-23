56bc1a388a58d482098f5fdd9694ae56623db878
Daehee Han <bookdori81@gmail.com>
2020-06-23 02:02:32 -0700

    Add Count Complete Tree Nodes problem

--- src/main/java/com/booknara/problem/tree/CountCompleteTreeNodes.java
+++ src/main/java/com/booknara/problem/tree/CountCompleteTreeNodes.java
@@ -10,6 +10,37 @@ import java.util.Queue;
  * https://leetcode.com/problems/count-complete-tree-nodes/
  */
 public class CountCompleteTreeNodes {
+    // T:O(logd*logd), S:O(logn)
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        int res = 0;
+        // 1. compare left side height + right side height
+        int leftHeight = getHeight(root, true);
+        int rightHeight = getHeight(root, false);
+        // 2. if the heights are same, it's a perfect tree
+        if (leftHeight == rightHeight) {
+            // the number of the nodes are 2^(height + 1) - 1
+            return (1 << leftHeight) - 1;
+        }
+        // 3. if the heights are different, plus 1(root) and count left/right sides
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public int getHeight(TreeNode node, boolean left) {
+        int res = 0;
+        while (node != null) {
+            res++;
+            if (left) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        return res;
+    }
+
     // T:O(n), S:O(logn)
     public int countNodes1(TreeNode root) {
         if (root == null) return 0;
