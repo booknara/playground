be67fc852fa5fa49794452a2f9b5c7bb57d75321
Daehee Han <bookdori81@gmail.com>
2020-06-29 00:12:37 -0700

    Add Reconstruct Itinerary problem

--- src/main/java/com/booknara/problem/graph/ReconstructItinerary.java
+++ src/main/java/com/booknara/problem/graph/ReconstructItinerary.java
@@ -7,37 +7,36 @@ import java.util.*;
  * https://leetcode.com/problems/reconstruct-itinerary/
  */
 public class ReconstructItinerary {
-    LinkedList<String> ans = new LinkedList<>();
-    Map<String, PriorityQueue<String>> map = new HashMap<>();
-
+    // input : list of list string(from, to)
+    // output : list (itinerary)
+    // T:O(E*logE/V), S:O(E + V)
     public List<String> findItinerary(List<List<String>> tickets) {
-        if (tickets == null || tickets.size() == 0) {
-            return ans;
-        }
-
+        // input check : ticket is not null or empty
+        // 1. create map for ticket
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
         for (List<String> t: tickets) {
-            if (!map.containsKey(t.get(0))) {
-                map.put(t.get(0), new PriorityQueue<>());
-            }
-
-            PriorityQueue<String> set = map.get(t.get(0));
-            set.add(t.get(1));
-
-            map.put(t.get(0), set);
+            String from = t.get(0);
+            String to = t.get(1);
+            PriorityQueue<String> pq = map.getOrDefault(from, new PriorityQueue<>());
+            pq.add(to);
+            map.put(from, pq);
         }
 
-        dfs("JFK");
+        LinkedList<String> res = new LinkedList<>();
+        dfs("JFK", map, res);
 
-        return ans;
+        return res;
     }
 
-    public void dfs(String start) {
+    // Bottom up dfs
+    public void dfs(String start, Map<String, PriorityQueue<String>> map, LinkedList<String> res) {
         PriorityQueue<String> pq = map.get(start);
 
         while (pq != null && !pq.isEmpty()) {
-            dfs(pq.poll());
+            dfs(pq.poll(), map, res);
         }
 
-        ans.addFirst(start);
+        // if there is no flight heading to, add string here
+        res.addFirst(start);
     }
 }
