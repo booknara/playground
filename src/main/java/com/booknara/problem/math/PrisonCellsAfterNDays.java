f02995b5c1bb2fd188536eb4fc026ed7c2b453b0
Daehee Han <bookdori81@gmail.com>
2020-07-04 23:02:55 -0700

    Add Prison Cells After N Days problem

--- src/main/java/com/booknara/problem/math/PrisonCellsAfterNDays.java
+++ src/main/java/com/booknara/problem/math/PrisonCellsAfterNDays.java
@@ -8,35 +8,48 @@ import java.util.Map;
  * https://leetcode.com/problems/prison-cells-after-n-days/
  */
 public class PrisonCellsAfterNDays {
-    // Time complexity O(2^n), n: the length of input array
+    // T:O(K * min(n, 2^K), K: the number of cells, n: n days
     public int[] prisonAfterNDays(int[] cells, int N) {
-        if (cells == null || cells.length == 0 || N == 0) {
-            return cells;
+        // input check 1 <= N <= 10^9
+        StringBuilder builder = new StringBuilder();
+        for (int c: cells) {
+            builder.append(c);
         }
 
         Map<String, Integer> seen = new HashMap<>();
-        StringBuilder origin = new StringBuilder();
-        for (int c: cells) {
-            origin.append(c);
-        }
-
-        while (N > 0) {
-            //System.out.println(N);
-            if (seen.containsKey(origin.toString())) {
-                N %= seen.get(origin.toString()) - N;
+        for (int i = N; i >= 1; i--) {
+            if (seen.containsKey(builder.toString())) {
+                i %= seen.get(builder.toString()) - i;
+                //System.out.println("hit : " + i);
             }
 
-            seen.put(origin.toString(), N);
-            if (N >= 1) {
-                N--;
-                origin = updateCell(origin);
+            // edge case: the result of modulus is 0
+            // e.g. [1,0,0,0,1,0,0,1], 99
+            if (i == 0) {
+                break;
             }
+            seen.put(builder.toString(), i);
+            builder = updateCells(builder);
         }
 
-        return getCellArray(origin);
+        return getCells(builder);
     }
 
-    private int[] getCellArray(StringBuilder builder) {
+    public StringBuilder updateCells(StringBuilder builder) {
+        StringBuilder res = new StringBuilder();
+        String s = builder.toString();
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || i == s.length() - 1) {
+                res.append("0");
+            } else {
+                res.append(s.charAt(i - 1) == s.charAt(i + 1) ? "1" : "0");
+            }
+        }
+
+        return res;
+    }
+
+    public int[] getCells(StringBuilder builder) {
         int[] res = new int[builder.length()];
         for (int i = 0; i < builder.length(); i++) {
             res[i] = Character.getNumericValue(builder.charAt(i));
@@ -45,71 +58,29 @@ public class PrisonCellsAfterNDays {
         return res;
     }
 
-    private StringBuilder updateCell(StringBuilder source) {
-        StringBuilder target = new StringBuilder();
-        int len = source.length();
-        for (int i = 0; i < len; i++) {
-            if (i == 0 || i == len - 1) {
-                target.append('0');
-            } else {
-                target.append(getCell(source, i));
-            }
-        }
-
-        return target;
-    }
-
-    private char getCell(StringBuilder builder, int i) {
-        char pre = builder.charAt(i - 1);
-        char next = builder.charAt(i + 1);
-        if ((pre == '0' && next == '0') || (pre == '1' && next == '1')) {
-            return '1';
-        }
-
-        return '0';
-    }
-
-    // Straight forward way(Time exceed if input is
+    // TLS
     // [1,0,0,1,0,0,1,0]
     // 1000000000
     public int[] prisonAfterNDays1(int[] cells, int N) {
-        if (cells == null || cells.length == 0 || N == 0) {
-            return cells;
+        // input check 1 <= N <= 10^9
+        int[][] temp = new int[2][cells.length];
+        // System.arraycopy(cells, 0, temp[0], 0, cells.length); // equivalent
+        for (int i = 0; i < cells.length; i++) {
+            temp[0][i] = cells[i];
         }
-
-        int[] cells2 = new int[cells.length];
-        for (int i = 1; i <= N; i++) {
-            if (i % 2 == 1) {
-                // cells -> cells2
-                updateCell(cells, cells2);
-            } else {
-                // cells2 -> cells
-                updateCell(cells2, cells);
+        int toggle = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 0 || j == 7) {
+                    temp[toggle][j] = 0;
+                } else {
+                    temp[toggle][j] = temp[toggle ^ 1][j - 1] == temp[toggle ^ 1][j + 1] ? 1 : 0;
+                }
             }
+
+            toggle ^= 1;
         }
 
-        return N % 2 == 1 ? cells2 : cells;
-    }
-
-    private void updateCell(int[] source, int[] target) {
-        int len = source.length;
-
-        for (int i = 0; i < len; i++) {
-            if (i == 0 || i == len - 1) {
-                target[i] = 0;
-            } else {
-                target[i] = getCell(source, i);
-            }
-        }
-    }
-
-    private int getCell(int[] cells, int i) {
-        int pre = cells[i - 1];
-        int next = cells[i + 1];
-        if ((pre == 0 && next == 0) || (pre == 1 && next == 1)) {
-            return 1;
-        }
-
-        return 0;
+        return temp[toggle ^ 1];
     }
 }
