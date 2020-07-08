8be93d1c7bfc1439f3066bc97e46ce5d08d3c864
Daehee Han <bookdori81@gmail.com>
2020-07-08 01:18:01 -0700

    Add 3Sum problem

--- src/main/java/com/booknara/problem/twopointers/ThreeSum.java
+++ src/main/java/com/booknara/problem/twopointers/ThreeSum.java
@@ -5,37 +5,40 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Leet code: 15. 3Sum (Medium)
+ * 15. 3Sum (Medium)
  * https://leetcode.com/problems/3sum/
  */
 public class ThreeSum {
+    // T:O(n^2), S:O(1)
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length < 3) {
-            return res;
-        }
+        // input check
+        if (nums == null || nums.length < 3) return res;
 
+        // sort (ascending)
         Arrays.sort(nums);
-        // a + b + c = 0 -> a + b = -c
+
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i - 1] == nums[i]) {
+                // prevent getting duplicated triplets
                 continue;
             }
-
-            // left = i + 1 for getting rid of duplicate triplets
+            int selected = nums[i];
             int l = i + 1, r = nums.length - 1;
             while (l < r) {
-                if (nums[l] + nums[r] == -nums[i]) {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    // to remove duplicate triplets
+                int sum = selected + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.add(Arrays.asList(selected, nums[l], nums[r]));
                     while (l < r && nums[l] == nums[l + 1]) l++;
-                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
 
                     l++;
                     r--;
-                } else if (nums[l] + nums[r] < -nums[i]) {
+                } else if (sum < 0) {
+                    // need to increase
                     l++;
                 } else {
+                    // need to decrease
                     r--;
                 }
             }
