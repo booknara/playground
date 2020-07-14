9b289be0a39d4261156852faed6f5552bbcd026d
Daehee Han <bookdori81@gmail.com>
2020-07-14 08:17:33 -0700

    Add Angle Between Hands of a Clock problem

--- src/main/java/com/booknara/problem/math/AngleBetweenHandsOfClock.java
+++ src/main/java/com/booknara/problem/math/AngleBetweenHandsOfClock.java
@@ -8,7 +8,9 @@ public class AngleBetweenHandsOfClock {
     // T:O(1), S:O(1)
     public double angleClock(int hour, int minutes) {
         if (hour == 12) hour = 0;
+        // 1 minute -> 6.0
         double mAngle = minutes * 6.0;
+        // 1 hour -> 30.0 + 0.5 * minutes
         double hAngle = (30 * hour) + (0.5 * minutes);
 
         double res = Math.abs(mAngle - hAngle);
