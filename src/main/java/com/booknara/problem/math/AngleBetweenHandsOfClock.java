fbf41ff4380de6bd49a1c4da425029fbe7599bf3
Daehee Han <bookdori81@gmail.com>
2020-07-14 08:16:06 -0700

    Add Angle Between Hands of a Clock problem

---
+++ src/main/java/com/booknara/problem/math/AngleBetweenHandsOfClock.java
@@ -0,0 +1,18 @@
+package com.booknara.problem.math;
+
+/**
+ * 1344. Angle Between Hands of a Clock (Medium)
+ * https://leetcode.com/problems/angle-between-hands-of-a-clock/
+ */
+public class AngleBetweenHandsOfClock {
+    // T:O(1), S:O(1)
+    public double angleClock(int hour, int minutes) {
+        if (hour == 12) hour = 0;
+        double mAngle = minutes * 6.0;
+        double hAngle = (30 * hour) + (0.5 * minutes);
+
+        double res = Math.abs(mAngle - hAngle);
+
+        return res <= 180.0 ? res : 360.0 - res;
+    }
+}
