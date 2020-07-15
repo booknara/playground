5760b0430ceec01277673e3a236de51f6782555b
Daehee Han <bookdori81@gmail.com>
2020-07-14 19:20:39 -0700

    Add Rotate List problem

--- src/main/java/com/booknara/problem/list/RotateList.java
+++ src/main/java/com/booknara/problem/list/RotateList.java
@@ -7,7 +7,47 @@ import com.booknara.problem.common.ListNode;
  * https://leetcode.com/problems/rotate-list/
  */
 public class RotateList {
+    // T:O(n), S:O(1)
     public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+
+        if (k == 0) return head;
+
+        int size = 0;
+        ListNode cur = head;
+        ListNode tail = null;
+        while (cur != null) {
+            size++;
+            tail = cur;
+            cur = cur.next;
+        }
+
+        // System.out.println(size);
+        // System.out.println(tail.val);
+        k = k % size;
+        // edge case (singl element)
+        if (size == 1) return head;
+        // edge case rotate all the element
+        if (k == 0) return head;
+
+        int n = size - k;
+
+        cur = head;
+        ListNode prev = null;
+        while (n > 0) {
+            //System.out.println(cur.val);
+            prev = cur;
+            cur = cur.next;
+            n--;
+        }
+
+        prev.next = null;
+        tail.next = head;
+
+        return cur;
+    }
+
+    public ListNode rotateRight1(ListNode head, int k) {
         if (head == null || head.next == null) {
             return head;
         }
