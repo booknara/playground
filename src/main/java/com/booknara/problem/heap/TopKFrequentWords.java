beb1a7b95dfd7b277fedf1f9029823bdcc72a975
Daehee Han <bookdori81@gmail.com>
2020-07-17 02:21:14 -0700

    Add Top K Frequent Words problem

--- src/main/java/com/booknara/problem/heap/TopKFrequentWords.java
+++ src/main/java/com/booknara/problem/heap/TopKFrequentWords.java
@@ -7,40 +7,40 @@ import java.util.*;
  * https://leetcode.com/problems/top-k-frequent-words/
  */
 public class TopKFrequentWords {
+    // T:O(nlogk), S:(n)
     public List<String> topKFrequent(String[] words, int k) {
-        List<String> ans = new ArrayList<>();
-        if (words == null || words.length == 0 || k == 0) {
-            return ans;
-        }
+        LinkedList<String> res = new LinkedList<>();
+        if (words == null || words.length == 0) return res;
 
+        // get words frequency
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            //System.out.println(words[i]);
-            int count = map.getOrDefault(words[i], 0);
-            map.put(words[i], count + 1);
+        for (String w: words) {
+            int c = map.getOrDefault(w, 0);
+            map.put(w, c + 1);
         }
 
-        // Min Heap
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> {
-            if (!a.getValue().equals(b.getValue())) {
-                return a.getValue() > b.getValue() ? 1 : -1;
+        // Min heap
+        Queue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((e1, e2) -> {
+            if (e1.getValue() == e2.getValue()) {
+                // Note: reverse alphabetical order because for result (min heap & reverse order)
+                return e2.getKey().compareTo(e1.getKey());
             }
 
-            return b.getKey().compareTo(a.getKey());
+            return e1.getValue() - e2.getValue();
         });
 
         for (Map.Entry<String, Integer> entry: map.entrySet()) {
-            pq.add(entry);
+            pq.offer(entry);
             if (pq.size() > k) {
                 pq.poll();
             }
         }
 
+        // return the result by frequency from highest to lowest
         while (!pq.isEmpty()) {
-            ans.add(pq.poll().getKey());
+            res.addFirst(pq.poll().getKey());
         }
 
-        Collections.reverse(ans);
-        return ans;
+        return res;
     }
 }
