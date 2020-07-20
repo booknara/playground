7c30b15c689f39d91df1109ac8401299fbea00a1
Daehee Han <bookdori81@gmail.com>
2020-07-20 01:48:47 -0700

    Add Find Largest Value in Each Tree Row problem

---
+++ src/main/java/com/booknara/problem/tree/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,44 @@
+package com.booknara.problem.tree;
+
+import com.booknara.problem.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 515. Find Largest Value in Each Tree Row (Medium)
+ * https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+ */
+public class FindLargestValueInEachTreeRow {
+    // T:O(n), S:O(n/2)
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i= 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            res.add(max);
+        }
+
+        return res;
+    }
+}
