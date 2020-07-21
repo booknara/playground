3e976169627de6ae49de03878da67a57077998d5
Daehee Han <bookdori81@gmail.com>
2020-07-21 00:40:21 -0700

    Add Word Search problem

--- src/main/java/com/booknara/problem/backtracking/WordSearch.java
+++ src/main/java/com/booknara/problem/backtracking/WordSearch.java
@@ -1,17 +1,17 @@
 package com.booknara.problem.backtracking;
 
+/**
+ * 79. Word Search (Medium)
+ * https://leetcode.com/problems/word-search/
+ */
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
-        if (board == null) {
-            return false;
-        }
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     // System.out.println(board[i][j]);
                     // Early termination
-                    if (dfs(board, 0, i, j, word)) return true;
+                    if (dfs(board, i, j, word, 0)) return true;
                 }
             }
         }
@@ -19,11 +19,12 @@ public class WordSearch {
         return false;
     }
 
-    public boolean dfs(char[][] board, int index, int i, int j, String word) {
+    public boolean dfs(char[][] board, int i, int j, String word, int index) {
         if (index == word.length()) {
             return true;
         }
 
+        // out of range
         if (i < 0 || j < 0 || i == board.length || j == board[i].length) {
             return false;
         }
@@ -35,10 +36,10 @@ public class WordSearch {
 
         // character -> Non character because it makes false when the following Character comparision happens(visited trick)
         board[i][j] ^= 256;
-        boolean found = (dfs(board, index + 1, i - 1, j, word)
-                || dfs(board, index + 1, i + 1, j, word)
-                || dfs(board, index + 1, i, j - 1, word)
-                || dfs(board, index + 1, i, j + 1, word));
+        boolean found = (dfs(board,i - 1, j, word, index + 1)
+                || dfs(board, i + 1, j, word, index + 1)
+                || dfs(board, i, j - 1, word, index + 1)
+                || dfs(board, i, j + 1, word, index + 1));
         // Non character -> character
         board[i][j] ^= 256;
 
