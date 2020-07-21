16b12a3a87d47ad0c80a0fa2c20cf8da977f7793
Daehee Han <bookdori81@gmail.com>
2020-07-21 02:27:29 -0700

    Add Maximum Subarray problem

--- src/main/java/com/booknara/problem/dp/MaxSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxSubArray.java
@@ -1,11 +1,11 @@
 package com.booknara.problem.dp;
 
 /**
- * Leet code: 53 (Easy)
+ * 53. Maximum Subarray (Easy)
  * https://leetcode.com/problems/maximum-subarray/
  */
 public class MaxSubArray {
-    // Time complexity: O(n), Space complexity: O(1)
+    // T:O(n), S:O(1)
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -19,25 +19,5 @@ public class MaxSubArray {
         }
 
         return total;
-    }
-
-    // Time complexity: O(n), Space complexity: O(n)
-    public int maxSubArray1(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int[] sum = new int[nums.length];
-        sum[0] = nums[0];
-        int max = sum[0];
-        for (int i = 1; i < nums.length; i++) {
-            sum[i] = Math.max(nums[i], sum[i - 1] + nums[i]);
-            max = Math.max(max, sum[i]);
-        }
-
-        return max;
     }
 }
