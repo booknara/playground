a4dc8aa1733c1260ca861188df4a736ae301b1bf
Daehee Han <bookdori81@gmail.com>
2020-07-21 02:16:40 -0700

    Add Maximum Product Subarray problem

--- src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
@@ -1,39 +1,31 @@
 package com.booknara.problem.dp;
 
 /**
- * Leet code : 152 (Medium)
- * Given an integer array nums,
- * find the contiguous subarray within an array (containing at least one number) which has the largest product.
- * Input: [2,3,-2,4]
- * Output: 6
- * Explanation: [2,3] has the largest product 6.
+ * 152. Maximum Product Subarray (Medium)
+ * https://leetcode.com/problems/maximum-product-subarray/
  */
 public class MaxProductSubArray {
-    // Time complexity: O(n), Space complexity: O(n)
+    // T:O(n), S:O(1)
     public int maxProduct(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
 
-        int totalMax;
-        int negativeMax;
-        int positiveMax;
+        int totalMax, positiveMax, negativeMax;
         if (nums[0] < 0) {
             negativeMax = nums[0];
             positiveMax = 0;
-            totalMax = positiveMax;
         } else {
             negativeMax = 0;
             positiveMax = nums[0];
-            totalMax = nums[0];
         }
 
+        totalMax = Math.max(positiveMax, negativeMax);
         for (int i = 1; i < nums.length; i++) {
-            // nums[i], positiveMax * nums[i], negativeMax * nums[i]
             int temp = positiveMax;
-            positiveMax = Math.max(Math.max(nums[i], positiveMax * nums[i]), negativeMax * nums[i]);
-            negativeMax = Math.min(Math.min(nums[i], temp * nums[i]), negativeMax * nums[i]);
-            totalMax = Math.max(Math.max(totalMax, positiveMax), negativeMax);
+            positiveMax = Math.max(nums[i], Math.max(temp * nums[i], negativeMax * nums[i]));
+            negativeMax = Math.min(nums[i], Math.min(temp * nums[i], negativeMax * nums[i]));
+
+            totalMax = Math.max(totalMax, positiveMax);
         }
 
         return totalMax;
