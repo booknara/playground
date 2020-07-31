f5984ba847e0cddba42bfd1496c42f0309f16652
Daehee Han <bookdori81@gmail.com>
2020-07-31 15:05:07 -0700

    Add Min Cost Climbing Stairs problem

--- src/main/java/com/booknara/problem/dp/MinCostClimbStairs.java
+++ src/main/java/com/booknara/problem/dp/MinCostClimbStairs.java
@@ -4,21 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Leet code : 746 (Easy)
- * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
- * Once you pay the cost, you can either climb one or two steps.
- * You need to find minimum cost to reach the top of the floor,
- * and you can either start from the step with index 0, or the step with index 1.
- *
- * Input: cost = [10, 15, 20]
- * Output: 15
- * Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+ * 746. Min Cost Climbing Stairs (Easy)
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
  */
 public class MinCostClimbStairs {
-    private Map<Integer, Integer> memo = new HashMap<>();
+    // T:O(n), S:O(1), Bottom-up DP
+    public int minCostClimbingStairs(int[] cost) {
+        int pp = cost[0];
+        int p = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            int temp = Math.min(pp, p) + cost[i];
+            pp = p;
+            p = temp;
+        }
+
+        return Math.min(pp, p);
+    }
+
+    // T:O(n), S:O(n), Bottom-up DP
+    public int minCostClimbingStairs1(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
+        }
+
+        return Math.min(dp[cost.length - 2], dp[cost.length - 1]);
+    }
 
     // Time complexity: O(n), Space complexity: O(n), Top down DP + memoization
-    public int minCostClimbingStairs(int[] cost) {
+    private Map<Integer, Integer> memo = new HashMap<>();
+    public int minCostClimbingStairs2(int[] cost) {
         if (cost == null || cost.length == 0) {
             return 0;
         }
@@ -48,56 +65,5 @@ public class MinCostClimbStairs {
         memo.put(index, value);
 
         return value;
-    }
-
-    // Time complexity: O(n), Space complexity: O(n), Bottom-up DP
-    public int optimizedMinCostClimbingStairs(int[] cost) {
-        if (cost == null || cost.length == 0) {
-            return 0;
-        }
-
-        if (cost.length == 1) {
-            return cost[0];
-        }
-
-        if (cost.length == 2) {
-            return cost[0] < cost[1] ? cost[0] : cost[1];
-        }
-
-        int[] min = new int[cost.length];
-        min[0] = cost[0];
-        min[1] = cost[1];
-        for (int i = 2; i < cost.length; i++) {
-            // F(i) = min(F(i-1), F(i-2)) + cost[i]
-            min[i] = Math.min(min[i - 1], min[i - 2]) + cost[i];
-        }
-
-        return min[cost.length - 2] < min[cost.length - 1] ?
-                min[cost.length - 2] : min[cost.length - 1];
-    }
-
-    // Time complexity: O(n), Space complexity: O(1), Bottom-up DP
-    public int optimizedMinCostClimbingStairs2(int[] cost) {
-        if (cost == null || cost.length == 0) {
-            return 0;
-        }
-
-        if (cost.length == 1) {
-            return cost[0];
-        }
-
-        if (cost.length == 2) {
-            return cost[0] < cost[1] ? cost[0] : cost[1];
-        }
-
-        int ppVal = cost[0];
-        int pVal = cost[1];
-        for (int i = 2; i < cost.length; i++) {
-            int current = Math.min(pVal, ppVal) + cost[i];
-            ppVal = pVal;
-            pVal = current;
-        }
-
-        return ppVal < pVal ? ppVal : pVal;
     }
 }
