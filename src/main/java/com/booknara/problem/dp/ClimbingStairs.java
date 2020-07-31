1cff19ea6c171b20980aae4eb8c27eff975a64b6
Daehee Han <bookdori81@gmail.com>
2020-07-31 12:31:33 -0700

    Add Climbing Stairs problem

--- src/main/java/com/booknara/problem/dp/ClimbingStairs.java
+++ src/main/java/com/booknara/problem/dp/ClimbingStairs.java
@@ -7,19 +7,22 @@ package com.booknara.problem.dp;
 public class ClimbingStairs {
     // T:O(n), S:O(1)
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 0;
-        }
+        if (n <= 1) return n;
 
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= 2; j++) {
-                dp[i] += dp[i - j];
-            }
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         return dp[n];
     }
 }
+// dp[0] = 1
+// dp[1] = 1
+// dp[2] = dp[1] + dp[0] = 2
+// dp[3] = dp[1] + dp[2] = 3
+// dp[i] = dp[i-1] + dp[i - 2]
+// ...
+// dp[n] = dp[n - 1] + dp[n - 2]
