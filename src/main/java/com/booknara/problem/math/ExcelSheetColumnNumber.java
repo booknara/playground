18361d5a335abe2ebebb0b112f923741d44faf3d
Daehee Han <bookdori81@gmail.com>
2020-08-10 23:15:34 -0700

    Add Excel Sheet Column Number problem

---
+++ src/main/java/com/booknara/problem/math/ExcelSheetColumnNumber.java
@@ -0,0 +1,18 @@
+package com.booknara.problem.math;
+
+/**
+ * 171. Excel Sheet Column Number (Easy)
+ * https://leetcode.com/problems/excel-sheet-column-number/
+ */
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        // input check, the len of S is bigger than 1
+        int res = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            res += Math.pow(26, len - 1 - i) * (s.charAt(i) - 'A' + 1);
+        }
+
+        return res;
+    }
+}
