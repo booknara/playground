97b2e10dc0ab62a610d3a658324c0f4b60fbe5c6
Daehee Han <bookdori81@gmail.com>
2020-08-13 23:19:19 -0700

    Add Number of Connected Components in an Undirected Graph problem

---
+++ src/main/java/com/booknara/problem/graph/NumberOfConnectedComponentsInUndirectedGraph.java
@@ -0,0 +1,40 @@
+package com.booknara.problem.graph;
+
+/**
+ * 323. Number of Connected Components in an Undirected Graph (Medium)
+ * https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+ */
+public class NumberOfConnectedComponentsInUndirectedGraph {
+    public int countComponents(int n, int[][] edges) {
+        // input check
+        if (edges == null || edges.length == 0) return n;
+
+        int[] roots = new int[n];
+        for (int i = 0; i < n; i++) {
+            roots[i] = i;
+        }
+
+        int res = n;
+        for (int[] e: edges) {
+            int root1 = findRoot(roots, e[0]);
+            int root2 = findRoot(roots, e[1]);
+            // union
+            if (root1 != root2) {
+                roots[root1] = root2;
+                res--;
+            }
+        }
+
+        return res;
+    }
+
+    public int findRoot(int[] roots, int id) {
+        // find
+        while (roots[id] != id) {
+            roots[id] = roots[roots[id]];   // path compression
+            id = roots[id];
+        }
+
+        return id;
+    }
+}
