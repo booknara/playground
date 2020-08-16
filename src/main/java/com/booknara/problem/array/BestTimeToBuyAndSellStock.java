63a6d879af6f90157742c1f3715d4e739d4eed9c
Daehee Han <bookdori81@gmail.com>
2020-08-16 02:35:47 -0700

    Add Best Time to Buy and Sell Stock problem

---
+++ src/main/java/com/booknara/problem/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package com.booknara.problem.array;
+
+/**
+ * 121. Best Time to Buy and Sell Stock (Easy)
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+public class BestTimeToBuyAndSellStock {
+    // T:O(n), S:O(1)
+    public int maxProfit(int[] prices) {
+        // input check
+        if (prices == null || prices.length == 0) return 0;
+        // input : all the values of prices are non-negative
+
+        int buy = Integer.MIN_VALUE, sell = 0;
+        for (int p:prices) {
+            buy = Math.max(buy, -p);
+            sell = Math.max(sell, buy + p);
+        }
+
+        return sell;
+    }
+}
