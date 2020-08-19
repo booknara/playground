839e8c0695f457532d3d9b72c8c381eec1ba2634
Daehee Han <bookdori81@gmail.com>
2020-08-19 13:31:13 -0700

    Add Goat Latin problem

--- src/main/java/com/booknara/problem/string/GoatLatin.java
+++ src/main/java/com/booknara/problem/string/GoatLatin.java
@@ -7,33 +7,69 @@ package com.booknara.problem.string;
 public class GoatLatin {
     // T:O(n^2), S:O(n^2)
     public String toGoatLatin(String S) {
-        if (S == null || S.length() == 0) {
-            return S;
-        }
+        // input check (no word)
+        if (S == null || S.length() == 0) return "";
 
-        String[] sArray = S.split("\\s+");
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < sArray.length; i++) {
-            String word = sArray[i];
-            //System.out.println(word);
-            if (word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' ||
-                    word.charAt(0) == 'o' || word.charAt(0) == 'u' ||
-                    word.charAt(0) == 'A' || word.charAt(0) == 'E' || word.charAt(0) == 'I' ||
-                    word.charAt(0) == 'O' || word.charAt(0) == 'U') {
-                // vowel case
-                builder.append(word).append("ma");
+        //String[] array = S.split(" ");
+        String[] array = S.split("\\s+");
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            // each word
+            // first character handling
+            if (isVowel(array[i].charAt(0))) {
+                // Vowel
+                res.append(array[i]);
             } else {
-                // consonant case
-                builder.append(word.substring(1, word.length())).append(word.charAt(0)).append("ma");
+                // Consonant
+                res.append(array[i].substring(1, array[i].length()));
+                res.append(array[i].charAt(0));
             }
-            // 'a' added to the end
-            for (int j = 0; j < i + 1; j++) {
-                builder.append("a");
-            }
+            res.append("ma");
 
-            if (i != sArray.length - 1) builder.append(" ");
+            // extra "a"
+            for (int j = 0; j < i + 1; j++) {
+                res.append("a");
+            }
+            // space
+            res.append(" ");
         }
 
-        return builder.toString();
+        res.deleteCharAt(res.length() - 1);
+        return res.toString();
+    }
+
+    public boolean isVowel(char c) {
+        char lower = Character.toLowerCase(c);
+        return (lower == 'a' || lower == 'e'
+                || lower == 'i' || lower == 'o' || lower == 'u');
     }
 }
+
+/**
+ A sentence S is given, composed of words separated by spaces.
+ Each word consists of lowercase and uppercase letters only.
+
+ We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+ The rules of Goat Latin are as follows:
+
+ 1. If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+ apple -> applema (append "ma" at the end)
+
+ 2. If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+ goat -> oatma (remove "g" and append "g" and ma")
+
+ 3. Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+ For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+
+ Return the final sentence representing the conversion from S to Goat Latin.
+
+ "I speak Goat Latin"
+ -> "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+ "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+ Input: string
+ Output: string
+
+ */
