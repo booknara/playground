3a1800b4bc3ddb9b51dab6f0147f37469085e0e3
Daehee Han <bookdori81@gmail.com>
2020-08-20 02:53:58 -0700

    Add Search in Rotated Sorted Array problem

--- src/main/java/com/booknara/problem/search/binary/SearchInRotatedSortedArray.java
+++ src/main/java/com/booknara/problem/search/binary/SearchInRotatedSortedArray.java
@@ -6,27 +6,23 @@ package com.booknara.problem.search.binary;
  */
 public class SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
+        if (nums == null || nums.length == 0) return -1;
 
         int l = 0, r = nums.length - 1;
         while (l <= r) {
-            int m = l + (r - l) /2;
-            if (target == nums[m]) {
-                return m;
-            }
+            int m = l + (r - l) / 2;
 
-            //System.out.println(nums[m]);
+            if (nums[m] == target) return m;
+
             if (nums[l] <= nums[m]) {
-                // Pivot is larger than the first element (non-rotated array)
+                // ascending (no pivot)
                 if (nums[l] <= target && target < nums[m]) {
                     r = m - 1;
                 } else {
                     l = m + 1;
                 }
             } else {
-                // Pivot is smaller than the first element (non-rotated array)
+                // pivot inside
                 if (nums[m] < target && target <= nums[r]) {
                     l = m + 1;
                 } else {
