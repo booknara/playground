0fea1c055384564dd02e3b5725e75312950ebc33
Daehee Han <bookdori81@gmail.com>
2020-08-23 03:11:32 -0700

    Add Serialize and Deserialize BST problem

--- src/main/java/com/booknara/problem/tree/bst/SerializeDeserializeBinarySearchTree.java
+++ src/main/java/com/booknara/problem/tree/bst/SerializeDeserializeBinarySearchTree.java
@@ -3,6 +3,7 @@ package com.booknara.problem.tree.bst;
 import com.booknara.problem.common.TreeNode;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 449. Serialize and Deserialize BST (Medium)
@@ -10,59 +11,62 @@ import java.util.LinkedList;
  */
 public class SerializeDeserializeBinarySearchTree {
     // Encodes a tree to a single string.
+    // T:O(n), S:O(n)
     public String serialize(TreeNode root) {
-        if (root == null) {
-            return "";
-        }
-
+        // preorder traverse
         StringBuilder builder = new StringBuilder();
         dfsSerialize(root, builder);
-
+        // System.out.println(builder.toString());
         return builder.toString();
     }
 
     public void dfsSerialize(TreeNode node, StringBuilder builder) {
-        if (node == null) {
-            return;
-        }
+        if (node == null) return;
 
         builder.append(node.val);
-        if (node.left != null) {
+        if(node.left != null) {
             builder.append(",");
             dfsSerialize(node.left, builder);
         }
-        if (node.right != null) {
+
+        if(node.right != null) {
             builder.append(",");
             dfsSerialize(node.right, builder);
         }
     }
 
     // Decodes your encoded data to tree.
+    // T:O(n), S:O(n)
     public TreeNode deserialize(String data) {
-        if (data == null || data.length() == 0) {
-            return null;
-        }
+        if (data == null || data.length() == 0) return null;
 
-        String[] sArray = data.split(",");
-        LinkedList<Integer> input = new LinkedList<>();
-        for (String s: sArray) {
-            input.add(Integer.parseInt(s));
+        // "2,1,3"
+        String[] array = data.split(",");
+        Queue<Integer> q = new LinkedList<>();
+        for (String s: array) {
+            q.offer(Integer.parseInt(s));
         }
-
-        return dfsDeserialize(input, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        // [2,1,3]
+        return dfsDeserialize(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    private TreeNode dfsDeserialize(LinkedList<Integer> list, int lower, int higher) {
-        if (list.isEmpty()) return null;
+    public TreeNode dfsDeserialize(Queue<Integer> q, int min, int max) {
+        if (q.isEmpty()) return null;
 
-        int val = list.peekFirst(); // check point
-        if (val < lower || val > higher) return null;
+        int val = q.peek();
+        if (min > val || val > max) return null;
 
-        list.pollFirst();
-        TreeNode root = new TreeNode(val);
-        root.left = dfsDeserialize(list, lower, val);
-        root.right = dfsDeserialize(list, val, higher);
+        TreeNode node = new TreeNode(q.poll());
+        node.left = dfsDeserialize(q, min, node.val);
+        node.right = dfsDeserialize(q, node.val, max);
+        /**
+         2
+         left = dfsDeserialize([1,3], MIN_VALUE, 2);
+         1
+         left = dfsDeserialize([3], MIN_VALUE, 1);
+         right = dfsDeserialize([1,3], MIN_VALUE, 2);
+         */
 
-        return root;
+        return node;
     }
 }
