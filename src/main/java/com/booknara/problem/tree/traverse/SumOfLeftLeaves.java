9d25cbb415029006dd10dd3fe02bccf41150d0b9
Daehee Han <bookdori81@gmail.com>
2020-08-24 10:34:20 -0700

    Add Sum of Left Leaves problem

--- src/main/java/com/booknara/problem/tree/traverse/SumOfLeftLeaves.java
+++ src/main/java/com/booknara/problem/tree/traverse/SumOfLeftLeaves.java
@@ -26,4 +26,21 @@ public class SumOfLeftLeaves {
         if (node.left != null) dfs(node.left, -1);
         if (node.right != null) dfs(node.right, 1);
     }
+
+    // T:O(n), S:O(h)
+    public int sumOfLeftLeaves1(TreeNode root) {
+        if (root == null) return 0;
+
+        return dfs(root.left, true) + dfs(root.right, false);
+    }
+
+    public int dfs(TreeNode node, boolean left) {
+        if (node == null) return 0;
+
+        if (node.left == null && node.right == null) {
+            return left ? node.val : 0;
+        }
+
+        return dfs(node.left, true) + dfs(node.right, false);
+    }
 }
