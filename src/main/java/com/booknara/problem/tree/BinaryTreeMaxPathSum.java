20a854b1a6569813a6830d19371726bd772ebab9
Daehee Han <bookdori81@gmail.com>
2020-08-24 21:24:58 -0700

    Add Binary Tree Maximum Path Sum problem

--- src/main/java/com/booknara/problem/tree/BinaryTreeMaxPathSum.java
+++ src/main/java/com/booknara/problem/tree/BinaryTreeMaxPathSum.java
@@ -3,32 +3,30 @@ package com.booknara.problem.tree;
 import com.booknara.problem.common.TreeNode;
 
 /**
- * Leet code : 124 (Hard)
+ * 124. Binary Tree Maximum Path Sum (Hard)
  * https://leetcode.com/problems/binary-tree-maximum-path-sum/
  */
 public class BinaryTreeMaxPathSum {
-    int totalMax = Integer.MIN_VALUE;
+    int max = Integer.MIN_VALUE;
 
+    // T:O(n), S:O(h)
     public int maxPathSum(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
         dfs(root);
-
-        return totalMax;
+        return max;
     }
 
     public int dfs(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
+        if (node == null) return 0;
 
-        int left = Math.max(dfs(node.left), 0);
-        int right = Math.max(dfs(node.right), 0);
+        int val = node.val;
 
-        totalMax = Math.max(totalMax, left + right + node.val);
+        int left = dfs(node.left);
+        if (left < 0) left = 0;
+        int right = dfs(node.right);
+        if (right < 0) right = 0;
 
-        return Math.max(left, right) + node.val;
+        max = Math.max(max, val + left + right);
+
+        return Math.max(left + val, right + val);
     }
 }
