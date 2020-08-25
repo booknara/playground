2e589cc737cca7f4d381f2dcc87bc540cdefed9b
Daehee Han <bookdori81@gmail.com>
2020-08-24 21:55:22 -0700

    Add Range Sum of BST problem

--- src/main/java/com/booknara/problem/tree/RangeSumOfBST.java
+++ src/main/java/com/booknara/problem/tree/RangeSumOfBST.java
@@ -5,43 +5,27 @@ import com.booknara.problem.common.TreeNode;
 import java.util.Stack;
 
 /**
- * Leet code: 938(Easy)
- * Given the root node of a binary search tree,
- * return the sum of values of all nodes with value between L and R (inclusive).
- *
- * The binary search tree is guaranteed to have unique values.
- * Example 1:
- * Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
- * Output: 32
- *
- * Example 2:
- * Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
- * Output: 23
- *
- * Note: The number of nodes in the tree is at most 10000.
- * The final answer is guaranteed to be less than 2^31.
+ * 938. Range Sum of BST (Easy)
+ * https://leetcode.com/problems/range-sum-of-bst/
  */
 public class RangeSumOfBST {
     int sum = 0;
-    // Recursive method. Time: O(n), Space: O(1)
+    // T:O(n), S:O(h)
     public int rangeSumBST(TreeNode root, int L, int R) {
-        if (root == null) {
-            return 0;
-        }
-
-        if (root.val >= L && root.val <= R) {
-            sum += root.val;
-        }
-
-        if (L < root.val) {
-            rangeSumBST(root.left, L, R);
-        }
-
-        if (R > root.val) {
-            rangeSumBST(root.right, L, R);
-        }
-
+        dfs(root, L, R);
         return sum;
+    }
+
+    public void dfs(TreeNode node, int L, int R) {
+        if (node == null) return;
+
+        // inclusive
+        if (node.val >= L && node.val <= R) {
+            sum += node.val;
+        }
+
+        dfs(node.left, L, R);
+        dfs(node.right, L, R);
     }
 
     // Recursive method. Time: O(n), Space: O(h), height of BST
