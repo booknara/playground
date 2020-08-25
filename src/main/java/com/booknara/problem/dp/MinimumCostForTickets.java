e7ada02c9301f050fc1551f190512aba5a4cedeb
Daehee Han <bookdori81@gmail.com>
2020-08-25 15:27:05 -0700

    Add Minimum Cost For Tickets problem

--- src/main/java/com/booknara/problem/dp/MinimumCostForTickets.java
+++ src/main/java/com/booknara/problem/dp/MinimumCostForTickets.java
@@ -17,10 +17,12 @@ public class MinimumCostForTickets {
 
         for (int i = 1; i < dp.length; i++) {
             if (!travel[i]) {
+                // not travel day
                 dp[i] = dp[i - 1];
                 continue;
             }
 
+            // travel day
             int one = dp[Math.max(i - 1, 0)] + costs[0];
             int seven = dp[Math.max(i - 7, 0)] + costs[1];
             int thirty = dp[Math.max(i - 30, 0)] + costs[2];
