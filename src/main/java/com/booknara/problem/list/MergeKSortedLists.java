5162212432edd7d36cdd6c21478f8cda03d7db82
Daehee Han <bookdori81@gmail.com>
2020-08-27 01:51:07 -0700

    Add Merge k Sorted Lists problem

--- src/main/java/com/booknara/problem/list/MergeKSortedLists.java
+++ src/main/java/com/booknara/problem/list/MergeKSortedLists.java
@@ -2,23 +2,11 @@ package com.booknara.problem.list;
 
 import com.booknara.problem.common.ListNode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.PriorityQueue;
 
 /**
- * Leet code: 23. Merge k Sorted Lists(Hard)
- * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
- *
- * Example:
- * Input:
- * [
- *   1->4->5,
- *   1->3->4,
- *   2->6
- * ]
- * Output: 1->1->2->3->4->4->5->6
+ * 23. Merge k Sorted Lists (Hard)
+ * https://leetcode.com/problems/merge-k-sorted-lists/
  */
 public class MergeKSortedLists {
     // Time Complexity: O(Nlogk), Space Complexity: O(k)
@@ -30,13 +18,6 @@ public class MergeKSortedLists {
         if (lists.length == 1) {
             return lists[0];
         }
-
-//        PriorityQueue<ListNode> minQueue = new PriorityQueue<>(new Comparator<ListNode>() {
-//            @Override
-//            public int compare(ListNode n1, ListNode n2) {
-//                return n1.val - n2.val;
-//            }
-//        });
 
         PriorityQueue<ListNode> minQueue = new PriorityQueue<>((n1, n2) -> {
             return n1.val - n2.val;
@@ -64,38 +45,9 @@ public class MergeKSortedLists {
 
         return head.next;
     }
-    // Time Complexity: O(kN), Space Complexity: O(N)
-    public ListNode mergeKLists1(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
-
-        if (lists.length == 1) {
-            return lists[0];
-        }
-
-        List<Integer> list = new ArrayList<>();
-        for(ListNode node: lists) {
-            while(node != null) {
-                list.add(node.val);
-                node = node.next;
-            }
-        }
-
-        Collections.sort(list);
-        ListNode head = new ListNode(0);
-        ListNode current = head;
-        for(Integer i: list) {
-            ListNode node = new ListNode(i);
-            current.next = node;
-            current = node;
-        }
-
-        return head.next;
-    }
 
     // Time Complexity: O(kN), Space Complexity: O(1)
-    public ListNode mergeKLists2(ListNode[] lists) {
+    public ListNode mergeKLists1(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
