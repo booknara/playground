80e0a10f0f02c74d1801cf7944a38ae4c5a1115b
Daehee Han <bookdori81@gmail.com>
2020-08-28 08:16:06 -0700

    Add Binary Tree Right Side View problem

--- src/main/java/com/booknara/problem/tree/BinaryTreeRightSideView.java
+++ src/main/java/com/booknara/problem/tree/BinaryTreeRightSideView.java
@@ -3,14 +3,17 @@ package com.booknara.problem.tree;
 import com.booknara.problem.common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
- * Leet code: 199. Binary Tree Right Side View (Medium)
+ * 199. Binary Tree Right Side View (Medium)
  * https://leetcode.com/problems/binary-tree-right-side-view/
  */
 public class BinaryTreeRightSideView {
     // Using DFS
+    // T:O(n), S:O(h)
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) {
@@ -33,5 +36,34 @@ public class BinaryTreeRightSideView {
         // Key point: visit right side first
         dfs(node.right, ans, level + 1);
         dfs(node.left, ans, level + 1);
+    }
+
+    // Using BFS
+    // T:O(n), S:O(h)
+    public List<Integer> rightSideView1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                if (i == 0) {
+                    res.add(node.val);
+                }
+
+                // right first
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+            }
+        }
+
+        return res;
     }
 }
