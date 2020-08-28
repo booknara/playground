352527cad53be8eb74a02ffbc474099c0a4ccb01
Daehee Han <bookdori81@gmail.com>
2020-08-28 11:22:29 -0700

    Add Regular Expression Matching problem

--- src/main/java/com/booknara/problem/dp/RegularExpressionMatching.java
+++ src/main/java/com/booknara/problem/dp/RegularExpressionMatching.java
@@ -1,37 +1,72 @@
 package com.booknara.problem.dp;
 
 /**
- * Leet code : 10. Regular Expression Matching (Hard)
+ * 10. Regular Expression Matching (Hard)
  * https://leetcode.com/problems/regular-expression-matching/
  */
 public class RegularExpressionMatching {
+    // T:O(n*m), S:O(n*m)
     public boolean isMatch(String s, String p) {
-        if ((s == null && p == null)
-                || (s.length() == 0 && p.length() == 0)) {
-            return true;
+        // input check
+        if (s == null && p == null) return true;
+        if (s == null || p == null) return false;
+
+        int m = s.length(); // s -> i
+        int n = p.length(); // p -> j
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // '*' handling for input pattern
+        for (int j = 1; j < n + 1; j++) {
+            if (j > 1 && p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
         }
 
-        //
-        boolean[][] memo = new boolean[s.length() + 1][p.length() + 1];
-        memo[0][0] = true;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
-                if (s.charAt(i) == p.charAt(j)
-                        || p.charAt(j) == '.') {
-                    memo[i + 1][j + 1] = memo[i][j];
-                } else if (p.charAt(j) == '*') {
-                    if (p.charAt(j - 1) == '.' || s.charAt(i) == p.charAt(j - 1)) {
-                        // TODO: Check again because it's not working
-                        memo[i + 1][j + 1] = memo[i + 1][j - 1] || memo[i][j + 1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1)) {
+                    // matching characters
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '.') {
+                    // any single character
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    // zero or more of the preceding element
+                    // 2 cases
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        // only an empty string
+                        dp[i][j] = dp[i][j - 2];
                     } else {
-                        memo[i + 1][j + 1] = memo[i + 1][j - 1];
+                        // empty or single or more string
+                        dp[i][j] = dp[i][j - 2] || dp[i][j - 1] || dp[i - 1][j];
                     }
-                } else {
-                    memo[i + 1][j + 1] = false;
                 }
             }
         }
 
-        return memo[s.length()][p.length()];
+        return dp[m][n];
     }
 }
+/**
+ "aaa"
+ "a*"
+
+ "b*" vs "aa" -> F
+ "a*" vs "aa" -> T ("a*" -> "" or "a" or "aa")
+ "ab*" vs "aaa" -> F ("ab*" -> "a" or "ab" or "abb")
+ 0 a b *
+ 0 T F F F
+ a F T F T
+ a F F F F
+ a F F F F
+
+
+ 0 a * a .
+ 0 T F T F F
+ a F T T T F
+ a F F T T T
+ a F F T T
+ b F F T T
+ */
