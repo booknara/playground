6fa19d7224865c01572e47ce70f446d798f68faf
Daehee Han <bookdori81@gmail.com>
2020-08-30 03:26:31 -0700

    Add Add Binary problem

--- src/main/java/com/booknara/problem/string/AddBinary.java
+++ src/main/java/com/booknara/problem/string/AddBinary.java
@@ -53,4 +53,47 @@ public class AddBinary {
 
         return builder.reverse().toString();
     }
+
+    // T:O(max length (a, b), S:O(max length (a, b)
+    public String addBinary1(String a, String b) {
+        if (a.length() == 0) return b;
+        if (b.length() == 0) return a;
+
+        // a = "1010"
+        // b = "1011"
+        StringBuilder res = new StringBuilder();
+        int m = a.length() - 1; // 3
+        int n = b.length() - 1; // 3
+        int carry = 0;
+        while (m >= 0 || n >= 0 || carry == 1) {
+            int f = 0;
+            if (m >= 0) {
+                f = Character.getNumericValue(a.charAt(m));    // 0
+            }
+            int s = 0;
+            if (n >= 0) {
+                s = Character.getNumericValue(b.charAt(n));    // 1
+            }
+
+            int sum = f + s + carry;    // 1 + 0 + 0 (carry)
+            if (sum == 3) {
+                res.insert(0, "1");
+                carry = 1;
+            } else if (sum == 2) {
+                res.insert(0, "0");
+                carry = 1;
+            } else if (sum == 1) {
+                res.insert(0, "1");
+                carry = 0;
+            } else {
+                res.insert(0, "0");
+                carry = 0;
+            }
+            m--;
+            n--;
+        }
+
+        // "10101"
+        return res.toString();
+    }
 }
