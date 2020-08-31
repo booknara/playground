66093840797f1656e32c7391ce067c2ae9ca97b7
Daehee Han <bookdori81@gmail.com>
2020-08-31 14:20:40 -0700

    Add Convert Binary Search Tree to Sorted Doubly Linked List problem

--- src/main/java/com/booknara/problem/tree/ConvertBinarySearchTreeToSortedDoublyLinkedList.java
+++ src/main/java/com/booknara/problem/tree/ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -1,12 +1,48 @@
 package com.booknara.problem.tree;
 
+import java.util.Stack;
+
 /**
  * 426. Convert Binary Search Tree to Sorted Doubly Linked List (Medium)
  * https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
  */
 public class ConvertBinarySearchTreeToSortedDoublyLinkedList {
-    Node first = null, last = null;
+    // T:O(n), S:(h)
     public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+
+        Node head = null, prev = null;
+        // In-order traverse
+        Stack<Node> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            if (prev == null) {
+                // the left most node
+                head = root;
+            } else {
+                root.left = prev;
+                prev.right = root;
+            }
+
+            // print
+            prev = root;
+            root = root.right;
+        }
+
+        // prev : the right most node
+        prev.right = head;
+        head.left = prev;
+
+        return head;
+    }
+
+    Node first = null, last = null;
+    public Node treeToDoublyList1(Node root) {
         if (root == null) {
             return null;
         }
