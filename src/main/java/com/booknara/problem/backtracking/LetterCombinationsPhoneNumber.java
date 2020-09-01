1e255f5b19f8d6f52759240eb460ccc4166ca763
Daehee Han <bookdori81@gmail.com>
2020-09-01 00:48:01 -0700

    Add Letter Combinations of a Phone Number problem

--- src/main/java/com/booknara/problem/backtracking/LetterCombinationsPhoneNumber.java
+++ src/main/java/com/booknara/problem/backtracking/LetterCombinationsPhoneNumber.java
@@ -10,51 +10,73 @@ import java.util.List;
 public class LetterCombinationsPhoneNumber {
     // T: O(3^N * 4^M(N: the length of strings, M: the length of strings, e.g. 7, 9), S:O(3^N * 4^M)
     public List<String> letterCombinations(String digits) {
+        // input check
         List<String> res = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
-            return res;
-        }
+        if (digits == null || digits.length() == 0) return res;
 
-        StringBuilder builder = new StringBuilder();
-        dfs(digits, 0, builder, res);
+        // "23"
+        dfs(digits, 0, new StringBuilder(), res);
+
         return res;
     }
 
-    private void dfs(String digits, int index, StringBuilder s, List<String> list) {
+    public void dfs(String digits, int index, StringBuilder builder, List<String> res) {
+        // base case
         if (index == digits.length()) {
-            list.add(s.toString());
+            res.add(builder.toString());
             return;
         }
 
-        String letters = getLetter(digits.charAt(index));
-        //System.out.println(letters);
-        for (char c: letters.toCharArray()) {
-            StringBuilder builder = new StringBuilder(s.toString());
-            builder.append(c);
-            dfs(digits, index + 1, builder, list);
+        char c = digits.charAt(index);  // 2
+        String letters = getDigitLetters(c);    // "abc"
+        for (char l: letters.toCharArray()) {
+            builder.append(l);
+            dfs(digits, index + 1, builder, res);
+            builder.deleteCharAt(builder.length() - 1);
         }
     }
 
-    private String getLetter(char c) {
-        switch(c) {
-            case '2':
-                return "abc";
-            case '3':
-                return "def";
-            case '4':
-                return "ghi";
-            case '5':
-                return "jkl";
-            case '6':
-                return "mno";
-            case '7':
-                return "pqrs";
-            case '8':
-                return "tuv";
-            case '9':
-                return "wxyz";
+    public String getDigitLetters(char num) {
+        String res;
+        switch(num) {
+            case '2': res = "abc";
+                break;
+            case '3': res = "def";
+                break;
+            case '4': res = "ghi";
+                break;
+            case '5': res = "jkl";
+                break;
+            case '6': res = "mno";
+                break;
+            case '7': res = "pqrs";
+                break;
+            case '8': res = "tuv";
+                break;
+            case '9': res = "wxyz";
+                break;
+            default:
+                res = "";
+                break;
         }
 
-        return null;
+        return res;
     }
 }
+/**
+ Input: "23"
+ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ 2 = "abc"
+ 3 = "def"
+ 4 = "ghi"
+ 5 = "jkl"
+ 6 = "mno"
+ 7 = "pqrs"
+ 8 = "tuv"
+ 9 = "wxyz"
+
+ 2 -> a or b or c
+ 3 -> ad   bd   cd
+ ae   be   ce
+ af   bf   cf
+ */
