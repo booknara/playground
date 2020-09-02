8b6c96c21f451ac809da1803b3457640a511d5df
Daehee Han <bookdori81@gmail.com>
2020-09-02 02:45:19 -0700

    Add Convert a Number to Hexadecimal problem

--- src/main/java/com/booknara/problem/bit/ConvertNumberToHexadecimal.java
+++ src/main/java/com/booknara/problem/bit/ConvertNumberToHexadecimal.java
@@ -5,6 +5,7 @@ package com.booknara.problem.bit;
  * https://leetcode.com/problems/convert-a-number-to-hexadecimal/
  */
 public class ConvertNumberToHexadecimal {
+    // T:O(1), S:O(1)
     public String toHex(int num) {
         if (num == 0) return "0";
 
