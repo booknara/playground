7bcf78b9730480d9243eb6be2932149122233dc3
Daehee Han <bookdori81@gmail.com>
2020-09-02 01:50:52 -0700

    Add Generate Parentheses problem

--- src/main/java/com/booknara/problem/recursive/GenerateParentheses.java
+++ src/main/java/com/booknara/problem/recursive/GenerateParentheses.java
@@ -8,6 +8,7 @@ import java.util.List;
  * https://leetcode.com/problems/generate-parentheses/
  */
 public class GenerateParentheses {
+    // T, S:O(nth-Catalan number), roughly it's exponential complexity
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         if (n == 0) {
@@ -31,5 +32,33 @@ public class GenerateParentheses {
         if (open < close && close > 0) {
             dfs(list, open, close - 1, max, s + ")");
         }
+    }
+
+    public List<String> generateParenthesis1(int n) {
+        // input check
+        List<String> res = new ArrayList<>();
+        if (n == 0) return res;
+
+        dfs(res, n, n, new StringBuilder());
+
+        return res;
+    }
+
+    public void dfs(List<String> res, int open, int close, StringBuilder builder) {
+        // base case
+        if (open > close) return;   // tree pruning
+        if (open < 0 || close < 0) return;
+
+        if (open == 0 && close == 0) {
+            res.add(builder.toString());
+            return;
+        }
+
+        // open == close or open < close
+        builder.append("(");
+        dfs(res, open - 1, close, new StringBuilder(builder.toString()));
+        builder.deleteCharAt(builder.length() - 1); // backtracking
+        builder.append(")");
+        dfs(res, open, close - 1, new StringBuilder(builder.toString()));
     }
 }
