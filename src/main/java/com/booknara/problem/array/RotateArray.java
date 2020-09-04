b9de9ad08d92c15191f784f0d70e9f638fbcaa2a
Daehee Han <bookdori81@gmail.com>
2020-09-04 02:53:20 -0700

    Add Rotate Array problem

--- src/main/java/com/booknara/problem/array/RotateArray.java
+++ src/main/java/com/booknara/problem/array/RotateArray.java
@@ -7,44 +7,40 @@ import java.util.LinkedList;
  * https://leetcode.com/problems/rotate-array/
  */
 public class RotateArray {
-    // input: integer array, k steps
-    // output: nums
     // T:O(n), S:O(1)
     public void rotate(int[] nums, int k) {
-        // input check: 1<= nums.length <= 2 * 10^4
-        // k >= 0
+        // input check
+        k = k % nums.length;
         if (k == 0) return;
 
         int n = nums.length;
-        k = k % n;
-        // 0 ~ (n - k - 1), (n - k) ~ (n - 1)
-        // reverse each part
+        // Intuition -> n-k th item will be at the front and next value to n-k th will take next place
+        // there are three times to reverse
         reverse(nums, 0, n - k - 1);
         reverse(nums, n - k, n - 1);
-        // reverse the entire array
         reverse(nums, 0, n - 1);
     }
 
-    public void reverse(int[] nums, int l, int r) {
-        while (l < r) {
-            swap(nums, l, r);
-            l++;
-            r--;
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    // [1,2,3,4,5,6,7] -> [5,6,7,1,2,3,4] after 3 steps
-    // there are two parts
-    // 0 ~ (n - k - 1), (n - k) ~ (n - 1)
-    // reverse each part
-    // [1,2,3,4,5,6,7] -> [4,3,2,1,7,6,5]
-    // reverse the entire array
-    // [4,3,2,1,7,6,5] -> [5,6,7,1,2,3,4]
+    /**
+     Intuition -> n-k th item will be at the front and next value to n-k th will take next place
+     [1,2,3,4,   5,6,7] -> [5,6,7,1,2,3,4] after 3 steps
+     there are two parts
+     0 ~ (n - k - 1), (n - k) ~ (n - 1)
+     reverse each part
+     [1,2,3,4,5,6,7] -> [4,3,2,1,7,6,5]
+     reverse the entire array
+     [4,3,2,1,7,6,5] -> [5,6,7,1,2,3,4]
+     */
 
     // T:O(n), S:O(n)
     public void rotate1(int[] nums, int k) {
