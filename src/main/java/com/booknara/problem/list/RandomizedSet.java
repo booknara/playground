a26c8ca7be6437168c48803fc524bdd8b8804e23
Daehee Han <bookdori81@gmail.com>
2020-09-04 15:17:52 -0700

    Add Insert Delete GetRandom O(1) problem

--- src/main/java/com/booknara/problem/list/RandomizedSet.java
+++ src/main/java/com/booknara/problem/list/RandomizedSet.java
@@ -2,10 +2,13 @@ package com.booknara.problem.list;
 
 import java.util.*;
 
+/**
+ * 380. Insert Delete GetRandom O(1) (Medium)
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/
+ */
 public class RandomizedSet {
-    private List<Integer> list;
-    private Map<Integer, Integer> map;
-
+    List<Integer> list;
+    Map<Integer, Integer> map;
     /** Initialize your data structure here. */
     public RandomizedSet() {
         list = new ArrayList<>();
@@ -20,7 +23,6 @@ public class RandomizedSet {
 
         map.put(val, list.size());
         list.add(val);
-
         return true;
     }
 
@@ -31,13 +33,16 @@ public class RandomizedSet {
         }
 
         int index = map.get(val);
+        // case 1: not the last index
         if (index != list.size() - 1) {
+            // update the list
             int lastValue = list.get(list.size() - 1);
             list.set(index, lastValue);
+            // update the map
             map.replace(lastValue, index);
         }
+        // case 2: the last index
         list.remove(list.size() - 1);
-
         map.remove(val);
 
         return true;
@@ -45,7 +50,30 @@ public class RandomizedSet {
 
     /** Get a random element from the set. */
     public int getRandom() {
-        System.out.println("size: " + list.size());
-        return list.get(new Random().nextInt(list.size()));
+        int random = new Random().nextInt(list.size());
+        return list.get(random);
     }
 }
+/**
+ [1,2] -> [2], delete 1
+
+ [
+ 1:0,
+ 2:1
+ ]
+
+ 1. get the index value (0)
+ 2. set the list the index(0) -> the last value[2]
+ 3. replace the map key, value -> the last value : the index of val[2:0]
+
+ 4. remove the last value of list
+ 5. remove the val from the map
+ */
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
