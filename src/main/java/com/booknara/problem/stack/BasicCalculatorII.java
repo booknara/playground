ae8a743bd9b1e99e9a5953402272b87e614c3ed7
Daehee Han <bookdori81@gmail.com>
2020-09-05 11:44:26 -0700

    Add Basic Calculator II problem

--- src/main/java/com/booknara/problem/stack/BasicCalculatorII.java
+++ src/main/java/com/booknara/problem/stack/BasicCalculatorII.java
@@ -19,8 +19,7 @@ public class BasicCalculatorII {
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isDigit(c)) {
-                num *= 10;
-                num += Character.getNumericValue(c);
+                num = num * 10 + (c - '0');
             }
 
             if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
