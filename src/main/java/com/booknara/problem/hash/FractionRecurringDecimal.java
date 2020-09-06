25fc7a784694dae13a598eac291f164f21af9d1e
Daehee Han <bookdori81@gmail.com>
2020-09-06 00:45:49 -0700

    Add Fraction to Recurring Decimal problem

--- src/main/java/com/booknara/problem/hash/FractionRecurringDecimal.java
+++ src/main/java/com/booknara/problem/hash/FractionRecurringDecimal.java
@@ -3,42 +3,50 @@ package com.booknara.problem.hash;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 166. Fraction to Recurring Decimal (Medium)
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/
+ */
 public class FractionRecurringDecimal {
+    // T:O(n), S:(m, the length of divisor times)
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0 || denominator == 1) {
-            return String.valueOf(numerator);
-        }
+        // input check
+        if (numerator == 0) return "0";
+        if (denominator == 1) return String.valueOf(numerator);
 
-        StringBuilder builder = new StringBuilder();
-        if (numerator < 0 ^ denominator < 0) {
-            builder.append("-");
-        }
+        StringBuilder res = new StringBuilder();
+        // negative handling
+        if (numerator < 0 && denominator > 0) res.append("-");
+        if (numerator > 0 && denominator < 0) res.append("-");
 
-        long dividend = Math.abs(Long.valueOf(numerator));
-        long divisor = Math.abs(Long.valueOf(denominator));
+        // Edge case [-1, -2147483648]
+        Long dividend = Math.abs(Long.valueOf(numerator));
+        Long divisor = Math.abs(Long.valueOf(denominator));
 
-        builder.append(dividend / divisor);
+        // integer part
+        res.append(dividend / divisor);
         long remaining = dividend % divisor;
         if (remaining == 0) {
-            return builder.toString();
+            return res.toString();
         }
-        builder.append(".");
-
+        res.append(".");
         Map<Long, Integer> map = new HashMap<>();
+        // floating part
+        // [0.] 2,3
         while (remaining != 0) {
             if (map.containsKey(remaining)) {
-                // Repeat
-                builder.insert(map.get(remaining), "(");
-                builder.append(")");
+                int idx = map.get(remaining);
+                res.insert(idx, "(");
+                res.append(")");
                 break;
             }
-
-            map.put(remaining, builder.length());
+            // 2, length = 2
+            map.put(remaining, res.length());
             remaining *= 10;
-            builder.append(remaining / divisor);
-            remaining = remaining % divisor;
+            res.append(remaining / divisor);
+            remaining %= divisor;
         }
 
-        return builder.toString();
+        return res.toString();
     }
 }
