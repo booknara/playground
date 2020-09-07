602b79dfe7ae1077dda72ddd258b686b0923ef3e
Daehee Han <bookdori81@gmail.com>
2020-09-07 16:41:15 -0700

    Add Binary Tree Level Order Traversal problem

--- src/main/java/com/booknara/problem/tree/traverse/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/booknara/problem/tree/traverse/BinaryTreeLevelOrderTraversal.java
@@ -3,13 +3,16 @@ package com.booknara.problem.tree.traverse;
 import com.booknara.problem.common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 102. Binary Tree Level Order Traversal (Medium)
  * https://leetcode.com/problems/binary-tree-level-order-traversal/
  */
 public class BinaryTreeLevelOrderTraversal {
+    // T:O(n), SO(n)
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
@@ -33,5 +36,38 @@ public class BinaryTreeLevelOrderTraversal {
         res.get(level).add(node.val);
         dfs(node.left, res, level + 1);
         dfs(node.right, res, level + 1);
+    }
+
+    // T:O(n), SO(n)
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        // input check
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            if (res.size() == level) {
+                res.add(new ArrayList<>());
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                res.get(level).add(node.val);
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+
+            level++;
+        }
+
+        return res;
     }
 }
