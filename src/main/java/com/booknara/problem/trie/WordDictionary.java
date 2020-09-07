50df93c00b37caac1908e9124b1edf12da7d1425
Daehee Han <bookdori81@gmail.com>
2020-09-06 17:42:27 -0700

    Add Add and Search Word - Data structure design problem

--- src/main/java/com/booknara/problem/trie/WordDictionary.java
+++ src/main/java/com/booknara/problem/trie/WordDictionary.java
@@ -4,7 +4,7 @@ package com.booknara.problem.trie;
  * 211. Add and Search Word - Data structure design (Medium)
  * https://leetcode.com/problems/add-and-search-word-data-structure-design/
  */
-public class WordDictionary {
+class WordDictionary {
     TrieNode root;
     /** Initialize your data structure here. */
     public WordDictionary() {
@@ -12,54 +12,81 @@ public class WordDictionary {
     }
 
     /** Adds a word into the data structure. */
+    // T:O(n*m), S:O(n*m)
     public void addWord(String word) {
         TrieNode cur = root;
-        for (char c: word.toCharArray()) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
             if (cur.children[c - 'a'] == null) {
                 cur.children[c - 'a'] = new TrieNode();
             }
             cur = cur.children[c - 'a'];
         }
-        cur.leaf = true;
+
+        // leaf node is true which means word
+        cur.word = true;
     }
 
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    // T:O(m*26^k), S:O(1)
     public boolean search(String word) {
-        TrieNode cur = root;
-        return dfs(cur, word, 0);
+        return search(word, root);
     }
 
-    private boolean dfs(TrieNode node, String s, int idx) {
-        if (idx == s.length()) {
-            return node.leaf;
-        }
+    public boolean search(String word, TrieNode cur) {
+        if (cur == null) return false;
 
-        char c = s.charAt(idx);
-        if (c != '.') {
-            if (node.children[c - 'a'] == null) {
+        for (int i = 0; i < word.length(); i++) {
+            // there are three cases
+            char c = word.charAt(i);
+            if (c == '.') {
+                for (TrieNode n: cur.children) {
+                    if (search(word.substring(i + 1), n)) return true;
+                }
                 return false;
             }
-            return dfs(node.children[c - 'a'], s, idx + 1);
+
+            if (cur.children[c - 'a'] == null) return false;
+            cur = cur.children[c - 'a'];
         }
 
-        // . character
-        for (int i = 0; i < 26; i++) {
-            if (node.children[i] != null) {
-                boolean b = dfs(node.children[i], s, idx + 1);
-                if (b) return true;
-            }
-        }
-
-        return false;
+        return cur.word;
     }
 
-    class TrieNode {
-        final static int NODE = 26;
-        boolean leaf;
+    static class TrieNode {
+        final int NODES = 26;
+        boolean word;
         TrieNode[] children;
         TrieNode() {
-            this.leaf = false;
-            this.children = new TrieNode[NODE];
+            this.word = false;
+            this.children = new TrieNode[NODES];
         }
     }
 }
+/**
+ WordDictionary wordDictionary = new WordDictionary();
+ wordDictionary.addWord("bad");
+ wordDictionary.addWord("dad");
+ wordDictionary.addWord("mad");
+ wordDictionary.search("pad"); // return False
+ wordDictionary.search("bad"); // return True
+ wordDictionary.search(".ad"); // return True
+ wordDictionary.search("b.."); // return True
+
+ Input character: only 26 alphabet (a~z)
+ Method: Using Trie
+ Root
+ b    d    m
+ a    a    a
+ d(T) d(T) d(T)
+
+ ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+ [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+
+ */
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
