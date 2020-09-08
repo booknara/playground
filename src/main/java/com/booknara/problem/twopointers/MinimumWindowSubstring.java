bb2dddedf913a5c8c59877296e7bf8f4fe01e582
Daehee Han <bookdori81@gmail.com>
2020-09-08 00:47:18 -0700

    Add Minimum Window Substring problem

--- src/main/java/com/booknara/problem/twopointers/MinimumWindowSubstring.java
+++ src/main/java/com/booknara/problem/twopointers/MinimumWindowSubstring.java
@@ -8,39 +8,36 @@ public class MinimumWindowSubstring {
     // Time complexity: O(n), Space complexity: O(1)
     public String minWindow(String s, String t) {
         // input check
-        if (s.length() == 0 || t.length() == 0) return "";
+        if (t.length() == 0) return s;
+        if (s.length() == 0) return "";
+        if (s.length() < t.length()) return "";
 
+        // bucketing t (A~Z, a~z)
         int[] bucket = new int[128];
         for (char c: t.toCharArray()) {
             bucket[c]++;
         }
-        // [2,1,1] = "AABC"
-        // two pointers
-        int l = 0, r = 0;
-        int len = t.length();       // required the total number of string contained
-        int min = Integer.MAX_VALUE;
+
         String res = "";
+        int min = Integer.MAX_VALUE;
+        int len = t.length();          // required the total number of string contained
+        int l = 0, r = 0;
         while (r < s.length()) {
             char c = s.charAt(r);
             bucket[c]--;
-            if (bucket[c] >= 0) {
-                len--;
-            }
+            if (bucket[c] >= 0) len--;  // Note: '>='
 
-            // "ADOBECODEAB"
-            while (len == 0) {
-                char d = s.charAt(l);
-                bucket[d]++;
-                if (bucket[d] > 0) {
+            while (l <= r && len == 0) {    // l <= r, edge case S:"a", T:"a"
+                int size = r - l + 1;
+                if (size < min) {
+                    min = size;
+                    res = s.substring(l, r + 1);
+                }
+
+                bucket[s.charAt(l)]++;
+                if (bucket[s.charAt(l)] > 0) {
                     len++;
                 }
-
-                // (r, l)
-                if (r - l + 1 < min) {
-                    res = s.substring(l, r + 1);
-                    min = r - l + 1;
-                }
-
                 l++;
             }
 
@@ -50,3 +47,14 @@ public class MinimumWindowSubstring {
         return res;
     }
 }
+/**         01234567890
+ Input: S = "ZADOBECODEABANC", T = "AABC"
+ Output: "ABANC"
+
+ T -> A:2, B:1, C:1
+ bucket = [2,1,1,0,0,0,0,], the number of alphabet = 3;
+ if (bucket[s.charAt(i)] >= 0) alphabet--
+ if (alphabet == 0) {
+ // get the sliding size
+ }
+ */
