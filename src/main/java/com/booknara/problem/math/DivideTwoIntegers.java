7f066921f37b30bc55e4635b4615afc5866d9544
Daehee Han <bookdori81@gmail.com>
2020-09-09 03:30:23 -0700

    Add Divide Two Integers problem

--- src/main/java/com/booknara/problem/math/DivideTwoIntegers.java
+++ src/main/java/com/booknara/problem/math/DivideTwoIntegers.java
@@ -7,25 +7,40 @@ package com.booknara.problem.math;
 public class DivideTwoIntegers {
     // O(logn), left shift operator
     public int divide(int dividend, int divisor) {
-        // input clarification
-        // Integer range (-2^31 ~ 2^31-1), -2^31*-1 = 2^31(out of range)
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-            return Integer.MAX_VALUE;
+        int quotient = 0;
+        // edge case
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == -1) return Integer.MAX_VALUE;
+            if (divisor == 1) return Integer.MIN_VALUE;
+            if (divisor == Integer.MIN_VALUE) return 1;
+            dividend += Math.abs(divisor);
+            quotient++;
         }
 
+        if (dividend == 0) return 0;
         if (divisor == 1) return dividend;
+        if (divisor == Integer.MIN_VALUE) return 0;
 
-        int a = Math.abs(dividend), b = Math.abs(divisor), res = 0, x = 0;
-        while (a - b >= 0) {
-            // 0 << 1 = 0, 0 + 1 << 1 = 2, 2(10 by binary) + 1 = 3 << 1 = 4;
-            for (x = 0; a - (b << x << 1) >= 0; x++);
-            // add 2 ^ x to result
-            res += 1 << x;
-            // subtract a - b * 2^x
-            a -= b << x;
+        // assume that divisor shouldn't be zero
+        boolean negative = false;
+        if (dividend < 0 && divisor > 0) negative = true;
+        if (dividend > 0 && divisor < 0) negative = true;
+
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        while (dividend >= divisor) {
+            int accum = divisor, count = 1;
+            while (dividend - accum >= accum) { // Note here
+                accum <<= 1;
+                count <<= 1;
+            }
+
+            dividend -= accum;
+            quotient += count;
         }
 
-        return (dividend > 0) == (divisor > 0) ? res : -res;
+        return negative ? -quotient : quotient;
     }
 
     // Linear search: Still slow O(n), dividend / divisor
