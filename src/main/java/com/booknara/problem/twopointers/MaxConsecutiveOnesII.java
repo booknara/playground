ef75ded85fc6cb65320534a88ccdd0f9d5982732
Daehee Han <bookdori81@gmail.com>
2020-09-09 23:40:05 -0700

    Add Max Consecutive Ones II problem

--- src/main/java/com/booknara/problem/twopointers/MaxConsecutiveOnesII.java
+++ src/main/java/com/booknara/problem/twopointers/MaxConsecutiveOnesII.java
@@ -7,28 +7,27 @@ package com.booknara.problem.twopointers;
 public class MaxConsecutiveOnesII {
     // T:O(n), S:O(1)
     public int findMaxConsecutiveOnes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        // input check
+        if (nums == null || nums.length == 0) return 0;
 
         int l = 0, r = 0;
-        int graceK = 1;
+        int k = 1;
         int max = 0;
         while (r < nums.length) {
-            if (nums[r] == 1 || graceK > 0) {
-                if (nums[r] == 0) graceK--;
-                max = Math.max(max, r - l + 1);
-                r++;
-            } else {
-                // else case means nums[l] is 0 and no more graceK
-                // to find 0 to move forward with graceK
-                while (nums[l] != 0) {
+            if (nums[r] == 0) { // 0
+                // decrease k
+                k--;
+                // move l to right til k == 0
+                while (k < 0) {
+                    if (nums[l] == 0) {
+                        k++;
+                    }
                     l++;
                 }
-
-                l++;
-                graceK++;
             }
+
+            max = Math.max(max, r - l + 1);
+            r++;
         }
 
         return max;
