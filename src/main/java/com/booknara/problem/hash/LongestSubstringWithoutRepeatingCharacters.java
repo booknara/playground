69473936aaaea0be2f03af0d5526076c928710a0
Daehee Han <bookdori81@gmail.com>
2020-09-13 03:51:34 -0700

    Add Longest Substring Without Repeating Characters problem

--- src/main/java/com/booknara/problem/hash/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/booknara/problem/hash/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,24 +1,32 @@
 package com.booknara.problem.hash;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * 3. Longest Substring Without Repeating Characters (Medium)
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
 public class LongestSubstringWithoutRepeatingCharacters {
+    // T:O(n), S:O(1)
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
+        if (s == null || s.length() == 0) return 0;
 
-        Map<Character, Integer> indexMap = new HashMap<>();
-        int max = 0;
-        int start = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (indexMap.containsKey(s.charAt(i))) {
-                start = Math.max(start, indexMap.get(s.charAt(i)) + 1);
+        int m = s.length();
+        int[] bucket = new int[128];  // input range is all ascii characters
+        int l = 0, r = 0;
+        int max = 1;
+        while (r < m) {
+            char c = s.charAt(r);
+            bucket[c]++;
+
+            if (bucket[c] >= 2) {
+                // repeated
+                while (l < r && bucket[c] > 1) {    // check bucket[c]
+                    bucket[s.charAt(l)]--;
+                    l++;
+                }
             }
 
-            max = Math.max(max, i - start + 1);
-            indexMap.put(s.charAt(i), i);
+            max = Math.max(max, r - l + 1);
+            r++;
         }
 
         return max;
