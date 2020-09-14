9bfe4af77fe49583063c7800d79df2749b554c27
Daehee Han <bookdori81@gmail.com>
2020-09-13 23:47:59 -0700

    Add Merge Sorted Array problem

--- src/main/java/com/booknara/problem/array/MergeSortedArray.java
+++ src/main/java/com/booknara/problem/array/MergeSortedArray.java
@@ -7,24 +7,33 @@ package com.booknara.problem.array;
 public class MergeSortedArray {
     // T:O(n+m), S:O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int idx = m + n - 1;
-        m = m - 1;
-        n = n - 1;
-        while (m >= 0 || n >= 0) {
-            // System.out.println(idx);
-            if (m >= 0 && n >= 0) {
-                if (nums1[m] > nums2[n]) {
-                    nums1[idx--] = nums1[m--];
-                } else {
-                    nums1[idx--] = nums2[n--];
-                }
-            } else if (m >= 0) {
-                nums1[idx--] = nums1[m--];
+        // input check
+        if (m == 0 && n == 0) return;
+
+        m--;
+        n--;
+        for (int i = m + n + 1; i >= 0; i--) {
+            if (m < 0) {
+                nums1[i] = nums2[n];
+                n--;
+            } else if (n < 0) {
+                nums1[i] = nums1[m];
+                m--;
+            } else if (nums1[m] < nums2[n]) {
+                nums1[i] = nums2[n];
+                n--;
             } else {
-                nums1[idx--] = nums2[n--];
+                nums1[i] = nums1[m];
+                m--;
             }
         }
-
-        return;
     }
 }
+/**
+ Input:
+ nums1 = [1,2,3,0,0,0], m = 3
+ nums2 = [2,5,6],       n = 3
+
+ Output: [1,2,2,3,5,6]
+ Merge the values from the end
+ */
