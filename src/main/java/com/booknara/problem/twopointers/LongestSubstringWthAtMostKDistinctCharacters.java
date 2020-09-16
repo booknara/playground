6dc6955be78ad6e33fae7a5a8caf1494456f3c15
Daehee Han <bookdori81@gmail.com>
2020-09-16 08:18:58 -0700

    Add Longest Substring with At Most K Distinct Characters problem

--- src/main/java/com/booknara/problem/twopointers/LongestSubstringWthAtMostKDistinctCharacters.java
+++ src/main/java/com/booknara/problem/twopointers/LongestSubstringWthAtMostKDistinctCharacters.java
@@ -7,29 +7,60 @@ package com.booknara.problem.twopointers;
 public class LongestSubstringWthAtMostKDistinctCharacters {
     // T:O(n*k), S:O(1)
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if (s == null || s.length() == 0 || k == 0) {
-            return 0;
-        }
+        if (s == null || s.length() == 0) return 0;
+        if (k == 0) return 0;
+        if (s.length() <= k) return s.length();
 
-        int[] bucket = new int[256];
+        int max = 1;
+        int[] bucket = new int[128];
         int l = 0, r = 0;
-        int distinct = 0;
-        int max = 0;
         while (r < s.length()) {
-            if (bucket[s.charAt(r)] == 0) distinct++;
-            bucket[s.charAt(r)]++;
+            char c = s.charAt(r);
 
-            while (distinct > k) {
-                bucket[s.charAt(l)]--;
-                if (bucket[s.charAt(l)] == 0) distinct--;
-                l++;
+            if (bucket[c] == 0) {
+                bucket[c]++;
+                k--;
+                while (l < r && k < 0) {
+                    char d = s.charAt(l);
+                    bucket[d]--;     // move l pointer bucket[l]--
+                    if (bucket[d] == 0) {
+                        k++;
+                    }
+                    l++;
+                }
+            } else {
+                bucket[c]++;
             }
-
-            // the longest length is the range of two pointers
             max = Math.max(max, r - l + 1);
+
             r++;
         }
 
         return max;
     }
 }
+/**
+ Input: s = "ec eba", k = 2
+ Output: 3
+ e -> 1, k = 1
+
+ c -> 0, k = 0
+ e -> 0, k = 0
+
+ b -> 1, k = -1, k = 0
+ a -> 1, k = -1
+ Method Two pointers
+ bucket[]
+ if (bucket[c] == 0) -> means new character. k--
+    bucket[c]++;
+    k--;
+    while (l < r && k < 0)
+        move l pointer bucket[l]--
+        if (bucket[l] == 0) {
+        k++;
+    }
+ else
+    bucket[c]++;
+
+ int max = Math.max(max, r - l + 1);
+ */
