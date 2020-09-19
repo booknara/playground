b591586ffa9c8e04987b9888f1132fc068dd5ad8
Daehee Han <bookdori81@gmail.com>
2020-09-18 23:59:56 -0700

    Add Reorder List problem

--- src/main/java/com/booknara/problem/list/ReorderList.java
+++ src/main/java/com/booknara/problem/list/ReorderList.java
@@ -24,23 +24,61 @@ public class ReorderList {
 
         pre.next = null;    // To prevent infinite loop
         // Reverse the second half list
-        ListNode halfHead = reverseLinkedList(slow);
+        ListNode halfHead = reverse(slow);
 
         // Merge the first half and second half
         merge(head, halfHead);
     }
 
-    // Leetcode #206
-    private ListNode reverseLinkedList(ListNode head) {
-        ListNode newHead = head;
-        while (head.next != null) {
-            ListNode p = head.next;
-            head.next = p.next;
-            p.next = newHead;
-            newHead = p;
+    // T:O(n), S:O(1)
+    public void reorderList1(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast.next != null && fast.next.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        return newHead;
+        if (fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+        }
+        prev.next = null;
+
+        ListNode reversed = reverse(slow);
+
+        //merge (head, reversed), head is equal or shorter than reversed
+        ListNode cur1 = head;
+        ListNode cur2 = reversed;
+        while (cur1 != null) {
+            ListNode fNext = cur1.next;
+            ListNode sNext = cur2.next;
+            cur1.next = cur2;
+            if (fNext == null) {
+                break;
+            }
+            cur2.next = fNext;
+            cur1 = fNext;
+            cur2 = sNext;
+        }
+    }
+
+    // Leetcode #206
+    private ListNode reverse(ListNode node) {
+        ListNode head = node;
+        while (node.next != null) {
+            ListNode p = node.next;
+            node.next = p.next;
+            p.next = head;
+            head = p;
+        }
+
+        return head;
     }
 
     private void merge(ListNode l1, ListNode l2) {
