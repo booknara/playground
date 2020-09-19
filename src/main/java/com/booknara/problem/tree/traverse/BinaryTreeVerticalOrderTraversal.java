44c5304b5aaa311395ffd4a86c61c925af91a950
Daehee Han <bookdori81@gmail.com>
2020-09-18 23:30:23 -0700

    Add Binary Tree Vertical Order Traversal problem

--- src/main/java/com/booknara/problem/tree/traverse/BinaryTreeVerticalOrderTraversal.java
+++ src/main/java/com/booknara/problem/tree/traverse/BinaryTreeVerticalOrderTraversal.java
@@ -44,4 +44,72 @@ public class BinaryTreeVerticalOrderTraversal {
 
         return res;
     }
+
+    // T:O(n), S:O(h)
+    public List<List<Integer>> verticalOrder1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        // from top to bottom (bfs)
+        int min = 0;
+        Queue<Element> q = new LinkedList<>();
+        q.offer(new Element(0, root));
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Element e = q.poll();
+                min = Math.min(min, e.col);
+                List<Integer> list = map.getOrDefault(e.col, new ArrayList<>());
+                list.add(e.node.val);
+                map.put(e.col, list);
+
+                if (e.node.left != null) {
+                    q.offer(new Element(e.col - 1, e.node.left));
+                }
+                if (e.node.right != null) {
+                    q.offer(new Element(e.col + 1, e.node.right));
+                }
+            }
+        }
+
+        while (map.containsKey(min)) {
+            res.add(map.get(min));
+            min++;
+        }
+
+        return res;
+    }
+
+    static class Element {
+        int col;
+        TreeNode node;
+        Element (int col, TreeNode node) {
+            this.col = col;
+            this.node = node;
+        }
+    }
 }
+/**
+ Level order traverse
+ left move -1
+ right move +1
+ Map<Integer, List<Integer>> map = new HashMap<>();
+
+ 3
+ /\
+ /  \
+ 9   8
+ /\  /\
+ /  \/  \
+ 4  01   7
+
+ -2 -> [4]
+ -1 -> [9]
+ 0 -> [3,0,1]
+ 1 -> [8]
+ 2 -> [7]
+
+ */
