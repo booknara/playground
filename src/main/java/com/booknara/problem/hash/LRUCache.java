1f6556109cd2b727ddf04651f2b5313e80e6b9cd
Daehee Han <bookdori81@gmail.com>
2020-09-22 17:59:46 -0700

    Add LRU Cache problem

--- src/main/java/com/booknara/problem/hash/LRUCache.java
+++ src/main/java/com/booknara/problem/hash/LRUCache.java
@@ -1,6 +1,6 @@
 package com.booknara.problem.hash;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -8,37 +8,93 @@ import java.util.Map;
  * https://leetcode.com/problems/lru-cache/
  */
 public class LRUCache {
-    Map<Integer, Integer> cache;
+    Map<Integer, Node> map;
+    Node head;
+    Node end;
     int capacity;
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        cache = new LinkedHashMap<Integer, Integer>() {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                boolean evict = size() > capacity;
-                if (evict) {
-                    //System.out.println("remove " + eldest.getKey());
-                }
 
-                return evict;
-            }
-        };
+    public LRUCache(int capacity) {
+        this.map = new HashMap<>();
+        this.head = null;
+        this.end = null;
+        this.capacity = capacity;
     }
 
     public int get(int key) {
-        int res = cache.getOrDefault(key, -1);
-        if (res != -1) {
-            cache.remove(key);
-            cache.put(key, res);
+        System.out.println(key);
+        if (!map.containsKey(key)) {
+            return -1;
         }
 
-        return res;
+        // re-arrange doubly linked list
+        Node n = map.get(key);
+        remove(n);
+        addAtTop(n);
+
+        // the key exists
+        return map.get(key).val;
     }
 
     public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            cache.remove(key);
+        System.out.println(key + "," + value);
+        Node n;
+        if (map.containsKey(key)) {
+            n = map.get(key);
+            n.val = value;
+            remove(n);
+        } else {
+            n = new Node(key, value);
+            map.put(key, n);
+
+            if (map.size() > capacity) {
+                map.remove(end.key);
+                remove(end);
+            }
         }
-        cache.put(key, value);
+
+        addAtTop(n);
+    }
+
+    public void remove(Node node) {
+        // if the node is the first node
+        if (node.prev == null) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+
+        // if the node is the last node
+        if (node.next == null) {
+            end = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
+    }
+
+    public void addAtTop(Node node) {
+        node.prev = null;
+
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+
+        if (end == null) {
+            end = head;
+        }
+    }
+
+    static class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
--- src/test/java/com/booknara/problem/hash/LRUCacheTest.java
+++ src/test/java/com/booknara/problem/hash/LRUCacheTest.java
@@ -17,17 +17,5 @@ public class LRUCacheTest {
         Assert.assertEquals(-1, cache.get(1));       // returns -1 (not found)
         Assert.assertEquals(3, cache.get(3));       // returns 3
         Assert.assertEquals(4, cache.get(4));       // returns 4
-
-        LRUCache2 cache2 = new LRUCache2(2);
-
-        cache2.put(1, 1);
-        cache2.put(2, 2);
-        Assert.assertEquals(1, cache2.get(1));       // returns 1
-        cache2.put(3, 3);    // evicts key 2
-        Assert.assertEquals(-1, cache2.get(2));       // returns -1 (not found)
-        cache2.put(4, 4);    // evicts key 1
-        Assert.assertEquals(-1, cache2.get(1));       // returns -1 (not found)
-        Assert.assertEquals(3, cache2.get(3));       // returns 3
-        Assert.assertEquals(4, cache2.get(4));       // returns 4
     }
 }
