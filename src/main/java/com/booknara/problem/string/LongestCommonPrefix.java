03766a7768ea86cca131415c598bd5ce4e26299e
Daehee Han <bookdori81@gmail.com>
2020-09-22 18:35:30 -0700

    Add Longest Common Prefix problem

--- src/main/java/com/booknara/problem/string/LongestCommonPrefix.java
+++ src/main/java/com/booknara/problem/string/LongestCommonPrefix.java
@@ -5,6 +5,7 @@ package com.booknara.problem.string;
  * https://leetcode.com/problems/longest-common-prefix/
  */
 public class LongestCommonPrefix {
+    // Vertical scanning
     // T:O(n*m), S:O(1)
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
@@ -21,5 +22,29 @@ public class LongestCommonPrefix {
         }
 
         return strs[0];
+    }
+
+    // Horizontal scanning
+    public String longestCommonPrefix1(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+
+        int prefix = strs[0].length();
+
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() == 0) return "";
+            if (prefix == 0) return "";
+
+            int j = 0;
+            prefix = Math.min(prefix, strs[i].length());
+            while (j < prefix) {
+                if (strs[i - 1].charAt(j) != strs[i].charAt(j)) {
+                    prefix = j;
+                }
+                j++;
+            }
+        }
+
+        return strs[0].substring(0, prefix);
     }
 }
