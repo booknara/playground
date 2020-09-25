7f97939c9e811a0dc20c2b18128bd9aecb1f20fa
Daehee Han <bookdori81@gmail.com>
2020-09-24 23:54:20 -0700

    Add Closest Binary Search Tree Value problem

--- src/main/java/com/booknara/problem/search/binary/ClosestBinarySearchTreeValue.java
+++ src/main/java/com/booknara/problem/search/binary/ClosestBinarySearchTreeValue.java
@@ -2,8 +2,6 @@ package com.booknara.problem.search.binary;
 
 import com.booknara.problem.common.TreeNode;
 
-import java.util.Stack;
-
 /**
  * 270. Closest Binary Search Tree Value (Easy)
  * https://leetcode.com/problems/closest-binary-search-tree-value/
@@ -11,17 +9,17 @@ import java.util.Stack;
 public class ClosestBinarySearchTreeValue {
     // T:O(h), S:O(h)
     public int closestValue(TreeNode root, double target) {
-        if (root == null) return 0;
-
-        // Binary search
         int res = root.val;
-        while (root != null) {
-            res = Math.abs(target - res) > Math.abs(target - root.val) ? root.val : res;
 
-            if (root.val < target) {
-                root = root.right;
-            } else {
+        while (root != null) {
+            if (Math.abs(res - target) > Math.abs(root.val - target)) {
+                res = root.val;
+            }
+
+            if (target < root.val) {
                 root = root.left;
+            } else {
+                root = root.right;
             }
         }
 
@@ -55,40 +53,4 @@ public class ClosestBinarySearchTreeValue {
             traverse(node.left, target);
         }
     }
-
-    // T:O(n), S:O(n)
-    public int closestValue2(TreeNode root, double target) {
-        if (root == null) return 0;
-
-        Integer res = null;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
-        while (!stack.isEmpty() || cur != null) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
-
-            TreeNode node = stack.pop();
-            if (target == node.val) return node.val;
-
-            if (target < node.val) {
-                if (res == null) return node.val;   // the target is smaller than the smallest value
-
-                // get the answer btw the existing and node.val
-                if (target - res < node.val - target) {
-                    return res;
-                }
-                return node.val;
-            }
-
-            // target is bigger than node.val
-            res = node.val;
-            // System.out.println(node.val);
-            cur = node.right;
-        }
-
-        return res;
-    }
-
 }
