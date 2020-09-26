5dc97545fb3c1111eb9c926f51cf3e308f026a41
Daehee Han <bookdori81@gmail.com>
2020-09-26 04:04:19 -0700

    Add Valid Number problem

--- src/main/java/com/booknara/problem/math/ValidNumber.java
+++ src/main/java/com/booknara/problem/math/ValidNumber.java
@@ -8,14 +8,10 @@ public class ValidNumber {
     // T:O(n), S:O(1)
     public boolean isNumber(String s) {
         // input check
-        if (s == null || s.length() == 0) {
-            return true;
-        }
-
-        boolean dot = false, exponetial = false, number = false;
         s = s.trim();
-        if (s.length() == 0) return false;  // empty string with white space is not valid
+        if (s.length() == 0) return false;
 
+        boolean exp = false, point = false, number = false;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isLetter(c) && c != 'e') {
@@ -23,33 +19,47 @@ public class ValidNumber {
                 return false;
             }
 
-            if (c == ' ') return false;   // invalid if there is white space in the middle
+            if (c == ' ') return false;
+            // the last character condition
             if (i == s.length() - 1 && !Character.isDigit(c) && !number) {
-                return false; // the last character should be number or . with number beforehand(e.g. "3.")
+                return false;
             }
 
-            if (Character.isDigit(c)) {
+            if (c >= '0' && c <= '9') {
                 number = true;
-            } else if (c == '.') {
-                // if (!number) return false; // '.' appears with any number before. Note: allows ".1" valid
-                if (dot|| exponetial) return false; // afer 'e', floating number is invalid
-                dot = true;
+                // number
             } else if (c == 'e') {
-                if (!number) return false; // 'e' appears with any number before
-                if (exponetial) return false;   // two times 'e'
+                // exponent
                 if (i == s.length() - 1) return false;
-
-                // once 'e' appears, '+', '-' can appear again
+                if (!number) return false;
+                if (exp) return false;
+                exp = true;
                 number = false;
-                exponetial = true;
-                dot = false;        // "32.e-80123" is valid
+            } else if (c == '.') {
+                // point
+                if (point || exp) return false;
+                point = true;
             } else if (c == '-' || c == '+') {
-                if (i != 0 && s.charAt(i - 1) != 'e') {
-                    return false; // negative or positive are only allowed at the first or next to 'e'
-                }
+                // sign is only allowed to appear the first or next to exponent
+                if (i - 1 < 0 || s.charAt(i - 1) == 'e') continue;
+                return false;
             }
         }
 
         return true;
     }
 }
+/**
+ Decimal number
+ 1. Number
+ 2. Positive/Negative sign '+/-'
+ 3. Exponent e
+ 4. Decimal point .
+
+ " " -> false
+ ".1" -> true
+ "." -> false
+ "3." -> true
+ "0e" -> false
+ "13 45" -> false
+ */
