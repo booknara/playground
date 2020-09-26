f2559073af6a44fdb3949a247824f10f3101737a
Daehee Han <bookdori81@gmail.com>
2020-09-25 23:38:01 -0700

    Add Integer to English Words problem

--- src/main/java/com/booknara/problem/string/IntegerToEnglishWords.java
+++ src/main/java/com/booknara/problem/string/IntegerToEnglishWords.java
@@ -10,99 +10,90 @@ public class IntegerToEnglishWords {
         if (num == 0) return "Zero";
 
         StringBuilder builder = new StringBuilder();
-        int unit = 0;
-        // 1234567891
+        int count = 0;
         while (num > 0) {
-            // 1_000_000
-            if (num % 1000 != 0) {
-                String s = " " + getNumber(num % 1000) + " " + getUnit(unit);
-                builder.insert(0, s);
+            int mod = num % 1000;
+            num /= 1000;
+
+            if (mod > 0) {
+                builder.insert(0, getValue(mod) + " " + getUnit(count) + " ");
             }
 
-            num /= 1000;
-            unit++;
+            count++;
+        }
+
+        return builder.toString().trim();
+    }
+
+    public String getValue(int num) {
+        StringBuilder builder = new StringBuilder();
+        // 917
+        int hundred = num / 100;
+        if (hundred > 0) {
+            builder.append(getNumber(hundred)).append(" ").append("Hundred").append(" ");
+        }
+        num %= 100;
+        if (num >= 11 && num <= 19) {
+            builder.append(getTeen(num));
+            return builder.toString();
+        }
+
+        int ten = num / 10;
+        if (ten > 0) {
+            builder.append(getTen(ten)).append(" ");
+        }
+
+        int one = num % 10;
+        if (one > 0) {
+            builder.append(getNumber(one));
         }
 
         return builder.toString().trim();
     }
 
     public String getNumber(int num) {
-        StringBuilder res = new StringBuilder();
-        // 123
-        int h = num / 100;
-        if (h != 0) {
-            // 123 -> 23
-            num %= 100;
-            res.append(" " + getSingleWord(h) + " Hundred");
-        }
-
-        if (num > 10 && num < 20) {
-            // 13
-            //return res.append(" " + getTeenWord(num));
-            return res.append(" " + getTeenWord(num)).toString().trim();
-        } else {
-            // 23 or 30
-            int t = num / 10;
-            if (t != 0) {
-                res.append(" " + getTenWord(t));
-                num %= 10;
-            }
-        }
-
-        if (num > 0) {
-            res.append(" " + getSingleWord(num));
-        }
-
-        return res.toString().trim();
-    }
-
-    public String getUnit(int c) {
-        if (c == 0) return "";
-        if (c == 1) return "Thousand";
-        if (c == 2) return "Million";
-        if (c == 3) return "Billion";
-
+        if (num == 1) return "One";
+        if (num == 2) return "Two";
+        if (num == 3) return "Three";
+        if (num == 4) return "Four";
+        if (num == 5) return "Five";
+        if (num == 6) return "Six";
+        if (num == 7) return "Seven";
+        if (num == 8) return "Eight";
+        if (num == 9) return "Nine";
         return "";
     }
 
-    public String getTeenWord(int n) {
-        if (n == 11) return "Eleven";
-        if (n == 12) return "Twelve";
-        if (n == 13) return "Thirteen";
-        if (n == 14) return "Fourteen";
-        if (n == 15) return "Fifteen";
-        if (n == 16) return "Sixteen";
-        if (n == 17) return "Seventeen";
-        if (n == 18) return "Eighteen";
-        if (n == 19) return "Nineteen";
-
+    public String getTen(int num) {
+        if (num == 1) return "Ten";
+        if (num == 2) return "Twenty";
+        if (num == 3) return "Thirty";
+        if (num == 4) return "Forty";
+        if (num == 5) return "Fifty";
+        if (num == 6) return "Sixty";
+        if (num == 7) return "Seventy";
+        if (num == 8) return "Eighty";
+        if (num == 9) return "Ninety";
         return "";
     }
 
-    public String getTenWord(int n) {
-        if (n == 1) return "Ten";
-        if (n == 2) return "Twenty";
-        if (n == 3) return "Thirty";
-        if (n == 4) return "Forty";
-        if (n == 5) return "Fifty";
-        if (n == 6) return "Sixty";
-        if (n == 7) return "Seventy";
-        if (n == 8) return "Eighty";
-        if (n == 9) return "Ninety";
-
+    public String getTeen(int num) {
+        if (num == 11) return "Eleven";
+        if (num == 12) return "Twelve";
+        if (num == 13) return "Thirteen";
+        if (num == 14) return "Fourteen";
+        if (num == 15) return "Fifteen";
+        if (num == 16) return "Sixteen";
+        if (num == 17) return "Seventeen";
+        if (num == 18) return "Eighteen";
+        if (num == 19) return "Nineteen";
         return "";
     }
 
-    public String getSingleWord(int n) {
-        if (n == 1) return "One";
-        if (n == 2) return "Two";
-        if (n == 3) return "Three";
-        if (n == 4) return "Four";
-        if (n == 5) return "Five";
-        if (n == 6) return "Six";
-        if (n == 7) return "Seven";
-        if (n == 8) return "Eight";
-        if (n == 9) return "Nine";
+    public String getUnit(int num) {
+        if (num == 1) return "Thousand";
+        if (num == 2) return "Million";
+        if (num == 3) return "Billion";
 
         return "";
     }
