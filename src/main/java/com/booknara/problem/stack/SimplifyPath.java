433ae69ab14c646932be8b3237c1146eee33ff69
Daehee Han <bookdori81@gmail.com>
2020-09-28 00:40:22 -0700

    Add Simplify Path problem

--- src/main/java/com/booknara/problem/stack/SimplifyPath.java
+++ src/main/java/com/booknara/problem/stack/SimplifyPath.java
@@ -1,12 +1,14 @@
 package com.booknara.problem.stack;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * 71. Simplify Path (Medium)
  * https://leetcode.com/problems/simplify-path/
  */
 public class SimplifyPath {
+    // T:O(n), S:O(n)
     public String simplifyPath(String path) {
         if (path == null || path.length() == 0) {
             return path;
@@ -37,4 +39,42 @@ public class SimplifyPath {
 
         return builder.length() == 0 ? "/" : builder.toString();
     }
+
+    // T:O(n), S:O(n)
+    public String simplifyPath1(String path) {
+        if (path == null || path.length() == 0) return "";
+
+        Stack<String> stack = new Stack<>();
+        // #2
+        path = path.replaceAll("//", "/");
+        String[] array = path.split("/");
+        for (String s: array) {
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else if (s.equals(".")) {
+                // skip
+            } else {
+                if (s.length() != 0) stack.push(s);
+            }
+        }
+
+        if (stack.isEmpty()) return "/";
+
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            String dir = "/" + stack.pop();
+            res.insert(0, dir);
+        }
+
+        return res.toString();
+    }
 }
+/**
+ Rule
+ #1: no trailing /
+ #2: // -> /
+ #3: . -> current
+ #4: .. -> directory up
+
+
+ */
