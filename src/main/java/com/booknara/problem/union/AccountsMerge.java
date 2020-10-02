ec2672826638944aebec441535184afca5d7b327
Daehee Han <bookdori81@gmail.com>
2020-10-02 00:15:19 -0700

    Add Accounts Merge problem

--- src/main/java/com/booknara/problem/union/AccountsMerge.java
+++ src/main/java/com/booknara/problem/union/AccountsMerge.java
@@ -14,26 +14,22 @@ public class AccountsMerge {
 
         int n = accounts.size();
         int[] root = new int[n];
-        for (int i = 0; i < root.length; i++) {
+        // [0,1,2,3]
+        for (int i = 0; i < n; i++) {
             root[i] = i;
         }
 
-        String[] name = new String[accounts.size()];
-
-        // Key: Email, Value: Index
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < accounts.size(); i++) {
+        for (int i = 0; i < n; i++) {
             List<String> account = accounts.get(i);
-            name[i] = account.get(0);
-
-            for (int j = 1; j < account.size(); j++) { // j = 0 : name
+            for (int j = 1; j < account.size(); j++) {
                 String email = account.get(j);
                 if (map.containsKey(email)) {
-                    int parentIdx = map.get(email);
-                    int root1 = findRoot(root, parentIdx);
+                    int root1 = findRoot(root, map.get(email));
                     int root2 = findRoot(root, i);
+                    // union
                     if (root1 != root2) {
-                        root[root2] = root[root1];
+                        root[root2] = root1;
                     }
                 } else {
                     map.put(email, i);
@@ -41,59 +37,68 @@ public class AccountsMerge {
             }
         }
 
-        Map<Integer, TreeSet<String>> sortedMap = new HashMap<>();
-        for (int i = 0; i < root.length; i++) {
-            int parentIndex = findRoot(root, i);
-            TreeSet<String> tree = sortedMap.getOrDefault(parentIndex, new TreeSet<>());
-            List<String> list = accounts.get(i);
-            for (int j = 1; j < list.size(); j++) {
-                tree.add(list.get(j));
+        // [0,1,0,3]
+        Map<Integer, TreeSet<String>> temp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int r = findRoot(root, i);
+
+            List<String> account = accounts.get(i);
+            for (int j = 1; j < account.size(); j++) {
+                String email = account.get(j);
+                TreeSet<String> treeSet = temp.getOrDefault(r, new TreeSet<>());
+                treeSet.add(email);
+                temp.put(r, treeSet);
             }
-            sortedMap.put(parentIndex, tree);
         }
 
         List<List<String>> res = new ArrayList<>();
-        for (Map.Entry<Integer, TreeSet<String>> email: sortedMap.entrySet()) {
+        for (Map.Entry<Integer, TreeSet<String>> entry: temp.entrySet()) {
             List<String> list = new ArrayList<>();
-            list.add(name[email.getKey()]);
-            list.addAll(email.getValue());
+            int key = entry.getKey();
+            list.add(accounts.get(key).get(0)); // name
+            list.addAll(entry.getValue());  // list of emails
+
             res.add(list);
         }
 
         return res;
     }
 
-    public void union(int[] root, int a, int b) {
-        root[findRoot(root, a)] = root[findRoot(root, b)];
-    }
-
-    public int findRoot(int[] root, int x) {
-        if (x != root[x]) {
-            root[x] = findRoot(root, root[x]);
+    public int findRoot(int[] root, int idx) {
+        while (root[idx] != idx) {
+            // path compression
+            //root[idx] = root[root[idx]];
+            idx = root[idx];
         }
-        return root[x];
+
+        return idx;
     }
 }
 /**
- accounts =
- [
- ["John", "johnsmith@mail.com", "john00@mail.com"],
- ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
- ["John", "johnnybravo@mail.com"],
- ["Mary", "mary@mail.com"]]
+ accounts = [
+ 0. ["John", "johnsmith@mail.com", "john00@mail.com"],
+ 1. ["John", "johnnybravo@mail.com"],
+ 2. ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+ 3. ["Mary", "mary@mail.com"]
+ ]
 
  [0,1,2,3]
+ [0,1,0,3]
 
- [0,0,2,3]
+ Map<String, Integer>
+ "johnsmith@mail.com", 0
+ "john00@mail.com", 0
+ "johnnybravo@mail.com", 1
 
- []
+ "johnsmith@mail.com" 2 -> 0
+ "john_newyork@mail.com" 2
+ "mary@mail.com" 3
 
- making graph
+ Map<Integer, TreeSet<String>>
+
  [
- "johnsmith@mail.com" -> 0, 1
- "john00@mail.com" -> 0
- "john_newyork@mail.com" -> 1
- "johnnybravo@mail.com" -> 2
- "mary@mail.com" -> 3
+ ["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
+ ["John", "johnnybravo@mail.com"],
+ ["Mary", "mary@mail.com"]
  ]
  */
