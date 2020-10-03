8a7a785969271e2f738b93613fe6482cec8c1495
Daehee Han <bookdori81@gmail.com>
2020-10-03 01:08:18 -0700

    Add Word Break II problem

--- src/main/java/com/booknara/problem/dp/WordBreakII.java
+++ src/main/java/com/booknara/problem/dp/WordBreakII.java
@@ -7,7 +7,7 @@ import java.util.*;
  * https://leetcode.com/problems/word-break-ii/
  */
 public class WordBreakII {
-    // T:O(n^2 + 2^n + w, w: the number of words
+    // T:O(n^2 + 2^n + w), w: the number of words
     public List<String> wordBreak(String s, List<String> wordDict) {
         // input check
         List<String> res = new ArrayList<>();
@@ -56,18 +56,66 @@ public class WordBreakII {
         return dp.get(s.length());
     }
 
-    // Backtracking, TLE
     public List<String> wordBreak1(String s, List<String> wordDict) {
+        // input check, s = non-empty string, wordDict = non-empty list
+        Set<String> set = new HashSet<>();
+        // Character check
+        int[] bucket = new int[128];
+        for (String word: wordDict) {
+            for (char c: word.toCharArray()) {
+                bucket[c]++;
+            }
+
+            set.add(word);
+        }
+
+        for (char c: s.toCharArray()) {
+            // a character in s not present in wordDict
+            if (bucket[c] == 0) {
+                return new ArrayList<>();
+            }
+        }
+
+        Map<Integer, List<String>> map = new HashMap<>();
+        map.put(0, new ArrayList<>());
+
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(i)) continue;
+
+            for (int j = i; j < n; j++) {
+                String sub = s.substring(i, j + 1);
+                if (set.contains(sub)) {
+                    // get the previous i th list
+                    List<String> cur = new ArrayList<>();
+                    if (i == 0) {
+                        cur.add(sub);
+                    } else {
+                        List<String> prev = map.get(i);
+                        for (String str: prev) {
+                            cur.add(str + " " + sub);
+                        }
+                    }
+
+                    List<String> list = map.getOrDefault(j + 1, new ArrayList<>());
+                    list.addAll(cur);
+                    map.put(j + 1, list);
+                }
+            }
+        }
+
+        return map.containsKey(n) ? map.get(n) : new ArrayList<>();
+    }
+
+    // Backtracking, TLE
+    public List<String> wordBreak2(String s, List<String> wordDict) {
         // input check
         List<String> res = new ArrayList<>();
         if (s == null || s.length() == 0) {
             return res;
         }
 
-        Set<String> set = new HashSet<>();
-        for (String w: wordDict) {
-            set.add(w);
-        }
+        Set<String> set = new HashSet<>(wordDict);
 
         search(s, 0, new StringBuilder(), set, res);
         return res;
