e1fb5da74d2e73bfe345102a3183b22036573e10
Daehee Han <bookdori81@gmail.com>
2020-10-03 01:33:10 -0700

    Add Subarray Sum Equals K problem

--- src/main/java/com/booknara/problem/array/SubarraySumEqualsK.java
+++ src/main/java/com/booknara/problem/array/SubarraySumEqualsK.java
@@ -11,67 +11,43 @@ public class SubarraySumEqualsK {
     // Using Hash(one pass)
     // T:O(n), S:O(n)
     public int subarraySum(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int count = 0;
-        int sum = 0;
+        // input check, nums = non-empty array
+        int n = nums.length;
+        // Key: Sum, Value: the count of sum
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
-        for (int i = 0; i < nums.length; i++) {
+        int sum = 0, res = 0;
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
             if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
+                res += map.get(sum - k);
             }
 
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            int count = map.getOrDefault(sum, 0);
+            map.put(sum, count + 1);
         }
 
-        return count;
-    }
-
-    // O(n^2) without space
-    public int subarraySum1(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    // O(n^2) with space
-    public int subarraySum2(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int[] sum = new int[nums.length + 1];
-        sum[0] = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum[i + 1] = sum[i] + nums[i];
-        }
-
-        int count = 0;
-        for (int i = 0; i < sum.length; i++) {
-            for (int j = i + 1; j < sum.length; j++) {
-                if (sum[j] - sum[i] == k) {
-                    count++;
-                }
-            }
-        }
-
-        return count;
+        return res;
     }
 }
+/**
+ Input:nums =
+ [1,1,1], k = 2
+ [0,1,2,3]
+
+ [
+ 0:1
+ 1:1 -> sum = 1,
+ 2:1 -> sum = 2, 2 - k(2) = 0, res = 1
+ 3:1 -> sum = 3, 3 - k(2) = 1, res = 2
+ ]
+
+ [0,0], k = 0
+ [
+ 0:2 // init
+ sum = 0 res = 1
+ sum = 0 res = 1 + 2 = 3
+ ]
+
+ Output: 2
+ */
