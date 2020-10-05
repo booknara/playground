41769151e2b83839bdc4a344899178961e49d981
Daehee Han <bookdori81@gmail.com>
2020-10-05 01:52:24 -0700

    Add Find Minimum in Rotated Sorted Array problem

--- src/main/java/com/booknara/problem/search/binary/FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/booknara/problem/search/binary/FindMinimumInRotatedSortedArray.java
@@ -24,4 +24,30 @@ public class FindMinimumInRotatedSortedArray {
 
         return nums[r];
     }
+
+    public int findMin1(int[] nums) {
+        // input check 0, 1 edge case handling
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+
+            // early detection
+            if (m - 1 >= 0 && nums[m - 1] > nums[m]) return nums[m];
+
+            if (nums[l] <= nums[m]) {
+                // sorted area
+                if (nums[m] > nums[r]) {
+                    l = m + 1;  // not pivot area
+                } else {
+                    r = m - 1;  // pivot area
+                }
+            } else {
+                // pivot area
+                r = m - 1;
+            }
+        }
+
+        return nums[l];
+    }
+
 }
