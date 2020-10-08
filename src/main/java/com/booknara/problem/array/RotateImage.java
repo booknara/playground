23d0670ae3111a3e03973490d1fa681c41e2ec1a
Daehee Han <bookdori81@gmail.com>
2020-10-07 23:02:59 -0700

    Add Rotate Image problem

--- src/main/java/com/booknara/problem/array/RotateImage.java
+++ src/main/java/com/booknara/problem/array/RotateImage.java
@@ -5,6 +5,7 @@ package com.booknara.problem.array;
  * https://leetcode.com/problems/rotate-image/
  */
 public class RotateImage {
+    // T:O(n^2), S:O(1)
     public void rotate(int[][] matrix) {
         if (matrix == null || matrix.length == 0) {
             return;
@@ -30,4 +31,40 @@ public class RotateImage {
             }
         }
     }
+
+    // T:O(n^2), S:O(1)
+    public void rotate1(int[][] matrix) {
+        int n = matrix.length;
+
+        int last = n - 1;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[last - j][i];
+                matrix[last - j][i] = matrix[last - i][last - j];
+                matrix[last - i][last - j] = matrix[j][last - i];
+                matrix[j][last - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
 }
+/**
+ Input: matrix = [
+ [1,2,3]
+ [4,5,6]
+ [7,8,9]
+
+ [7,4,1]
+ [8,5,2]
+ [9,6,3]
+ ]
+
+ (0,0)
+ temp = (2,0)
+ (2,0) = (2,2)
+ (2,2) = (0,2)
+ (0,2) = (0,0)
+ (0,0) = temp
+
+ Output: [[7,4,1],[8,5,2],[9,6,3]]
+ */
