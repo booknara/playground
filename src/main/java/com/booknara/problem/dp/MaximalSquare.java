11d2c5d2e61cd89aec90d5e3f39d29e1c20255cc
Daehee Han <bookdori81@gmail.com>
2020-10-10 21:49:14 -0700

    Add Maximal Square problem

--- src/main/java/com/booknara/problem/dp/MaximalSquare.java
+++ src/main/java/com/booknara/problem/dp/MaximalSquare.java
@@ -57,4 +57,32 @@ public class MaximalSquare {
 
         return max;
     }
+
+    // T:O(m * n), S:O(m * n)
+    public int maximalSquare2(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int res = 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') continue;
+
+                if (i - 1 >= 0 && j - 1 >= 0 &&
+                        matrix[i - 1][j] == '1' && matrix[i][j - 1] == '1' && matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                            Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                } else {
+                    dp[i][j] = 1;
+                }
+
+                res = Math.max(res, dp[i][j] * dp[i][j]);
+            }
+        }
+
+        return res;
+    }
 }
