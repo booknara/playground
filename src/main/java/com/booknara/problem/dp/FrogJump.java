d9ca221715344e415ca1f07401f7021a5150d410
Daehee Han <bookdori81@gmail.com>
2020-10-12 00:44:35 -0700

    Add Frog Jump problem

--- src/main/java/com/booknara/problem/dp/FrogJump.java
+++ src/main/java/com/booknara/problem/dp/FrogJump.java
@@ -31,4 +31,33 @@ public class FrogJump {
 
         return !memo.get(stones[n - 1]).isEmpty();
     }
+
+    // T:O(n^2), S:O(n^2)
+    public boolean canCross1(int[] stones) {
+        // input check, stones >= 2
+        // Key: stone, Value: possible steps to move
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int s: stones) {
+            Set<Integer> set = new HashSet<>();
+            map.put(s, set);
+        }
+
+        map.get(0).add(0);  // init
+        for (int i = 0; i < stones.length; i++) {
+            Set<Integer> steps = map.get(stones[i]);
+            for (Integer step: steps) {
+                // step: 0
+                for (int j = step - 1; j < step + 2; j++) {
+                    if (j <= 0) continue;
+
+                    if (map.containsKey(stones[i] + j)) {
+                        map.get(stones[i] + j).add(j);
+                    }
+                }
+            }
+        }
+
+        return map.get(stones[stones.length - 1]).size() != 0;
+    }
+
 }
