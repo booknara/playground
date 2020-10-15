f61d3360e04c9176db210f3fb870ccae11ea6e47
Daehee Han <bookdori81@gmail.com>
2020-10-14 21:30:56 -0700

    Add 3Sum problem

--- src/main/java/com/booknara/problem/twopointers/ThreeSum.java
+++ src/main/java/com/booknara/problem/twopointers/ThreeSum.java
@@ -41,4 +41,38 @@ public class ThreeSum {
 
         return res;
     }
+
+    // T:O(n^2), S:O(1)
+    public List<List<Integer>> threeSum1(int[] nums) {
+        // input check
+        List<List<Integer>> res = new ArrayList<>();
+
+        // required at least 3 elements
+        if (nums == null || nums.length < 3) return res;
+
+        // ascending
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            int pivot = nums[i];
+            if (i - 1 >= 0 && nums[i - 1] == nums[i]) continue; // avoid the duplicated triplets
+
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                if (pivot + nums[l] + nums[r] == 0) {
+                    res.add(Arrays.asList(pivot, nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    l++;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    r--;
+                } else if (pivot + nums[l] + nums[r] < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+
+        return res;
+    }
 }
