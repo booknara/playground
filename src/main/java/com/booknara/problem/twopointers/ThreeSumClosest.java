03826b119b1a4b492c7c6cb048bd8a5d90c1213f
Daehee Han <bookdori81@gmail.com>
2020-10-14 21:48:28 -0700

    Add 3Sum Closest problem

--- src/main/java/com/booknara/problem/twopointers/ThreeSumClosest.java
+++ src/main/java/com/booknara/problem/twopointers/ThreeSumClosest.java
@@ -38,34 +38,55 @@ public class ThreeSumClosest {
         return (int) sum;
     }
 
+    // T:(n^2), S:O(1)
     public int threeSumClosest1(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        // input check, the element of nums * 3 <= Integer.MAX_VALUE;
+        if (nums == null || nums.length < 3) return 0;
 
-        int ans = 3 * 10 * 10 * 10; // max number of array element 10^3 * 3(numbers)
-        int closest = Integer.MAX_VALUE;
+        // ascending
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1, k = nums.length - 1;
-            while (j < k) {
-                if (nums[i] + nums[j] + nums[k]  == target) {
-                    return target;
+        int n = nums.length;
+        int res = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 2; i++) {
+            int pivot = nums[i];
+            if (i - 1 >= 0 && nums[i - 1] == nums[i]) continue;
+
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = pivot + nums[l] + nums[r];
+                if (sum == target) return target;
+
+                if (Math.abs(sum - target) < diff) {
+                    diff = Math.abs(sum - target);
+                    res = sum;
                 }
 
-                if (Math.abs(nums[i] + nums[j] + nums[k] - target) < closest) {
-                    closest = Math.abs(nums[i] + nums[j] + nums[k] - target);
-                    ans = nums[i] + nums[j] + nums[k];
-                }
-
-                if (nums[i] + nums[j] + nums[k] < target) {
-                    j++;
+                if (sum < target) {
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    // increment
+                    l++;
                 } else {
-                    k--;
+                    while (l < r && nums[r - 1] == nums[r]) r--;
+                    // decrement
+                    r--;
                 }
             }
         }
 
-        return ans;
+        return res;
     }
 }
+/**
+ Input: nums = [-1,2,1,-4], target = 1
+ Two pointers
+ [-4,-1,1,2]
+ .
+ -4 + -1 + 2 = -3 < target(1)
+ -4 + 1 + 2 = -1 < target (1)
+
+ -1 + 1 + 2 = 2 > target(2)
+
+ Output: 2
+ Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
