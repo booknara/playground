7cd54cb52dda4dcb98d7a73533d20da24fa7cbdb
Daehee Han <bookdori81@gmail.com>
2020-10-15 01:06:14 -0700

    Add Add Bold Tag in String problem

--- src/main/java/com/booknara/problem/string/AddBoldTagInString.java
+++ src/main/java/com/booknara/problem/string/AddBoldTagInString.java
@@ -1,5 +1,7 @@
 package com.booknara.problem.string;
 
+import java.util.Arrays;
+
 /**
  * 616. Add Bold Tag in String (Medium)
  * https://leetcode.com/problems/add-bold-tag-in-string/
@@ -60,8 +62,62 @@ public class AddBoldTagInString {
 
         return builder.toString();
     }
+
+    // T:O(n*m*avg(the length of dic), S:O(n)
+    public String addBoldTag1(String s, String[] dict) {
+        // input check
+        if (s == null || s.length() == 0) return "";
+
+        boolean[] mark = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            int max = 0;
+            for (String d: dict) {
+                if (s.startsWith(d, i)) {
+                    max = Math.max(max, d.length());
+                }
+            }
+
+            if (max > 0) Arrays.fill(mark, i, i + max, true);
+        }
+
+        String startTag = "<b>";
+        String endTag = "</b>";
+        boolean start = false;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (mark[i]) {
+                if (!start) {
+                    builder.append(startTag);
+                    start = true;
+                }
+            } else {
+                if (start) {
+                    builder.append(endTag);
+                    start = false;
+                }
+            }
+
+            builder.append(s.charAt(i));
+        }
+
+        if (start) {
+            builder.append(endTag);
+        }
+
+        return builder.toString();
+    }
 }
 /**
+ Input:
  s = "abcxyz123"
- "111000111"
+ ...   ...
+ dict = ["abc","123"]
+ Output: "<b>abc</b>xyz<b>123</b>"
+
+ Input:
+ s = "aaabbcc"
+ ......
+ dict = ["aaa","aab","bc"]
+ Output: "<b>aaabbc</b>c"
+
  */
