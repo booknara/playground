1ed93ee472effbbf3045e0b29c028328f9c1a68d
Daehee Han <bookdori81@gmail.com>
2020-10-17 00:21:33 -0700

    Add Basic Calculator problem

--- src/main/java/com/booknara/problem/stack/BasicCalculator.java
+++ src/main/java/com/booknara/problem/stack/BasicCalculator.java
@@ -7,7 +7,7 @@ import java.util.Stack;
  * https://leetcode.com/problems/basic-calculator/
  */
 public class BasicCalculator {
-    // T:O(n), S:O(2m, m = the number of '(')
+    // T:O(n), S:O(n)
     public int calculate(String s) {
         if (s == null || s.length() == 0) {
             return 0;
@@ -47,5 +47,60 @@ public class BasicCalculator {
         }
 
         return result + sign * num;
+    }
+
+    // T:O(n), S:O(n)
+    public int calculate1(String s) {
+        // input
+        s = s.trim();
+        return calc(s, 0).val;
+    }
+
+    public Pair calc(String s, int index) {
+        int sign = 1;
+        int num = 0;
+        int lastIndex = index;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = index; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') continue;
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            } else if (c == '(') {
+                Pair p = calc(s, i + 1);
+                num = p.val;
+                i = p.index;
+            } else if (c == ')') {
+                lastIndex = i;
+                break;
+            } else {
+                stack.push(sign * num);
+                // +, -
+                if (c == '+') sign = 1;
+                else sign = -1;
+
+                num = 0;
+            }
+        }
+
+        // last sign handling
+        stack.push(sign * num);
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+
+        return new Pair(res, lastIndex);
+    }
+
+    static class Pair {
+        int val;
+        int index;
+
+        Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
     }
 }
