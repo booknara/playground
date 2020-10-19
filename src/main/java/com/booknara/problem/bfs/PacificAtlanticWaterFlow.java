33d76c5b69c5bc2cfc8145654610eccb9190a43b
Daehee Han <bookdori81@gmail.com>
2020-10-19 16:37:38 -0700

    Add Pacific Atlantic Water Flow problem

--- src/main/java/com/booknara/problem/bfs/PacificAtlanticWaterFlow.java
+++ src/main/java/com/booknara/problem/bfs/PacificAtlanticWaterFlow.java
@@ -100,4 +100,93 @@ public class PacificAtlanticWaterFlow {
                     '}';
         }
     }
+
+    int[][] dirs = new int[][] {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+    };
+
+    // T:O(n^2), S:O(n^2)
+    public List<List<Integer>> pacificAtlantic1(int[][] matrix) {
+        // input check
+        List<List<Integer>> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return res;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] pVisited = new boolean[m][n];
+        boolean[][] aVisited = new boolean[m][n];
+        // m*n
+        // Pacific
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            int[] cell = new int[2];
+            cell[0] = 0;
+            cell[1] = i;
+            q.offer(cell);
+        }
+        for (int i = 0; i < m; i++) {
+            int[] cell = new int[2];
+            cell[0] = i;
+            cell[1] = 0;
+            q.offer(cell);
+        }
+
+        bfs(matrix, q, pVisited);
+
+        // Atlantic
+        q.clear();
+        for (int i = 0; i < n; i++) {
+            int[] cell = new int[2];
+            cell[0] = m - 1;
+            cell[1] = i;
+            q.offer(cell);
+        }
+        for (int i = 0; i < m; i++) {
+            int[] cell = new int[2];
+            cell[0] = i;
+            cell[1] = n - 1;
+            q.offer(cell);
+        }
+
+        bfs(matrix, q, aVisited);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pVisited[i][j] && aVisited[i][j]) {
+                    // both flowing
+                    res.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public void bfs(int[][] matrix, Queue<int[]> q, boolean[][] visited) {
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] cell = q.poll();
+                int row = cell[0];
+                int col = cell[1];
+                visited[row][col] = true;
+
+                // four directions
+                for (int[] dir: dirs) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    if (newRow < 0 || newCol < 0 || newRow >= matrix.length || newCol >= matrix[0].length
+                            || matrix[row][col] > matrix[newRow][newCol] || visited[newRow][newCol]) {
+                        continue;
+                    }
+
+                    q.offer(new int[] {newRow, newCol});
+                }
+            }
+        }
+    }
+
 }
