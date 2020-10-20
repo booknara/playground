3efe48b866bff938cec5abd066a6bbf21c6e0e85
Daehee Han <bookdori81@gmail.com>
2020-10-19 18:50:17 -0700

    Add Pow(x, n) problem

--- src/main/java/com/booknara/problem/math/Pow.java
+++ src/main/java/com/booknara/problem/math/Pow.java
@@ -47,4 +47,37 @@ public class Pow {
 
         return res;
     }
+    public double myPow2(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+
+        long num = n;
+        if (n < 0) {
+            num = -n;
+            x = 1 / x;
+        }
+
+        // x = 2.0
+        double res = 1.0;
+        while (num != 0) {
+            if ((num & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            num = num >>> 1;
+        }
+
+        return res;
+    }
 }
+/**
+ Input:
+ x = 2.00000
+ n = 10
+ (10)
+ 1.0 * (2.0 * 2.0) = 4.0
+ 4.0 * (4.0 * 4.0) = 16.0
+ 16.0 * 16.0 = 4.0 * 256 = 1024;
+ Output: 1024.00000
+ */
