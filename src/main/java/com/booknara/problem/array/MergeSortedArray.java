6b7eaadbe63c4a784ecbd1af1ac4c6ae45a49271
Daehee Han <bookdori81@gmail.com>
2020-10-21 11:56:30 -0700

    Add Merge Sorted Array problem

--- src/main/java/com/booknara/problem/array/MergeSortedArray.java
+++ src/main/java/com/booknara/problem/array/MergeSortedArray.java
@@ -28,6 +28,39 @@ public class MergeSortedArray {
             }
         }
     }
+
+    // T:O(n+m), S:O(1)
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) return;
+
+        int i = m - 1;
+        int j = n - 1;
+        int index = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums1[index] = nums2[j];
+                j--;
+            } else {
+                nums1[index] = nums1[i];
+                i--;
+            }
+            index--;
+        }
+
+        // the remaining i
+        while (i >= 0) {
+            nums1[index] = nums1[i];
+            index--;
+            i--;
+        }
+
+        // the remaining j
+        while (j >= 0) {
+            nums1[index] = nums2[j];
+            index--;
+            j--;
+        }
+    }
 }
 /**
  Input:
