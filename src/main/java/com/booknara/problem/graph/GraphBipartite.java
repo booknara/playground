56755129d4d4bf8d37314e746197f3253e0708ca
Daehee Han <bookdori81@gmail.com>
2020-10-21 23:28:18 -0700

    Add Is Graph Bipartite? problem

--- src/main/java/com/booknara/problem/graph/GraphBipartite.java
+++ src/main/java/com/booknara/problem/graph/GraphBipartite.java
@@ -10,48 +10,73 @@ import java.util.Queue;
 public class GraphBipartite {
     // T:O(v + e), S:O(v)
     public boolean isBipartite(int[][] graph) {
-        // input check
-        if (graph == null || graph.length == 0) return true;
+        // input check, graph.length >= 1
+        int n = graph.length;
 
-        // O: undetermined, 1:red:, 2:blue
-        // [0,0,0,0]
-        int[] colors = new int[graph.length];
-        for (int i = 0; i < graph.length; i++) {
+        // 1:red, 2:blue, 0:undetermined
+        int[] colors = new int[n];
+        for (int i = 0; i < n; i++) {
             if (colors[i] == 0) {
-                if (!isBipartite(graph, colors, i)) return false;
+                // 0:undetermined
+                if (!bfs(graph, colors, i)) return false;
             }
         }
 
         return true;
     }
 
-    public boolean isBipartite(int[][] graph, int[] colors, int i) {
-        Queue<Integer> q = new LinkedList<>();
-        colors[i] = 1;
-        q.offer(i);
-        while (!q.isEmpty()) {
-            int n = q.poll();
-            int color = colors[n];
-            for (int nei: graph[n]) {
-                if (colors[nei] == color) return false; // the neighbor has the same color, valid
+    public boolean bfs(int[][] graph, int[] colors, int n) {
+        Queue<Element> q = new LinkedList<>();
+        colors[n] = 1;
+        q.offer(new Element(n, 1));
 
-                if (colors[nei] == 0) {
-                    // undetermined
-                    colors[nei] = (color == 1) ? 2 : 1;
-                    q.offer(nei);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Element e = q.poll();
+                // color = e.color
+                // check connected edge
+                for (int nei: graph[e.index]) {
+                    // 0, same, different
+                    if (colors[nei] == e.color) {
+                        return false;
+                    }
+
+                    if (colors[nei] == 0) {
+                        colors[nei] = e.color == 1 ? 2 : 1;
+                        q.offer(new Element(nei, colors[nei]));
+                    }
                 }
             }
         }
 
         return true;
     }
+
+    static class Element {
+        int index;
+        int color;
+        Element (int index, int color) {
+            this.index = index;
+            this.color = color;
+        }
+    }
 }
 /**
- [0,0,0,0]
- Input: [
+ Input: graph = [[1,3],[0,2],[1,3],[0,2]]
+ Generate adjacent relation
+
+
  0:[1,3]
  1:[0,2]
  2:[1,3]
  3:[0,2]
- ]
+
+ int[] color = new int[graph.length];    // 1:red, 2:blue, 0:undetermined
+ [0,1,2,3]
+ [1,2,1,2]
+
+ Queue<Integer> q = new LinkedList<>();
+ // 0 -> 1,3 -> 2
+ Output: true
  */
