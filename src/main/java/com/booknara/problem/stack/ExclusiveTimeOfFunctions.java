c2dffebd841b705a48f1b7777a2dd8928bcef7fb
Daehee Han <bookdori81@gmail.com>
2020-10-21 21:15:59 -0700

    Add Exclusive Time of Functions problem

--- src/main/java/com/booknara/problem/stack/ExclusiveTimeOfFunctions.java
+++ src/main/java/com/booknara/problem/stack/ExclusiveTimeOfFunctions.java
@@ -42,6 +42,42 @@ public class ExclusiveTimeOfFunctions {
 
         return res;
     }
+
+    // T:O(n), S:O(n)
+    public int[] exclusiveTime1(int n, List<String> logs) {
+        // input check, n >= 1, logs.size() >= 1
+
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        String[] init = logs.get(0).split(":");
+        stack.push(Integer.parseInt(init[0]));   // function id
+        int prevTime = Integer.parseInt(init[2]);
+        //if (!init[1].equals("start"))             // illegal format
+
+        for (int i = 1; i < logs.size(); i++) {
+            String[] array = logs.get(i).split(":");
+            int funcId = Integer.parseInt(array[0]);
+            int time = Integer.parseInt(array[2]);
+
+            // two cases
+            if (array[1].equals("start")) {
+                // start
+                if (!stack.isEmpty()) {
+                    res[stack.peek()] += (time - prevTime);
+                }
+
+                stack.push(funcId);
+                prevTime = time;    // 2
+            } else {
+                // end
+                res[stack.pop()] += (time - prevTime + 1);
+                prevTime = time + 1;
+            }
+        }
+
+        return res;
+    }
 }
 /**
  function Id: 0 ~ n - 1
