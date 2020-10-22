a757ae1dc72ce95e32dae4a605ed1a1038746e01
Daehee Han <bookdori81@gmail.com>
2020-10-22 16:38:50 -0700

    Add Maximum Product Subarray problem

--- src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
@@ -5,8 +5,48 @@ package com.booknara.problem.dp;
  * https://leetcode.com/problems/maximum-product-subarray/
  */
 public class MaxProductSubArray {
-    // T:O(n), S:O(1)
     public int maxProduct(int[] nums) {
+        // input check
+        if (nums == null || nums.length == 0) return 0;
+
+        int max = Integer.MIN_VALUE;
+        int product = 1;
+        for (int i = 0; i < nums.length; i++) {
+            product = product * nums[i];
+            max = Math.max(max, product);
+            if (product == 0) {
+                product = 1;    // reset
+            }
+        }
+
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            product = product * nums[i];
+            max = Math.max(max, product);
+            if (product == 0) {
+                product = 1;    // reset
+            }
+        }
+
+        return max;
+    }
+    /**
+     Input: [2,3, -2,4]
+     [1,2,6,-12,-48] left -> right
+     [2,3, -2,4]
+     [-48,-24,-8,4] 1
+
+     [-2,0,-1]
+     [1,-2,0,-1]
+     1
+     [-2,0,-1]
+     [-2,0,-1,1]
+     1
+     Output: 6
+     */
+
+    // T:O(n), S:O(1)
+    public int maxProduct1(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
 
