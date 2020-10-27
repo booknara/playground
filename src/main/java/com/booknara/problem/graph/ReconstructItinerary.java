97ac3db59aa95adfd208a46cb92caef20350473f
Daehee Han <bookdori81@gmail.com>
2020-10-26 17:21:10 -0700

    Add Reconstruct Itinerary problem

--- src/main/java/com/booknara/problem/graph/ReconstructItinerary.java
+++ src/main/java/com/booknara/problem/graph/ReconstructItinerary.java
@@ -39,4 +39,78 @@ public class ReconstructItinerary {
         // if there is no flight heading to, add string here
         res.addFirst(start);
     }
+
+    // T:O(E + V), S:O(E + V)
+    public List<String> findItinerary1(List<List<String>> tickets) {
+        // input check
+        List<String> res = new ArrayList<>();
+
+        if (tickets == null || tickets.size() == 0) return res;
+
+        Map<String, List<String>> graph = new HashMap<>();
+        for (List<String> t: tickets) {
+            String from = t.get(0);
+            String to = t.get(1);
+
+            List<String> des = graph.getOrDefault(from, new ArrayList<>());
+            des.add(to);
+            graph.put(from, des);
+        }
+
+        for (String s: graph.keySet()) {
+            // sorting the destinations
+            Collections.sort(graph.get(s));
+        }
+
+        dfs(graph, "JFK", res);
+
+        return res;
+    }
+
+    public boolean dfs(Map<String, List<String>> graph, String start, List<String> list) {
+        // base case, no key valud in the map
+        if (graph.keySet().size() == 0) {
+            list.add(start);
+            return true;
+        }
+
+        if (!graph.containsKey(start)) {
+            // no route further
+            return false;
+        }
+
+        List<String> des = graph.get(start);
+        if (des == null) return false;
+
+        for (int i = 0; i < des.size(); i++) {
+            String to = graph.get(start).get(i);
+            list.add(start);
+
+            graph.get(start).remove(i);
+            if (graph.get(start).size() == 0) graph.remove(start);
+
+            if (dfs(graph, to, list)) return true;
+
+            graph.putIfAbsent(start, new ArrayList<>());
+            graph.get(start).add(i, to);
+
+            list.remove(list.size() - 1);
+        }
+
+        return false;
+    }
 }
+/**
+ [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ from - to
+
+ Map<String, List<String>> = new HashMap<>();
+ "JFK" -> "ATL", "SFO",
+ "SFO" -> "ATL",
+ "ATL" -> "JFK", "SFO",
+
+ "JFK" -> "ATL" -> "JFK" -> "SFO" -> "ATL" -> "SFO"
+ -> "SFO" -> "ATL"
+
+ -> "SFO" -> "ATL" -> "JFK"
+ */
