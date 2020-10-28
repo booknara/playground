be6e9fbb87232cc160c87c0f020f41d6e9fcdfa7
Daehee Han <bookdori81@gmail.com>
2020-10-28 13:52:11 -0700

    Add String to Integer problem

--- src/main/java/com/booknara/problem/string/StringToInteger.java
+++ src/main/java/com/booknara/problem/string/StringToInteger.java
@@ -32,4 +32,51 @@ public class StringToInteger {
 
         return sign * (int) num;
     }
+
+    // T:O(n), S:O(1)
+    public int myAtoi1(String s) {
+        // input check
+        if (s == null || s.length() == 0) return 0;
+
+        s = s.trim();
+        if (s.length() == 0) return 0;
+
+        int sign = 1;
+        long num = 0L;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                break; // white space btw the start and the end
+            }
+
+            if (i == 0) {
+                if (c == '+' || c == '-') {
+                    if (c == '-') sign = -1;
+                    continue;
+                }
+
+                if (c == '.') break;
+
+                if (Character.isLetter(c)) return 0;
+
+                num = num * 10 + c - '0';
+                continue;
+            }
+
+            if (c == '+' || c == '-') {
+                break;
+            }
+
+            if (c == '.') break;
+
+            if (Character.isLetter(c)) break;
+
+            num = num * 10 + c - '0';
+
+            if (sign * num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if (sign * num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        }
+
+        return sign * (int) num;
+    }
 }
