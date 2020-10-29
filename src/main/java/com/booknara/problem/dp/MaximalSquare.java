4b7d91a7674c7c7a259eb798f8f527851133efd5
Daehee Han <bookdori81@gmail.com>
2020-10-29 16:21:55 -0700

    Add Maximal Square problem

--- src/main/java/com/booknara/problem/dp/MaximalSquare.java
+++ src/main/java/com/booknara/problem/dp/MaximalSquare.java
@@ -36,26 +36,28 @@ public class MaximalSquare {
 
     // DP, Space O(n*m)
     public int maximalSquare1(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            return 0;
-        }
+        // input check
+        if (matrix == null || matrix.length == 0) return 0;
 
         int max = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (matrix[i - 1][j - 1] == '0') {
-                    continue;
-                }
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') continue;
 
-                dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
-                max = Math.max(max, dp[i][j] * dp[i][j]);
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else {
+                    // matrix[i][j] -> '1'
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+                max = Math.max(max, dp[i][j]);
             }
         }
 
-        return max;
+        return max * max;
     }
 
     // T:O(m * n), S:O(m * n)
