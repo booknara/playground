48d74ac7e39f9195badc8511b1ede426b4661a8b
Daehee Han <bookdori81@gmail.com>
2020-10-29 22:26:00 -0700

    Add Maximize Distance to Closest Person problem

--- src/main/java/com/booknara/problem/array/MaximizeDistanceToClosestPerson.java
+++ src/main/java/com/booknara/problem/array/MaximizeDistanceToClosestPerson.java
@@ -1,7 +1,7 @@
 package com.booknara.problem.array;
 
 /**
- * 849. Maximize Distance to Closest Person (Easy)
+ * 849. Maximize Distance to Closest Person (Medium)
  * https://leetcode.com/problems/maximize-distance-to-closest-person/
  */
 public class MaximizeDistanceToClosestPerson {
@@ -32,6 +32,35 @@ public class MaximizeDistanceToClosestPerson {
 
         // the last '1'
         max = Math.max(max, seats.length - prev - 1);
+
+        return max;
+    }
+
+    // T:O(n), S:O(1)
+    public int maxDistToClosest1(int[] seats) {
+        // input check, seats.length >= 2
+        Integer s = null;
+        int max = 0;
+        int n = seats.length;
+
+        for (int i = 0; i < n; i++) {
+            int num = seats[i];
+            if (num == 0) continue;
+
+            if (s == null) {
+                // start
+                max = i;
+            } else {
+                // mid - s => distance
+                int mid = s + (i - s) / 2;
+                max = Math.max(max, mid - s);
+            }
+
+            s = i;
+        }
+
+        // end
+        max = Math.max(max, n - s - 1);
 
         return max;
     }
