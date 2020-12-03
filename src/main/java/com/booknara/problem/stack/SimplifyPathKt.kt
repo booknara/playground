1b5e2453ae21a276092e8d18516a36c340d11386
Daehee Han <bookdori81@gmail.com>
2020-12-02 17:24:32 -0800

    Add Simplify Path problem

---
+++ src/main/java/com/booknara/problem/stack/SimplifyPathKt.kt
@@ -0,0 +1,39 @@
+package com.booknara.problem.stack
+
+import java.util.*
+
+/**
+ * 71. Simplify Path (Medium)
+ * https://leetcode.com/problems/simplify-path/
+ */
+class SimplifyPathKt {
+  // T:O(n), S:O(n)
+  fun simplifyPath(path: String): String {
+    val res = StringBuilder()
+
+    // two slash to one slash
+    path.replace("//", "/")
+
+    val tokens = path.split("/")
+
+    val stack = Stack<String>()
+    for (token in tokens) {
+      if (token.isNullOrEmpty()) continue
+
+      //println(token)
+      if (token.equals(".")) continue
+
+      if (token.equals("..")) {
+        if (!stack.isEmpty()) stack.pop()
+      } else {
+        stack.push(token)
+      }
+    }
+
+    while (!stack.isEmpty()) {
+      res.insert(0, "/" + stack.pop())
+    }
+
+    return if (res.isEmpty()) "/" else res.toString()
+  }
+}
