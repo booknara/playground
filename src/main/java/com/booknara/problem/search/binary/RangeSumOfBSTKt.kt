edef9975b71e8fafef60b33252c593661bfeff5a
Daehee Han <bookdori81@gmail.com>
2020-12-09 16:24:40 -0800

    Add Range Sum of BST problem

--- src/main/java/com/booknara/problem/search/binary/RangeSumOfBSTKt.kt
+++ src/main/java/com/booknara/problem/search/binary/RangeSumOfBSTKt.kt
@@ -1,34 +1,67 @@
 package com.booknara.problem.search.binary
 
+import java.util.*
+
 /**
  * 938. Range Sum of BST (Easy)
  * https://leetcode.com/problems/range-sum-of-bst/
  */
 class RangeSumOfBSTKt {
+  var res = 0
+
+  // T:O(n), S:O(h)
+  fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+    helper(root, low, high)
+    return res
+  }
+
+  fun helper(node: TreeNode?, low: Int, high: Int) {
+    // base case
+    if (node == null) return
+
+    if (node.`val` in low..high)
+      res += node.`val`
+
+    if (low < node.`val`) {
+      helper(node.left, low, high)
+    }
+    if (node.`val` < high) {
+      helper(node.right, low, high)
+    }
+  }
+
+  // T:O(n), S:O(h)
+  fun rangeSumBST1(root: TreeNode?, low: Int, high: Int): Int {
+    // input check, the number of nodes >= 1
+
+    val queue = LinkedList<TreeNode>()
+    queue.offer(root)
+
     var res = 0
-    // T:O(n), S:O(h)
-    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
-        helper(root, low, high)
-        return res
-    }
+    while (!queue.isEmpty()) {
+      val size = queue.size
+      for (i in 0 until size) {
+        val node = queue.poll()
 
-    fun helper(node: TreeNode?, low: Int, high: Int) {
-        // base case
-        if (node == null) return
-
-        if (node.`val` in low..high)
-            res += node.`val`
-
-        if (low < node.`val`) {
-            helper(node.left, low, high)
+        val num = node.`val`
+        if (num in low..high) {
+          res += num
         }
-        if (node.`val` < high) {
-            helper(node.right, low, high)
+
+        if (num >= low && node.left != null) {
+          queue.offer(node.left)
         }
+        if (num <= high && node.right != null) {
+          queue.offer(node.right)
+        }
+      }
     }
 
-    class TreeNode(var `val`: Int) {
-        var left: TreeNode? = null
-        var right: TreeNode? = null
-    }
+    return res
+  }
+
+  class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+  }
 }
