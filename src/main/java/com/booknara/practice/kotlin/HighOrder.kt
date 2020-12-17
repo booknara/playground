445dec8eb345b23c100ceae15a6fd0c7d05fac60
Daehee Han <bookdori81@gmail.com>
2020-12-17 02:24:21 -0800

    Add High order function practice

--- src/main/java/com/booknara/practice/kotlin/HighOrder.kt
+++ src/main/java/com/booknara/practice/kotlin/HighOrder.kt
@@ -12,6 +12,24 @@ fun operation(): (Int) -> Int {
 
 fun square(x: Int) = x * x
 
+class ContactListFilters {
+    var prefix: String = ""
+    var onlyWithPhoneNumber: Boolean = false
+
+    fun getPredicate() : (Person) -> Boolean {
+        val startsWithPrefix = { p: Person ->
+            p.firsName.startsWith(prefix) || p.lastName.startsWith(prefix)
+        }
+
+        if (!onlyWithPhoneNumber) {
+            return startsWithPrefix
+        }
+        return { startsWithPrefix(it)
+                && it.phoneNUmber != null
+        }
+    }
+}
+
 fun main() {
     val sumResult = calculate(4, 5, ::sum)
     val mulResult = calculate(4, 5) {
@@ -22,5 +40,13 @@ fun main() {
 
     val func = operation()
     println(func(2))
+
+    val contacts = listOf(Person("Dmitry", "Jemerov", "123-456"),
+            Person("Svetlana", "Isakova",  null))
+    val contactListFilters = ContactListFilters()
+    with(contactListFilters) {
+        prefix = "Dm"
+        onlyWithPhoneNumber = true
+    }
 }
 
