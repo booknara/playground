a8e056d1dbc0d00435774fa99255aed6454423b3
Daehee Han <bookdori81@gmail.com>
2020-12-17 02:23:33 -0800

    Add Set Intersection Size At Least Two problem

---
+++ src/main/java/com/booknara/problem/greedy/SetIntersectionSizeAtLeastTwo.java
@@ -0,0 +1,39 @@
+package com.booknara.problem.greedy;
+
+import java.util.Arrays;
+
+/**
+ * 757. Set Intersection Size At Least Two
+ * https://leetcode.com/problems/set-intersection-size-at-least-two/
+ */
+public class SetIntersectionSizeAtLeastTwo {
+    public int intersectionSizeTwo(int[][] intervals) {
+        int res = 0;
+        if (intervals == null || intervals.length == 0) {
+            return res;
+        }
+
+        Arrays.sort(intervals, (a, b)-> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
+        // known two rightmost point in the set/res
+        int left = intervals[0][1] - 1;
+        int right = intervals[0][1];
+        res += 2;
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            // 1. one element of the set is in the interval
+            // 2. no element of the set is in the interval
+            if (left < curr[0] && curr[0] <= right) {
+                res++;
+                left = right;
+                right = curr[1];
+            } else if (curr[0] > right) {
+                res += 2;
+                left = curr[1] - 1;
+                right = curr[1];
+            }
+        }
+
+        return res;
+    }
+}
