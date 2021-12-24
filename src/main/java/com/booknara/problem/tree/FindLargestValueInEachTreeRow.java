2c2f4a1e11f5b0d08bc7f5634aa4e58f3837b907
Daehee Han <daeheeh@fb.com>
2021-12-24 01:15:56 -0800

    Updating Find Largest Value in Each Tree Row problem

--- src/main/java/com/booknara/problem/tree/FindLargestValueInEachTreeRow.java
+++ src/main/java/com/booknara/problem/tree/FindLargestValueInEachTreeRow.java
@@ -12,33 +12,35 @@ import java.util.Queue;
  * https://leetcode.com/problems/find-largest-value-in-each-tree-row/
  */
 public class FindLargestValueInEachTreeRow {
-    // T:O(n), S:O(n/2)
-    public List<Integer> largestValues(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            int max = Integer.MIN_VALUE;
-            for (int i= 0; i < len; i++) {
-                TreeNode node = queue.poll();
-                max = Math.max(max, node.val);
-
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-
-            res.add(max);
-        }
-
-        return res;
+  // T:O(n), S:O(n/2)
+  public List<Integer> largestValues(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
     }
+
+    // BFS
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      int len = queue.size();
+      int max = Integer.MIN_VALUE;
+      // Level traverse
+      for (int i = 0; i < len; i++) {
+        TreeNode node = queue.poll();
+        max = Math.max(max, node.val);
+
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+
+      res.add(max);
+    }
+
+    return res;
+  }
 }
