e5982591ca71f78cf7a8d144618ff22cdc76b169
Daehee Han <daeheeh@fb.com>
2021-12-26 12:49:39 -0800

    Updating Minimum Remove to Make Valid Parentheses problem

--- src/main/java/com/booknara/problem/string/MinimumRemoveValidParentheses.java
+++ src/main/java/com/booknara/problem/string/MinimumRemoveValidParentheses.java
@@ -7,73 +7,70 @@ import java.util.Stack;
  * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
  */
 public class MinimumRemoveValidParentheses {
-    // T:O(n), S:O(1)
-    public String minRemoveToMakeValid(String s) {
-        // input check
-        if (s == null || s.length() == 0) return "";
+  // T:O(n -> n^2) because of lastIndexOf(), S:O(n)
+  public String minRemoveToMakeValid(String s) {
+    // input check
+    if (s == null || s.length() == 0) return "";
 
-        int open = 0;
-        StringBuilder builder = new StringBuilder();
-        for (char c: s.toCharArray()) {
-            // three cases
-            if (c == '(') {
-                open++;
-                builder.append(c);
-            } else if (c == ')') {
-                if (open != 0) {
-                    open--;
-                    builder.append(c);
-                }
-            } else {
-                builder.append(c);
-            }
+    int open = 0;
+    StringBuilder res = new StringBuilder();
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        open++;
+      } else if (c == ')') {
+        if (open <= 0) {
+          continue;
         }
-
-        for (int i = 0; i < open; i++) {
-            int idx = builder.lastIndexOf("(");
-            builder.deleteCharAt(idx);
-        }
-
-        return builder.toString();
+        open--;
+      }
+      res.append(c);
     }
 
-    public String minRemoveToMakeValid1(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-
-        Stack<Character> stack = new Stack<>();
-        int count = 0;
-        for (char c: s.toCharArray()) {
-            if (c == '(') {
-                count++;
-                stack.push(c);
-            } else if (c == ')') {
-                if (count != 0) {
-                    count--;
-                    stack.push(c);
-                }
-            } else {
-                // lowercase letter
-                stack.push(c);
-            }
-        }
-
-        StringBuilder builder = new StringBuilder();
-        while (!stack.isEmpty()) {
-            char c = stack.pop();
-
-            if (c == '(') {
-                if (count > 0) {
-                    count--;
-                } else {
-                    builder.insert(0, c);
-                }
-            } else {
-                builder.insert(0, c);
-            }
-        }
-
-        return builder.toString();
+    for (int i = 0; i < open; i++) {
+      int idx = res.lastIndexOf("(");
+      res.deleteCharAt(idx);
     }
+
+    return res.toString();
+  }
+
+  public String minRemoveToMakeValid1(String s) {
+    if (s == null || s.length() == 0) {
+      return "";
+    }
+
+    Stack<Character> stack = new Stack<>();
+    int count = 0;
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        count++;
+        stack.push(c);
+      } else if (c == ')') {
+        if (count != 0) {
+          count--;
+          stack.push(c);
+        }
+      } else {
+        // lowercase letter
+        stack.push(c);
+      }
+    }
+
+    StringBuilder builder = new StringBuilder();
+    while (!stack.isEmpty()) {
+      char c = stack.pop();
+
+      if (c == '(') {
+        if (count > 0) {
+          count--;
+        } else {
+          builder.insert(0, c);
+        }
+      } else {
+        builder.insert(0, c);
+      }
+    }
+
+    return builder.toString();
+  }
 }
