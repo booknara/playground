f557e47493d69714c964a8b2caa394565e881040
Daehee Han <bookdori81@gmail.com>
2022-10-17 01:57:28 -0700

    Update Find Pivot Index problem

--- src/main/java/com/booknara/problem/array/FindPivotIndex.java
+++ src/main/java/com/booknara/problem/array/FindPivotIndex.java
@@ -5,25 +5,45 @@ package com.booknara.problem.array;
  * https://leetcode.com/problems/find-pivot-index/
  */
 public class FindPivotIndex {
-    public int pivotIndex(int[] nums) {
-        if (nums == null || nums.length < 3) {
-            return -1;
-        }
+  // T:O(n), S:O(1)
+  public int pivotIndex(int[] nums) {
+    // input check nums.length >= 1
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+    }
 
-        int[] sum = new int[nums.length];
-        sum[nums.length - 1] = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
-            sum[i] = nums[i] + sum[i + 1];
-        }
+    int result = -1;
+    // left prefix sum
+    int leftSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      // rightSum = sum - leftSum - nums[i]
+      if (leftSum == (sum - leftSum - nums[i])) return i;
+      leftSum += nums[i];
+    }
 
-        int left = 0;
-        for (int i = 0; i < nums.length; i++) {
-            left = left + nums[i];
-            if (left == sum[i]) {
-                return i;
-            }
-        }
+    return result;
+  }
 
+  public int pivotIndex1(int[] nums) {
+    if (nums == null || nums.length < 3) {
         return -1;
     }
+
+    int[] sum = new int[nums.length];
+    sum[nums.length - 1] = nums[nums.length - 1];
+    for (int i = nums.length - 2; i >= 0; i--) {
+        sum[i] = nums[i] + sum[i + 1];
+    }
+
+    int left = 0;
+    for (int i = 0; i < nums.length; i++) {
+        left = left + nums[i];
+        if (left == sum[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+  }
 }
