a39f9d139c1cf338b236fa9e6b55797796b4ff8f
Daehee Han <bookdori81@gmail.com>
2022-11-13 22:31:37 -0800

    Update TwoSum.java

--- src/main/java/com/booknara/problem/hash/TwoSum.java
+++ src/main/java/com/booknara/problem/hash/TwoSum.java
@@ -4,25 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Leet code: 1. Two Sum (Easy)
+ * 1. Two Sum (Easy)
  * https://leetcode.com/problems/two-sum/
  */
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return null;
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        // Key: remaining value, value: index
-        for (int i = 0; i < nums.length; i++) {
-            //System.out.println(nums[i]);
-            if (map.containsKey(nums[i])) {
-                return new int[] {map.get(nums[i]), i};
-            }
-
-            map.put(target - nums[i], i);
-        }
-
-        return null;
+  // T:O(n), S:O(n)
+  public int[] twoSum(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return null;
     }
+    Map<Integer, Integer> map = new HashMap<>();
+    // Key: remaining value, value: index
+    for (int i = 0; i < nums.length; i++) {
+      //System.out.println(nums[i]);
+      if (map.containsKey(nums[i])) {
+        return new int[]{map.get(nums[i]), i};
+      }
+
+      map.put(target - nums[i], i);
+    }
+
+    return null;
+  }
 }
