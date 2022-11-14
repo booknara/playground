2ea25bfb8a1ad680f584535afb1c054b93e92ba1
Daehee Han <bookdori81@gmail.com>
2022-11-13 23:54:15 -0800

    Update MaxSubArray.java

--- src/main/java/com/booknara/problem/dp/MaxSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxSubArray.java
@@ -5,84 +5,89 @@ package com.booknara.problem.dp;
  * https://leetcode.com/problems/maximum-subarray/
  */
 public class MaxSubArray {
-    // T:O(n), S:O(1)
-    public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int max = nums[0];
-        int total = max;
-        for (int i = 1; i < nums.length; i++) {
-            max = Math.max(nums[i] + max, nums[i]);
-            total = Math.max(total, max);
-        }
-
-        return total;
+  // T:O(n), S:O(1)
+  public int maxSubArray(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
     }
 
-    // T:O(n), S:O(1)
-    public int maxSubArray1(int[] nums) {
-        // input check num.length >= 1
-        if (nums.length == 1) return nums[0];
-
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        int min = 0;
-        for (int n: nums) {
-            sum += n;
-            max = Math.max(max, sum - min);
-            min = Math.min(min, sum);
-        }
-
-        return max;
+    int max = nums[0];
+    int total = max;
+    for (int i = 1; i < nums.length; i++) {
+      max = Math.max(nums[i] + max, nums[i]);
+      total = Math.max(total, max);
     }
 
-    // T:O(nlogn), S:O(1)
-    public int maxSubArray2(int[] nums) {
-        // input check num.length >= 1
-        if (nums.length == 1) return nums[0];
+    return total;
+  }
 
-        return helper(nums, 0, nums.length - 1);
+  // T:O(n), S:O(1)
+  public int maxSubArray1(int[] nums) {
+    // input check num.length >= 1
+    if (nums.length == 1) return nums[0];
+
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    int min = 0;
+    for (int n : nums) {
+      sum += n;
+      max = Math.max(max, sum - min);
+      min = Math.min(min, sum);
     }
 
-    public int helper(int[] nums, int left, int right) {
-        if (left == right) return nums[left];
+    return max;
+  }
 
-        int mid = left + (right - left) / 2;
+  // T:O(nlogn), S:O(1)
+  public int maxSubArray2(int[] nums) {
+    // input check num.length >= 1
+    if (nums.length == 1) return nums[0];
 
-        int leftSum = helper(nums, left, mid);
-        int rightSum = helper(nums, mid + 1, right);
-        int crossSum = crossSum(nums, left, right, mid);
+    return helper(nums, 0, nums.length - 1);
+  }
 
-        return Math.max(crossSum, Math.max(leftSum, rightSum));
+  public int helper(int[] nums, int left, int right) {
+    if (left == right) return nums[left];
+
+    int mid = left + (right - left) / 2;
+
+    int leftSum = helper(nums, left, mid);
+    int rightSum = helper(nums, mid + 1, right);
+    int crossSum = crossSum(nums, left, right, mid);
+
+    return Math.max(crossSum, Math.max(leftSum, rightSum));
+  }
+
+  public int crossSum(int[] nums, int left, int right, int p) {
+    if (left == right) return nums[left];
+
+    int leftMax = Integer.MIN_VALUE;
+    int sum = 0;
+    for (int i = p; i > left - 1; i--) {
+      sum += nums[i];
+      leftMax = Math.max(leftMax, sum);
     }
 
-    public int crossSum(int[] nums, int left, int right, int p) {
-        if (left == right) return nums[left];
-
-        int leftMax = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int i = p; i > left - 1; i--) {
-            sum += nums[i];
-            leftMax = Math.max(leftMax, sum);
-        }
-
-        int rightMax = Integer.MIN_VALUE;
-        sum = 0;
-        for (int i = p + 1; i < right + 1; i++) {
-            sum += nums[i];
-            rightMax = Math.max(rightMax, sum);
-        }
-
-        return leftMax + rightMax;
+    int rightMax = Integer.MIN_VALUE;
+    sum = 0;
+    for (int i = p + 1; i < right + 1; i++) {
+      sum += nums[i];
+      rightMax = Math.max(rightMax, sum);
     }
+
+    return leftMax + rightMax;
+  }
 }
 /**
- Given an integer array nums, find the contiguous subarray (containing at least one number)
- which has the largest sum and return its sum.
- element = [-2, 1,-3,4,-1,2,1,-5,4]
- max     = [-2, 1,-2,4, 3,5,6, 1,5]
- total.  = [-2, 1, 1,4, 4,5,6, 6,6]
- [4,-1,2,1], res = 6
+ * Given an integer array nums, find the contiguous subarray (containing at least one number)
+ * which has the largest sum and return its sum.
+ * element = [-2, 1,-3,4,-1,2,1,-5,4]
+ * max     = [-2, 1,-2,4, 3,5,6, 1,5]
+ * total.  = [-2, 1, 1,4, 4,5,6, 6,6]
+ * [4,-1,2,1], res = 6
+ * <p>
+ * 1. sub max subarray
+ * 2. current element
+ * formular #1: sub max = 1/2
+ * formular #2: total max = Math.max(total max, new sub max)
  */
