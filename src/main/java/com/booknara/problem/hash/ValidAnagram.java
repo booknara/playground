771a29b237a3708326ca8cc4ac23289662992ddc
Daehee Han <bookdori81@gmail.com>
2022-11-13 22:58:17 -0800

    Update ValidAnagram.java

--- src/main/java/com/booknara/problem/hash/ValidAnagram.java
+++ src/main/java/com/booknara/problem/hash/ValidAnagram.java
@@ -7,70 +7,70 @@ import java.util.Arrays;
  * https://leetcode.com/problems/valid-anagram/
  */
 public class ValidAnagram {
-    // O(n), Unicode applicable
-    public boolean isAnagram(String s, String t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
+  // T:O(n), S:O(1), Unicode applicable
+  public boolean isAnagram(String s, String t) {
+    if (s == null && t == null) return true;
+    if (s == null || t == null) return false;
 
-        if (s.length() != t.length()) return false;
+    if (s.length() != t.length()) return false;
 
-        int[] count = new int[256];
-        for (char c: s.toCharArray()) {
-            count[c]++;
-        }
-        for (char c: t.toCharArray()) {
-            count[c]--;
-        }
-
-        for (int i = 0; i < 256; i++) {
-            if (count[i] != 0) return false;
-        }
-
-        return true;
+    int[] count = new int[256];
+    for (char c : s.toCharArray()) {
+      count[c]++;
+    }
+    for (char c : t.toCharArray()) {
+      count[c]--;
     }
 
-    // O(n), alphabet applicable only
-    public boolean isAnagram1(String s, String t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
-
-        if (s.length() != t.length()) return false;
-
-        int[] count = new int[26];
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
-            count[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < len; i++) {
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                return false;
-            }
-        }
-
-        return true;
+    for (int i = 0; i < 256; i++) {
+      if (count[i] != 0) return false;
     }
 
-    // O(n * logn)
-    public boolean isAnagram2(String s, String t) {
-        if (s == null && t == null) return true;
-        if (s == null || t == null) return false;
+    return true;
+  }
 
-        if (s.length() != t.length()) return false;
+  // O(n), alphabet applicable only
+  public boolean isAnagram1(String s, String t) {
+    if (s == null && t == null) return true;
+    if (s == null || t == null) return false;
 
-        char[] sArr= s.toCharArray();
-        char[] tArr= t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
+    if (s.length() != t.length()) return false;
 
-        int len = sArr.length;
-        for (int i = 0; i < len; i++) {
-            if (sArr[i] != tArr[i]) return false;
-        }
-
-        return true;
+    int[] count = new int[26];
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+      count[s.charAt(i) - 'a']++;
     }
+    for (int i = 0; i < len; i++) {
+      count[t.charAt(i) - 'a']--;
+    }
+
+    for (int i = 0; i < 26; i++) {
+      if (count[i] != 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // O(n * logn)
+  public boolean isAnagram2(String s, String t) {
+    if (s == null && t == null) return true;
+    if (s == null || t == null) return false;
+
+    if (s.length() != t.length()) return false;
+
+    char[] sArr = s.toCharArray();
+    char[] tArr = t.toCharArray();
+    Arrays.sort(sArr);
+    Arrays.sort(tArr);
+
+    int len = sArr.length;
+    for (int i = 0; i < len; i++) {
+      if (sArr[i] != tArr[i]) return false;
+    }
+
+    return true;
+  }
 }
