db21de3c774bf2f8b3c185f5aab8857d187593ef
Daehee Han <bookdori81@gmail.com>
2022-11-13 22:50:32 -0800

    Update BestTimeToBuyAndSellStock.java

--- src/main/java/com/booknara/problem/dp/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/booknara/problem/dp/BestTimeToBuyAndSellStock.java
@@ -5,43 +5,58 @@ package com.booknara.problem.dp;
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  */
 public class BestTimeToBuyAndSellStock {
-    // T:O(n), S:O(1)
-    public int maxProfit(int[] prices) {
-        // input check
-        if (prices == null || prices.length == 0) return 0;
-        // input : all the values of prices are non-negative
+  // T:O(n), S:O(1)
+  public int maxProfit(int[] prices) {
+    // input check
+    if (prices == null || prices.length == 0) return 0;
+    // input : all the values of prices are non-negative
 
-        int buy = Integer.MIN_VALUE, sell = 0;
-        for (int p:prices) {
-            buy = Math.max(buy, -p);
-            sell = Math.max(sell, buy + p);
-        }
-
-        return sell;
+    int buy = Integer.MIN_VALUE, sell = 0;
+    for (int p : prices) {
+      buy = Math.max(buy, -p);
+      sell = Math.max(sell, buy + p);
     }
 
-    public int maxProfit1(int[] prices) {
-        int max = 0;
+    return sell;
+  }
 
-        int low = Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            low = Math.min(low, prices[i]);
-            max = Math.max(max, prices[i] - low);
-        }
+  public int maxProfit1(int[] prices) {
+    int max = 0;
 
-        return max;
+    int low = Integer.MAX_VALUE;
+    for (int i = 0; i < prices.length; i++) {
+      low = Math.min(low, prices[i]);
+      max = Math.max(max, prices[i] - low);
     }
 
+    return max;
+  }
+
+  public int maxProfit2(int[] prices) {
+    // input check, prices.length >= 1
+    int low = Integer.MAX_VALUE, profit = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      // lowest?
+      int price = prices[i];
+      if (price < low) {
+        low = price;
+      } else {
+        profit = Math.max(profit, price - low);
+      }
+    }
+    return profit;
+  }
 }
 
 /**
- input : all the values of prices are non-negative
- prices: [7,1,5,3,6,4]
- profit = sell - buy = sell + (-buy)
- int buy = Integer.MIN_VALUE, sell = 0;
- loop 0 ~ n - 1
- buy = Math.max(buy, -prices[i]);
- sell = Math.max(sell, prices[i] + buy);
-
- return sell;
+ * input : all the values of prices are non-negative
+ * prices: [7,1,5,3,6,4]
+ * profit = sell - buy = sell + (-buy)
+ * int buy = Integer.MIN_VALUE, sell = 0;
+ * loop 0 ~ n - 1
+ * buy = Math.max(buy, -prices[i]);
+ * sell = Math.max(sell, prices[i] + buy);
+ * <p>
+ * return sell;
  */
