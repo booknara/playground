2ba697f8f2dddbc4714667c13185ed92e0f61d0f
Daehee Han <bookdori81@gmail.com>
2022-11-20 20:10:06 -0800

    Update LinkedListCycle.java

--- src/main/java/com/booknara/problem/list/LinkedListCycle.java
+++ src/main/java/com/booknara/problem/list/LinkedListCycle.java
@@ -7,23 +7,23 @@ import com.booknara.problem.common.ListNode;
  * https://leetcode.com/problems/linked-list-cycle/
  */
 public class LinkedListCycle {
-    // T:O(n), S:O(1)
-    public boolean hasCycle(ListNode head) {
-        if (head == null) {
-            return false;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while (fast != null && fast.next != null) {
-            if (slow == fast) {
-                return true;
-            }
-
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return false;
+  // T:O(n), S:O(1)
+  public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) {
+      return false;
     }
+
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (fast != null && fast.next != null) {
+      if (slow == fast) {
+        return true;
+      }
+
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return false;
+  }
 }
