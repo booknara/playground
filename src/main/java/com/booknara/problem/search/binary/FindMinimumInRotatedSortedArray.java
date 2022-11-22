d1ca620b640ec0d917d9e4663d5d8f72c3f81ab9
Daehee Han <bookdori81@gmail.com>
2022-11-21 21:24:30 -0800

    Update FindMinimumInRotatedSortedArray.java

--- src/main/java/com/booknara/problem/search/binary/FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/booknara/problem/search/binary/FindMinimumInRotatedSortedArray.java
@@ -5,49 +5,49 @@ package com.booknara.problem.search.binary;
  * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
  */
 public class FindMinimumInRotatedSortedArray {
-    // T:O(logn), S:O(1)
-    public int findMin(int[] nums) {
-        if (nums.length == 1) return nums[0];
+  // T:O(logn), S:O(1)
+  public int findMin(int[] nums) {
+    if (nums.length == 1) return nums[0];
 
-        int l = 0, r = nums.length - 1;
-        while (l < r) {
-            int m = l + (r - l) / 2;
-            if (nums[l] < nums[r]) return nums[l];
+    int l = 0, r = nums.length - 1;
+    while (l < r) {
+      int m = l + (r - l) / 2;
+      if (nums[l] < nums[r]) return nums[l];
 
-            // e.g. [2,1]
-            if (nums[l] <= nums[m]) {
-                l = m + 1;
-            } else {
-                r = m;
-            }
-        }
-
-        return nums[r];
+      // e.g. [2,1]
+      if (nums[l] <= nums[m]) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
     }
 
-    public int findMin1(int[] nums) {
-        // input check 0, 1 edge case handling
-        int l = 0, r = nums.length - 1;
-        while (l < r) {
-            int m = l + (r - l) / 2;
+    return nums[r];
+  }
 
-            // early detection
-            if (m - 1 >= 0 && nums[m - 1] > nums[m]) return nums[m];
+  public int findMin1(int[] nums) {
+    // input check 0, 1 edge case handling
+    int l = 0, r = nums.length - 1;
+    while (l < r) {
+      int m = l + (r - l) / 2;
 
-            if (nums[l] <= nums[m]) {
-                // sorted area
-                if (nums[m] > nums[r]) {
-                    l = m + 1;  // not pivot area
-                } else {
-                    r = m - 1;  // pivot area
-                }
-            } else {
-                // pivot area
-                r = m - 1;
-            }
+      // early detection
+      if (m - 1 >= 0 && nums[m - 1] > nums[m]) return nums[m];
+
+      if (nums[l] <= nums[m]) {
+        // sorted area
+        if (nums[m] > nums[r]) {
+          l = m + 1;  // not pivot area
+        } else {
+          r = m - 1;  // pivot area
         }
-
-        return nums[l];
+      } else {
+        // pivot area
+        r = m - 1;
+      }
     }
+
+    return nums[l];
+  }
 
 }
