bf0692f890b51a70393e0850bee4566458597d87
Daehee Han <bookdori81@gmail.com>
2022-11-21 20:44:07 -0800

    Update ContainerWithMostWater.java

--- src/main/java/com/booknara/problem/twopointers/ContainerWithMostWater.java
+++ src/main/java/com/booknara/problem/twopointers/ContainerWithMostWater.java
@@ -5,26 +5,25 @@ package com.booknara.problem.twopointers;
  * https://leetcode.com/problems/container-with-most-water/
  */
 public class ContainerWithMostWater {
-    // T:O(n), S:O(1)
-    public int maxArea(int[] height) {
-        if (height == null || height.length == 0) {
-            return 0;
-        }
-
-        int l = 0, r = height.length - 1;
-        int max = 0;
-        while (l < r) {
-            int width = r - l;
-            max = Math.max(max, Math.min(height[l], height[r]) * width);
-
-            // move smaller height to get a chance to make max value
-            if (height[l] < height[r]) {
-                l++;
-            } else {
-                r--;
-            }
-        }
-
-        return max;
+  // T:O(n), S:O(1)
+  public int maxArea(int[] height) {
+    if (height == null || height.length == 0) {
+      return 0;
     }
+
+    int l = 0, r = height.length - 1;
+    int max = 0;
+    while (l < r) {
+      int w = r - l;
+      int h = Math.min(height[l], height[r]);
+      max = Math.max(max, w * h);
+      // left or right move which is smaller height between current ones because the taller one should keep
+      if (height[l] < height[r]) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+    return max;
+  }
 }
