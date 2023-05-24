659b6b08d1765e0286351612e3b2156925c17d64
Daehee Han <bookdori81@gmail.com>
2023-05-23 23:24:11 -0700

    Create AddTwoIntegers.java

---
+++ src/main/java/com/booknara/problem/math/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+package com.booknara.problem.math;
+
+/**
+ * 2235. Add Two Integers
+ * https://leetcode.com/problems/add-two-integers/
+ */
+public class AddTwoIntegers {
+  // T:O(1), S:O(1)
+  public int sum(int num1, int num2) {
+    return num1 + num2;
+  }
+}
