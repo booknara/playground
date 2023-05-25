6338c484c1301a472084b8e1eab4287cc08afb04
Daehee Han <bookdori81@gmail.com>
2023-05-25 16:06:09 -0700

    Update CheckIfSentenceIsPangram.java

--- src/main/java/com/booknara/problem/array/CheckIfSentenceIsPangram.java
+++ src/main/java/com/booknara/problem/array/CheckIfSentenceIsPangram.java
@@ -5,21 +5,18 @@ package com.booknara.problem.array;
  * https://leetcode.com/problems/check-if-the-sentence-is-pangram/
  */
 public class CheckIfSentenceIsPangram {
-  final static int ALPHABET = 26;
-  final static int START = 97;
-
   // T:O(n), S:O(1)
   public boolean checkIfPangram(String sentence) {
-    int[] array = new int[256];
-    for (int i = 0; i < sentence.length(); i++) {
-      char c = sentence.charAt(i);
-      array[c]++;
+    // edge case
+    if (sentence.length() == 0) return false;
+
+    int[] data = new int[26];
+    for (char c: sentence.toCharArray()) {
+      data[c - 'a']++;
     }
 
-    for (int i = 0; i < ALPHABET; i++) {
-      if (array[97 + i] == 0) {
-        return false;
-      }
+    for (int i = 0; i < 26; i++) {
+      if (data[i] == 0) return false;
     }
 
     return true;
