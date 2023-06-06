521d4f901b90770e9d97de95f1a2611bae39d458
Daehee Han <bookdori81@gmail.com>
2023-06-05 17:50:10 -0700

    Update LengthOfLastWord.java

--- src/main/java/com/booknara/problem/string/LengthOfLastWord.java
+++ src/main/java/com/booknara/problem/string/LengthOfLastWord.java
@@ -26,4 +26,28 @@ public class LengthOfLastWord {
 
     return builder.toString().length();
   }
+
+  // T:O(n), S:O(1)
+  public int lengthOfLastWord1(String s) {
+    // input check
+    if (s == null || s.length() == 0) return 0;
+
+    int res = 0;
+    boolean start = false;
+    for (int i = s.length() - 1; i >= 0; i--) {
+      char c = s.charAt(i);
+      if (c == ' ') {
+        if (!start) {
+          continue;
+        } else {
+          break;
+        }
+      } else {
+        start = true;
+        res++;
+      }
+    }
+
+    return res;
+  }
 }
