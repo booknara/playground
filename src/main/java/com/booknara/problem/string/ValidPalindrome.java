5062f4315353169f82406fbe9ef152608a40c98b
Daehee Han <bookdori81@gmail.com>
2023-06-07 12:13:24 -0700

    Update ValidPalindrome.java

--- src/main/java/com/booknara/problem/string/ValidPalindrome.java
+++ src/main/java/com/booknara/problem/string/ValidPalindrome.java
@@ -5,25 +5,25 @@ package com.booknara.problem.string;
  * https://leetcode.com/problems/valid-palindrome/
  */
 public class ValidPalindrome {
-    // T:O(n), S:O(1)
-    public boolean isPalindrome(String s) {
-        if (s == null || s.length() == 0) return true;
+  // T:O(n), S:O(1)
+  public boolean isPalindrome(String s) {
+    if (s == null || s.length() == 0) return true;
 
-        int l = 0, r = s.length() - 1;
-        while (l < r) {
-            // filtering left character, only alphanumeric
-            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
-            // filtering right character, only alphanumeric
-            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+    int l = 0, r = s.length() - 1;
+    while (l < r) {
+      // filtering left character, only alphanumeric
+      while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+      // filtering right character, only alphanumeric
+      while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
 
-            if (l >= r) return true;
+      if (l >= r) return true;
 
-            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+      if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
 
-            l++;
-            r--;
-        }
-
-        return true;
+      l++;
+      r--;
     }
+
+    return true;
+  }
 }
