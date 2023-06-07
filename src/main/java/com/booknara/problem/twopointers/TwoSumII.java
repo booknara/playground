4b324a449d7d05487f08018fa923947a531f9f4b
Daehee Han <bookdori81@gmail.com>
2023-06-07 13:05:16 -0700

    Update TwoSumII.java

--- src/main/java/com/booknara/problem/twopointers/TwoSumII.java
+++ src/main/java/com/booknara/problem/twopointers/TwoSumII.java
@@ -1,29 +1,27 @@
 package com.booknara.problem.twopointers;
 
 /**
- * Leet code: 167. Two Sum II - Input array is sorted (Easy)
+ * 167. Two Sum II - Input Array Is Sorted (Medium)
  * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  */
 public class TwoSumII {
-    public int[] twoSum(int[] numbers, int target) {
-        if (numbers == null || numbers.length == 0 || numbers.length == 1) {
-            return null;
-        }
-
-        int i = 0, j = numbers.length - 1;
-        while (i < j) {
-            //System.out.println(numbers[i]);
-            if (numbers[i] + numbers[j]  == target) {
-                return new int[] {i + 1, j + 1};
-            }
-
-            if (numbers[i] + numbers[j]  < target) {
-                i++;
-            } else {
-                j--;
-            }
-        }
-
-        return null;
+  // T:(n), S:O(1)
+  public int[] twoSum(int[] numbers, int target) {
+    int[] res = new int[2];
+    int l = 0, r = numbers.length - 1;
+    while (l < r) {
+      if (numbers[l] + numbers[r] == target) {
+        res[0] = l + 1;
+        res[1] = r + 1;
+        break;
+      }
+      if (numbers[l] + numbers[r] < target) {
+        l++;
+      } else {
+        r--;
+      }
     }
+
+    return res;
+  }
 }
