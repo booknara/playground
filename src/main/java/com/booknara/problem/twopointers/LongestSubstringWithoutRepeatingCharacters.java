84d4add1a85ce6196e193b6d266b67b5696ccc6a
Daehee Han <bookdori81@gmail.com>
2023-07-18 14:18:35 -0700

    Update LongestSubstringWithoutRepeatingCharacters.java

--- src/main/java/com/booknara/problem/twopointers/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/booknara/problem/twopointers/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,60 +5,61 @@ package com.booknara.problem.twopointers;
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 public class LongestSubstringWithoutRepeatingCharacters {
-    // T:O(n), S:O(1)
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) return 0;
+  // T:O(n), S:O(1)
+  public int lengthOfLongestSubstring(String s) {
+    if (s == null || s.length() == 0) return 0;
 
-        int m = s.length();
-        int[] bucket = new int[128];  // input range is all ascii characters
-        int l = 0, r = 0;
-        int max = 1;
-        while (r < m) {
-            char c = s.charAt(r);
-            bucket[c]++;
+    int m = s.length();
+    int[] bucket = new int[128];  // input range is all ascii characters
+    int l = 0, r = 0;
+    int max = 1;
+    while (r < m) {
+      char c = s.charAt(r);
+      bucket[c]++;
 
-            if (bucket[c] >= 2) {
-                // repeated
-                while (l < r && bucket[c] > 1) {    // check bucket[c]
-                    bucket[s.charAt(l)]--;
-                    l++;
-                }
-            }
-
-            max = Math.max(max, r - l + 1);
-            r++;
+      if (bucket[c] >= 2) {
+        // repeated
+        while (l < r && bucket[c] > 1) {    // check bucket[c]
+          bucket[s.charAt(l)]--;
+          l++;
         }
+      }
 
-        return max;
+      max = Math.max(max, r - l + 1);
+      r++;
     }
 
-    // T:O(n), S:O(1)
-    public int lengthOfLongestSubstring1(String s) {
-        // input check
-        if (s == null || s.length() == 0) return 0;
+    return max;
+  }
 
-        int[] bucket = new int[128];
-        int l = 0, r = 0;
-        int max = 0;
+  // T:O(n), S:O(1)
+  public int lengthOfLongestSubstring1(String s) {
+    // input check
+    if (s == null || s.length() == 0) return 0;
+    if (s.length() == 1) return 1;
 
-        while (r < s.length()) {
-            char c = s.charAt(r);
-            bucket[c]++;
-            if (bucket[c] > 1) {
-                while (l < r) {
-                    char d = s.charAt(l);
-                    bucket[d]--;
-                    l++;
-                    if (d == c) {
-                        break;
-                    }
-                }
-            }
-
-            max = Math.max(max, r - l + 1);
-            r++;
+    int l = 0, r = 0;
+    int max = 0;
+    int[] bucket = new int[128];
+    while (r < s.length()) {
+      char c = s.charAt(r);
+      // already exist
+      if (bucket[c] != 0) {
+        while (l < r) {
+          char d = s.charAt(l);
+          bucket[d]--;
+          l++;
+          if (c == d) {
+            break;
+          }
         }
+      }
 
-        return max;
+      bucket[c]++;
+      max = Math.max(max, r - l + 1);
+      r++;
     }
+
+    return max;
+  }
 }
