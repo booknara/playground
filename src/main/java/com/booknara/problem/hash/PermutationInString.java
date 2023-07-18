06a6444f5b984b8bf51ee15d24cf09f271733a7a
Daehee Han <bookdori81@gmail.com>
2023-07-18 14:49:03 -0700

    Update PermutationInString.java

--- src/main/java/com/booknara/problem/hash/PermutationInString.java
+++ src/main/java/com/booknara/problem/hash/PermutationInString.java
@@ -7,31 +7,68 @@ import java.util.Arrays;
  * https://leetcode.com/problems/permutation-in-string/
  */
 public class PermutationInString {
-    // T:O(n), S:O(1)
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1 == null || s1.length() == 0) return true;
-        if (s2 == null || s2.length() == 0) return false;
+  // T:O(n), S:O(1)
+  public boolean checkInclusion(String s1, String s2) {
+    if (s1 == null || s1.length() == 0) return true;
+    if (s2 == null || s2.length() == 0) return false;
 
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
-        for (char c: s1.toCharArray()) {
-            s1Count[c - 'a']++;
-        }
-
-        for (int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-            s2Count[c - 'a']++;
-
-            int r = i - s1.length();
-            if (r >= 0) {
-                s2Count[s2.charAt(r) - 'a']--;
-            }
-
-            if (Arrays.equals(s1Count, s2Count)) {
-                return true;
-            }
-        }
-
-        return false;
+    int[] s1Count = new int[26];
+    int[] s2Count = new int[26];
+    for (char c : s1.toCharArray()) {
+      s1Count[c - 'a']++;
     }
+
+    for (int i = 0; i < s2.length(); i++) {
+      char c = s2.charAt(i);
+      s2Count[c - 'a']++;
+
+      int r = i - s1.length();
+      if (r >= 0) {
+        s2Count[s2.charAt(r) - 'a']--;
+      }
+
+      if (Arrays.equals(s1Count, s2Count)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // T:O(n), S:O(1)
+  public boolean checkInclusion1(String s1, String s2) {
+    if (s1.length() > s2.length()) {
+      return false;
+    }
+
+    int[] bucket = new int[26];
+    for (char c : s1.toCharArray()) {
+      bucket[c - 'a']++;
+    }
+
+    int l = 0, r = 0;
+    while (r < s2.length()) {
+      char c = s2.charAt(r);
+      bucket[c - 'a']--;
+
+      if (r >= s1.length()) {
+        char d = s2.charAt(l);
+        bucket[d - 'a']++;
+        l++;
+      }
+      if (isPermutation(bucket)) return true;
+      r++;
+    }
+
+    return false;
+  }
+
+  public boolean isPermutation(int[] bucket) {
+    for (int b : bucket) {
+      if (b > 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
