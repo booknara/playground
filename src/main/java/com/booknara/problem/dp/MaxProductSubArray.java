2ecd813f291dae35cf6f470fc91d474ebd02a698
Daehee Han <bookdori81@gmail.com>
2023-09-22 22:43:56 -0700

    Update MaxProductSubArray.java

--- src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
+++ src/main/java/com/booknara/problem/dp/MaxProductSubArray.java
@@ -7,6 +7,27 @@ package com.booknara.problem.dp;
 public class MaxProductSubArray {
   // T:O(n), S:O(1)
   public int maxProduct(int[] nums) {
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    int max = 0;
+    for (int n: nums) {
+      max = Math.max(max, n);
+    }
+    int curMin = 1;
+    int curMax = 1;
+
+    for (int i = 0; i < nums.length; i++) {
+      int temp = curMax;
+      curMax = Math.max(nums[i], Math.max(temp * nums[i], curMin * nums[i]));
+      curMin = Math.min(nums[i], Math.min(temp * nums[i], curMin * nums[i]));
+      max = Math.max(max, curMax);
+    }
+
+    return max;
+  }
+
+  public int maxProduct1(int[] nums) {
     // input check
     if (nums == null || nums.length == 0) return 0;
 
@@ -30,32 +51,6 @@ public class MaxProductSubArray {
     }
 
     return max;
-  }
-
-  // T:O(n), S:O(1)
-  public int maxProduct1(int[] nums) {
-    if (nums == null || nums.length == 0) return 0;
-    if (nums.length == 1) return nums[0];
-
-    int totalMax, positiveMax, negativeMax;
-    if (nums[0] < 0) {
-      negativeMax = nums[0];
-      positiveMax = 0;
-    } else {
-      negativeMax = 0;
-      positiveMax = nums[0];
-    }
-
-    totalMax = Math.max(positiveMax, negativeMax);
-    for (int i = 1; i < nums.length; i++) {
-      int temp = positiveMax;
-      positiveMax = Math.max(nums[i], Math.max(temp * nums[i], negativeMax * nums[i]));
-      negativeMax = Math.min(nums[i], Math.min(temp * nums[i], negativeMax * nums[i]));
-
-      totalMax = Math.max(totalMax, positiveMax);
-    }
-
-    return totalMax;
   }
 }
 /**
