8c2225d169079a570fc82292892b39e71a53e6e8
Daehee Han <bookdori81@gmail.com>
2023-12-12 22:15:12 -0800

    Update TreeNode.java

--- src/main/java/com/booknara/problem/common/TreeNode.java
+++ src/main/java/com/booknara/problem/common/TreeNode.java
@@ -4,7 +4,11 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int val) {
+
+  public TreeNode() {
+  }
+
+  public TreeNode(int val) {
         this.val = val;
     }
 
